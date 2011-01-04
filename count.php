b9a43aa4fec7d48924cd72a9822186966c8ea9ea
andreasb123 <andreasb123@9fe2bcd3-a095-4d8b-a836-9b85dc8d7627>
2011-01-04 11:45:12 +0000

    git-svn-id: svn://svn.code.sf.net/p/xournal/svn/trunk@6 9fe2bcd3-a095-4d8b-a836-9b85dc8d7627

---
+++ count.php
@@ -0,0 +1,31 @@
+<?php
+
+$c = 0;
+
+$dir = './src/';
+
+function countline($dir) {
+	$c = 0;
+	if (is_dir($dir)) {
+		if ($dh = opendir($dir)) {
+			while (($file = readdir($dh)) !== false) {
+				if(substr($file, -4) == '.cpp') {
+					foreach(file($dir . $file) as $l) {
+						if(trim($l) != '') {
+							$c++;
+						}
+					}
+				} else if($file[0] != '.') {
+					$c += countline($dir . $file . '/');
+				}
+			}
+			closedir($dh);
+		}
+	}
+	
+	return $c;
+}
+
+print "count of lines: " . countline($dir) . "\n";
+
+?>
