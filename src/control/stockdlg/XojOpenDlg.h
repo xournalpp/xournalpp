1aaf3e4f03a5c537f2b0b077a51e6132ea2d21e4
andreasb123 <andreasb123@9fe2bcd3-a095-4d8b-a836-9b85dc8d7627>
2011-03-03 16:09:36 +0000

    git-svn-id: svn://svn.code.sf.net/p/xournal/svn/trunk@55 9fe2bcd3-a095-4d8b-a836-9b85dc8d7627

---
+++ src/control/stockdlg/XojOpenDlg.h
@@ -0,0 +1,32 @@
+/*
+ * Xournal++
+ *
+ * GTK Open dialog to select XOJ (or PDF) file with preview
+ *
+ * @author Xournal Team
+ * http://xournal.sf.net
+ *
+ * @license GPL
+ */
+
+#ifndef __XOJOPENDLG_H__
+#define __XOJOPENDLG_H__
+
+#include "../../util/String.h"
+#include <gtk/gtk.h>
+
+#include "../settings/Settings.h"
+
+class XojOpenDlg {
+private:
+	XojOpenDlg();
+	virtual ~XojOpenDlg();
+
+public:
+	static String showOpenDialog(GtkWindow * win, Settings * settings, bool pdf, bool & attachPdf);
+
+private:
+	static void updatePreviewCallback(GtkFileChooser * fileChooser, void * userData);
+};
+
+#endif /* __XOJOPENDLG_H__ */
--- src/pdf/PdfExport.cpp
+++ src/pdf/PdfExport.cpp
@@ -698,3 +698,46 @@ bool PdfExport::createPdf(String uri) {
 	return true;
 }
 
+
+/**
+ *
+ * <?php
+require('fpdf.php');
+
+class PDF_Rotate extends FPDF
+{
+var $angle=0;
+
+function Rotate($angle, $x=-1, $y=-1)
+{
+    if($x==-1)
+        $x=$this->x;
+    if($y==-1)
+        $y=$this->y;
+    if($this->angle!=0)
+        $this->_out('Q');
+    $this->angle=$angle;
+    if($angle!=0)
+    {
+        $angle*=M_PI/180;
+        $c=cos($angle);
+        $s=sin($angle);
+        $cx=$x*$this->k;
+        $cy=($this->h-$y)*$this->k;
+        $this->_out(sprintf('q %.5f %.5f %.5f %.5f %.2f %.2f cm 1 0 0 1 %.2f %.2f cm', $c, $s, -$s, $c, $cx, $cy, -$cx, -$cy));
+    }
+}
+
+function _endpage()
+{
+    if($this->angle!=0)
+    {
+        $this->angle=0;
+        $this->_out('Q');
+    }
+    parent::_endpage();
+}
+}
+?>
+ */
+
