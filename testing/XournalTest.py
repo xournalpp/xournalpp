1764f91dc176984f43f9580cc40653f651d3004b
andreasb123 <andreasb123@9fe2bcd3-a095-4d8b-a836-9b85dc8d7627>
2011-04-03 17:51:49 +0000

    git-svn-id: svn://svn.code.sf.net/p/xournal/svn/trunk@111 9fe2bcd3-a095-4d8b-a836-9b85dc8d7627

---
+++ testing/XournalTest.py
@@ -0,0 +1,25 @@
+# Xournal++
+#
+# Testclass "Template"
+#
+# @author Xournal Team
+# http://xournal.sf.net
+#
+# @license GPL
+
+class ColorUndoAction(UndoRedoTest):
+
+	def test(self, xoj):
+		self.tearUp(xoj)
+		self.runTest(xoj)
+		self.tearDown(xoj)
+
+	def runTest(self, xoj):
+		pass
+
+	def tearUp(self, xoj):
+		pass
+
+	def tearDown(self, xoj):
+		pass
+
