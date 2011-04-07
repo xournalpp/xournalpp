7cd7185fcaf9a7cea1b3d9287e86f728770d5bb3
andreasb123 <andreasb123@9fe2bcd3-a095-4d8b-a836-9b85dc8d7627>
2011-04-07 19:13:04 +0000

    git-svn-id: https://xournal.svn.sourceforge.net/svnroot/xournal/trunk/xournalpp@116 9fe2bcd3-a095-4d8b-a836-9b85dc8d7627

---
+++ testing/tools/ToolHilighter/ToolHilighter.py
@@ -0,0 +1,26 @@
+# Xournal++
+#
+# Testclass for Undo / Redohandler
+#
+# @author Xournal Team
+# http://xournal.sf.net
+#
+# @license GPL
+
+from tools.ToolTest import ToolTest
+import os
+
+class ToolHilighter(ToolTest):
+	def __init__(self, xoj):
+		ToolTest.__init__(self, xoj)
+
+	def runTest(self):
+		self.xoj.setSelectedTool(self.xoj.TOOL_HILIGHTER)
+		self.xoj.setToolSize(self.xoj.TOOL_SIZE_FINE)
+		self.doTestInput()
+
+		path = os.path.realpath(__file__ + '/../result.xoj')
+		self.checkContents(path)
+
+
+
