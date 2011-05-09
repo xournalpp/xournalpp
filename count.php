e34d222e688463d96bf62392a4be8f4892519df6
andreasb123 <andreasb123@9fe2bcd3-a095-4d8b-a836-9b85dc8d7627>
2011-05-09 07:20:54 +0000

    Add dependency to librsvg (needed on Gentoo, because Xournal crashes while loading the SVG icons)
    git-svn-id: svn://svn.code.sf.net/p/xournal/svn/trunk@142 9fe2bcd3-a095-4d8b-a836-9b85dc8d7627

--- count.php
+++ count.php
@@ -1,18 +1,27 @@
 <?php
 
+
+$dontFilterWitespace = false;
+
 $c = 0;
 
 $dir = './src/';
 
 function countline($dir) {
+global $dontFilterWitespace;
+
 	$c = 0;
 	if (is_dir($dir)) {
 		if ($dh = opendir($dir)) {
 			while (($file = readdir($dh)) !== false) {
 				if(substr($file, -4) == '.cpp') {
-					foreach(file($dir . $file) as $l) {
-						if(trim($l) != '') {
-							$c++;
+					if($dontFilterWitespace) {
+						$c += count(file($dir . $file));
+					} else {
+						foreach(file($dir . $file) as $l) {
+							if(trim($l) != '') {
+								$c++;
+							}
 						}
 					}
 				} else if($file[0] != '.') {
