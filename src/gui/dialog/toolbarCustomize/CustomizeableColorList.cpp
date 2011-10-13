d94ad496f2c95c1e66dc410533abcbc06da4aa73
AndreasB123 <AndreasB123@9fe2bcd3-a095-4d8b-a836-9b85dc8d7627>
2011-10-13 08:07:40 +0000

    Added Tool SELECT_OBJECT to toolbar
    git-svn-id: https://xournal.svn.sourceforge.net/svnroot/xournal/trunk/xournalpp@182 9fe2bcd3-a095-4d8b-a836-9b85dc8d7627

---
+++ src/gui/dialog/toolbarCustomize/CustomizeableColorList.cpp
@@ -0,0 +1,10 @@
+#include "CustomizeableColorList.h"
+
+CustomizeableColorList::CustomizeableColorList() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CustomizeableColorList::~CustomizeableColorList() {
+	// TODO Auto-generated destructor stub
+}
