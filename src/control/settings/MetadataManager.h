7afee6e435fe6e2f6dbff592cccc6b2078460747
Sebastian 'Swift Geek' Grzywna <swiftgeek@gmail.com>
2018-07-20 23:17:25 +0200

    Likely an unlikely workaround

--- src/control/settings/MetadataManager.h
+++ src/control/settings/MetadataManager.h
@@ -16,7 +16,13 @@
 #include <glib.h>
 
 #include <boost/asio.hpp>
+
+#undef GOOLIKELY_H
+#undef likely
+#undef unlikely
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include "../../pdf/popplerdirect/workaround/poppler-0.64.0/goo/GooLikely.h"
+
 #include <boost/filesystem/path.hpp>
 #include <boost/property_tree/ini_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
