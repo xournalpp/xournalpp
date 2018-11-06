c16fb905ae586b22fc3cf5e998e89b10fec18c74
morro <morrolinux@gmail.com>
2018-11-06 22:39:21 +0100

    updated launcher

--- launcher/usr/local/bin/xopp-recording.sh
+++ launcher/usr/local/bin/xopp-recording.sh
@@ -14,6 +14,7 @@ then
 	filename=$2
         notify-send "Recording started"
         amixer sset 'Capture' 35%
+        amixer sset 'Master' 0%
         exec arecord -f cd -d 3600 -t wav |lame --preset 56 -mm - "$directory""$filename" &
 elif [[ $1 == "stop" ]]
 then
--- launcher/usr/local/bin/xournalpp-ts-launcher.sh
+++ launcher/usr/local/bin/xournalpp-ts-launcher.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-xournalpp -f /home/morro/Musica/recordings/ "$1"
+audiopath="$HOME/Musica/recordings/"
+
+if [[ $# -lt 1 ]]
+then
+	xournalpp -f $audiopath
+else
+	xournalpp -f $audiopath "$1"
+fi
