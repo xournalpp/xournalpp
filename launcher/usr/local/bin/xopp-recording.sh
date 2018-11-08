7c560ef3559f228207d63583baa7bff162660019
morro <morrolinux@gmail.com>
2018-11-08 11:14:50 +0100

    improving recording script

--- launcher/usr/local/bin/xopp-recording.sh
+++ launcher/usr/local/bin/xopp-recording.sh
@@ -12,10 +12,13 @@ fi
 if [[ $1 == "start" ]] && [[ $# -eq 2 ]]
 then
 	filename=$2
+
+	exec &> ${directory}${filename}.log 
+
         notify-send "Recording started"
         amixer sset 'Capture' 35%
         amixer sset 'Master' 0%
-        exec arecord -f cd -d 3600 -t wav |lame --preset 56 -mm - "$directory""$filename" &
+        arecord -f cd -t wav |lame --preset 56 -mm - "$directory""$filename" &
 elif [[ $1 == "stop" ]]
 then
         pkill -f "arecord" && notify-send "Recording stopped"
