ec649a686f2752d7bf232fae31035b143e8c9d01
morro <morrolinux@gmail.com>
2018-11-23 10:20:20 +0100

    using psw variable + added TODO

--- src/gui/PageView.cpp
+++ src/gui/PageView.cpp
@@ -386,11 +386,13 @@ void XojPageView::playObjectAt(double x, double y)
 							}
 						}
 						else
-						{
+						{	
+							//TODO: substitute system(..) with some c++ curl library
+							string psw("password");
 							string command(
-									"curl -s -u \"\":\"password\" --url \"http://127.0.0.1:8080/requests/status.xml?command=seek&val="
+									"curl -s -u \"\":\""+psw+"\" --url \"http://127.0.0.1:8080/requests/status.xml?command=seek&val="
 											+ std::to_string(ts) + "\" >/dev/null"
-											+ "&& curl -s -u \"\":\"password\" --url \"http://127.0.0.1:8080/requests/status.xml?command=pl_play\" >/dev/null");
+											+ "&& curl -s -u \"\":\""+psw+"\" --url \"http://127.0.0.1:8080/requests/status.xml?command=pl_play\" >/dev/null");
 							system(command.c_str());
 						}
 					}
