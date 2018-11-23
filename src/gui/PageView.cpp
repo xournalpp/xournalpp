fc1acb81c47fabb5d72a97f831d8b544a5ee9922
morro <morrolinux@gmail.com>
2018-11-23 10:13:20 +0100

    improved autoplay

--- src/gui/PageView.cpp
+++ src/gui/PageView.cpp
@@ -382,7 +382,6 @@ void XojPageView::playObjectAt(double x, double y)
 								string command(
 										"vlc --qt-start-minimized " + audioFolder + fn + " --start-time="
 												+ std::to_string(ts) + " &>/dev/null &");
-								std::cout << "command: " << command << std::endl;
 								system(command.c_str());
 							}
 						}
@@ -390,7 +389,8 @@ void XojPageView::playObjectAt(double x, double y)
 						{
 							string command(
 									"curl -s -u \"\":\"password\" --url \"http://127.0.0.1:8080/requests/status.xml?command=seek&val="
-											+ std::to_string(ts) + "\" >/dev/null");
+											+ std::to_string(ts) + "\" >/dev/null"
+											+ "&& curl -s -u \"\":\"password\" --url \"http://127.0.0.1:8080/requests/status.xml?command=pl_play\" >/dev/null");
 							system(command.c_str());
 						}
 					}
