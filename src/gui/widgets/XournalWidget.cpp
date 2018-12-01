6b2aa7ad0f560eb85278ed1cc2f24f2a6bbb4821
Andreas Butti <andreas.butti@gmail.com>
2018-12-01 11:14:25 +0100

    Comment out code which is not fully working

--- src/gui/widgets/XournalWidget.cpp
+++ src/gui/widgets/XournalWidget.cpp
@@ -92,25 +92,26 @@ GType gtk_xournal_get_type(void)
 
 static void gtk_xournal_init_touch_handling(GtkXournal* xournal)
 {
-	Settings* settings = xournal->view->getControl()->getSettings();
-	ButtonConfig* cfg = settings->getTouchButtonConfig();
-
-	if (cfg->getDisableDrawing())
-	{
-		DeviceListHelper devList;
-		for (InputDevice& dev : devList.getDeviceList())
-		{
-			if (cfg->device == dev.getName())
-			{
-				printf("Disable device for drawing: %s\n", dev.getName().c_str());
-//				gtk_widget_set_device_enabled(GTK_WIDGET(xournal), dev.getDevice(), false);
-				gtk_widget_set_device_events(GTK_WIDGET(xournal), dev.getDevice(), (GdkEventMask) 0);
-				return;
-			}
-		}
-
-		printf("Could NOT disable device for drawing!\n");
-	}
+// Currently does not work, needs further testing
+//	Settings* settings = xournal->view->getControl()->getSettings();
+//	ButtonConfig* cfg = settings->getTouchButtonConfig();
+//
+//	if (cfg->getDisableDrawing())
+//	{
+//		DeviceListHelper devList;
+//		for (InputDevice& dev : devList.getDeviceList())
+//		{
+//			if (cfg->device == dev.getName())
+//			{
+//				printf("Disable device for drawing: %s\n", dev.getName().c_str());
+////				gtk_widget_set_device_enabled(GTK_WIDGET(xournal), dev.getDevice(), false);
+//				gtk_widget_set_device_events(GTK_WIDGET(xournal), dev.getDevice(), (GdkEventMask) 0);
+//				return;
+//			}
+//		}
+//
+//		printf("Could NOT disable device for drawing!\n");
+//	}
 }
 
 GtkWidget* gtk_xournal_new(XournalView* view, GtkScrollable* parent)
@@ -608,6 +609,9 @@ gboolean gtk_xournal_touch_event(GtkWidget* widget, GdkEventTouch* event)
 
 	// GtkXournal* xournal = GTK_XOURNAL(widget);
 	// printf("Touch event!\n");
+	// Currently does not really work
+	// It's getting called, but the events
+	// are not consumed!
 
 	// Consume event
 	return true;
