fa53ca920a8cd7ebf56ff763859d56ddb668e1ea
Andreas Butti <andreas.butti@gmail.com>
2018-12-03 20:52:20 +0100

    Starting with Debian Build / Ubuntu PPA

---
+++ packaging/debian/test.sh
@@ -0,0 +1,10 @@
+## Test Ubuntu PPA Build
+##
+## Preparation:
+## sudo apt install git-build-recipe
+
+git-build-recipe --package xournalpp --allow-fallback-to-native xournalpp.recipe ppa-build
+
+
+
+
