d6d41edc1900561e0ae661caf02d8aeed0c55369
Andreas Butti <andreas.butti@gmail.com>
2018-12-27 09:53:33 +0100

    Translation

--- src/gui/dialog/ButtonConfigGui.cpp
+++ src/gui/dialog/ButtonConfigGui.cpp
@@ -98,7 +98,7 @@ ButtonConfigGui::ButtonConfigGui(SettingsDialog* dlg, GladeSearchpath* gladeSear
 	// DRAWING_TYPE_ARROW
 	gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(this->cbDrawingType), _("Draw Arrow"));
 	// DRAWING_TYPE_COORDINATE_SYSTEM
-	gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(this->cbDrawingType), _("Draw Coordinate System"));
+	gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(this->cbDrawingType), _("Draw coordinate system"));
 	// DRAWING_TYPE_STROKE_RECOGNIZER
 	gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(this->cbDrawingType), _("Stroke recognizer"));
 
