09b445fadb4c21377f0337b4ee77a617fe09ed8c
Andreas Butti <andreas.butti@gmail.com>
2018-12-28 18:24:43 +0100

    Windows Build

--- src/util/GzUtil.cpp
+++ src/util/GzUtil.cpp
@@ -3,7 +3,7 @@
 gzFile GzUtil::openPath(Path path, string flags)
 {
 #ifdef WIN32
-	wchar_t* wfilename = g_utf8_to_utf16(path.c_str(), -1, NULL, NULL, NULL);
+	wchar_t* wfilename = (wchar_t*)g_utf8_to_utf16(path.c_str(), -1, NULL, NULL, NULL);
 	gzFile fp = gzopen_w(wfilename, flags.c_str());
 	g_free(wfilename);
 
