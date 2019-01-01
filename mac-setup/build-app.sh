95281e76ad3209893e0dbea5b6a45d066404653e
Andreas Butti <andreasbutti@gmail.com>
2019-01-01 20:57:01 +0100

    Mac build

--- mac-setup/build-app.sh
+++ mac-setup/build-app.sh
@@ -11,11 +11,7 @@ cd "${0%/*}"
 # delete old app, if there
 echo "clean old app"
 
-if [[ $1 == u ]]; then
-  echo "update app!"
-else
-  rm -rf ./Xournal++.app
-fi
+rm -rf ./Xournal++.app
 
 echo "prepare macdylibbundler"
 if [ ! -d "macdylibbundler" ]; then
@@ -29,21 +25,29 @@ fi
 make -j 2
 
 cd ..
-echo "prepare package"
+echo "copy binaries"
 
 mkdir -p Xournal++.app/Contents/MacOS
 mkdir -p Xournal++.app/Contents/Resources
 cp ../build/src/xournalpp ./Xournal++.app/Contents/MacOS/xournalpp
 
-if [[ $1 == u ]]; then
-  echo "update app!"
-else
-  ./macdylibbundler/dylibbundler -od -b -x ./Xournal++.app/Contents/MacOS/xournalpp -d ./Xournal++.app/Contents/libs/
-fi
+./macdylibbundler/dylibbundler -od -b -x ./Xournal++.app/Contents/MacOS/xournalpp -d ./Xournal++.app/Contents/libs/
+
+mdkir -p ./Xournal++.app/Contents/lib
+
+echo "copy pixbuf libs"
+cp -r /usr/local/lib/gdk-pixbuf-2.0 ./Xournal++.app/Contents/lib
+
+echo "copy pixbuf lib dependencies"
+./macdylibbundler/dylibbundler -od -b -x ./Xournal++.app/Contents/lib/gdk-pixbuf-2.0/2.10.0/loaders/*.so -d ./Xournal++.app/Contents/libs/
+
+echo "copy icons"
+## TODO
+
+echo "finalize packaging"
 
 cp icon/xournalpp.icns ./Xournal++.app/Contents/Resources/xournalpp.icns
 cp Info.plist ./Xournal++.app/Contents/Info.plist
 cp -rvp ../ui ./Xournal++.app/Contents/Resources/
-
 
 echo "finished"
