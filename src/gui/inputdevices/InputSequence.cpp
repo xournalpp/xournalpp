ced056eea66e1fad36c10c7da4ff06cf96ad4124
JJones780 <JJones780@users.noreply.github.com>
2019-04-05 02:57:25 -0600

    put back a check that might be useful for some hardware/GTK combinations.

--- src/gui/inputdevices/InputSequence.cpp
+++ src/gui/inputdevices/InputSequence.cpp
@@ -13,6 +13,7 @@
 #include "model/Point.h"
 
 #include <Util.h>
+#include <cmath>
 
 
 InputSequence::InputSequence(NewGtkInputDevice* inputHandler)
@@ -152,6 +153,13 @@ void InputSequence::handleScrollEvent()
 	// use root coordinates as reference point because
 	// scrolling changes window relative coordinates
 	// see github Gnome/evince@1adce5486b10e763bed869
+
+	// GTK handles event compression/filtering differently between versions - this may be needed on certain hardware/GTK combinations.
+	if (std::abs(lastMousePositionX - rootX) < 0.1 && std::abs( lastMousePositionY - rootY) < 0.1 )
+	{
+		return;
+	}
+
 
 	if (scrollOffsetX == 0 && scrollOffsetY == 0)
 	{
