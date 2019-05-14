70ac3d0db741168aa03fad1751a96d20f20cb23c
alexruetz <alex.ruetz1@gmail.com>
2019-05-14 07:55:28 +0200

    fixed typo in build-flatpack.sh

--- build-flatpack.sh
+++ build-flatpack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-flatpak-builder ./flatpak-build org.github.xournalpp.xournalpp.yaml --force-clean --repo=./repo --jobs=1
+flatpak-builder ./flatpak-build com.github.xournalpp.xournalpp.yaml --force-clean --repo=./repo --jobs=1
 flatpak build-bundle ./repo  xournalpp.flatpak com.github.xournalpp.xournalpp
