6ef874c06617c4a33081f7b9c0ba76448609474b
Ulrich Huber <ulrich@huberulrich.de>
2019-05-14 17:44:24 +0200

    Add ability to assign specific classes to devices Back GUI with settings Remodel settings to work with increased number of tabs

---
+++ src/gui/dialog/DeviceClassConfigGui.h
@@ -0,0 +1,48 @@
+/*
+ * Xournal++
+ *
+ * The configuration for a button in the Settings Dialog
+ *
+ * @author Xournal++ Team
+ * https://github.com/xournalpp/xournalpp
+ *
+ * @license GNU GPLv2 or later
+ */
+
+#pragma once
+
+#include "control/Actions.h"
+#include "gui/GladeGui.h"
+
+#include <gdk/gdk.h>
+#include <util/DeviceListHelper.h>
+
+class Settings;
+class SettingsDialog;
+
+class DeviceClassConfigGui : public GladeGui
+{
+public:
+	DeviceClassConfigGui(GladeSearchpath* gladeSearchPath, GtkWidget* w, Settings* settings, InputDevice device);
+	virtual ~DeviceClassConfigGui();
+
+public:
+	void loadSettings();
+	void saveSettings();
+
+	// Not implemented! This is not a dialog!
+	virtual void show(GtkWindow* parent);
+
+private:
+	static void cbSelectCallback(GtkComboBox* widget, DeviceClassConfigGui* gui);
+	void enableDisableTools();
+
+private:
+	XOJ_TYPE_ATTRIB;
+
+	Settings* settings;
+	InputDevice device;
+
+	GtkWidget* labelDevice;
+	GtkWidget* cbDeviceClass;
+};
