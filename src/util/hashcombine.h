f4e4fcff86611e76a391f41900199a7bd58a14e5
Fabian Keßler <fabian_kessler@gmx.de>
2019-08-07 19:17:04 +0200

    add a hashcombine function

---
+++ src/util/hashcombine.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//taken from boost
+namespace boost_c
+{
+
+template <class T>
+inline void hash_combine(std::size_t& seed, const T& v)
+{
+	std::hash<T> hasher;
+	seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+}  // namespace boost_c
