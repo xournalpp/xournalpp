b1e95c057f22c073083dc50e0ac6ced9021f98dc
Bryan Tan <Technius@users.noreply.github.com>
2019-10-16 10:08:47 -0700

    [skip ci] Merge pull request #1541 from Technius/ci/new-diff-lines
    Improve format_diff_lines.pl utility script

---
+++ azure-pipelines/util/sample.cpp
@@ -0,0 +1,28 @@
+void foo() {
+int bar;
+}
+
+class Hi
+{
+	Hi(
+		) : a(a), b(b)
+	{
+	}
+	int a;
+	int b; int c; int d;
+}
+	;
+
+
+void goodFormatting()
+{
+	int a = 0;
+	int b;
+	int c;
+	int d;
+	if (a) {
+		while (true) {
+			b = 1;
+		}
+	}
+}
