f5b6b31cb9a4776f4fc8f5358758895c297c543f
Bryan Tan <techniux@gmail.com>
2020-04-11 18:57:27 -0700

    Rename xournal-thumbnailer to xournalpp-thumbnailer
    "xournal-thumbnailer" breaks tab completion for xournalpp

--- src/xoj-preview-extractor/desktop-install.sh
+++ src/xoj-preview-extractor/desktop-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source`
-gconftool-2 --makefile-$1-rule xournal-thumbnailer-xoj.schemas
+gconftool-2 --makefile-$1-rule xournalpp-thumbnailer-xoj.schemas
