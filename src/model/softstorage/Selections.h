c365df653e59b27738963b29282a6ee934b4fcc2
Julius Lehmann <julius@devpi.de>
2020-04-28 16:07:09 +0200

    Fix missing header in Selections

--- src/model/softstorage/Selections.h
+++ src/model/softstorage/Selections.h
@@ -10,7 +10,8 @@
 #include <control/tools/EditSelection.h>
 #include <model/Storage.h>
 
-#include "SelectionEvent.h"
+class SelectionEvent {};
+
 class Selections: public Storage<SelectionEvent> {
 public:
     auto getSelection() -> std::shared_ptr<EditSelection>;
