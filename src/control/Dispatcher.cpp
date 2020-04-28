054a40ebf5f980fb576ff5b7532942c7a8dd647d
Julius Lehmann <julius@devpi.de>
2020-04-28 16:07:28 +0200

    Implement dispatcher with directed graph dependency management

--- src/control/Dispatcher.cpp
+++ src/control/Dispatcher.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "Dispatcher.h"
+
+#include <utility>
+
+#include <boost/graph/breadth_first_search.hpp>
+
+auto Dispatcher::dispatch(const Action& action) -> void {
+    boost::breadth_first_search(listenerGraph, initialNode,
+                                boost::visitor(Visitor{[&](auto v, auto g) { g[v].listener->onAction(action); }}));
+}
+auto Dispatcher::registerListener(std::shared_ptr<ActionListener> listener, std::function<bool(Action)> filter)
+        -> void {
+    VertexData vertexData{std::move(listener), std::move(filter)};
+    if (listenerGraph.vertex_set().empty()) {
+        auto descriptor = boost::add_vertex(vertexData, listenerGraph);
+        initialNode = descriptor;
+    } else
+        boost::add_vertex(vertexData, listenerGraph);
+}
+auto Dispatcher::waitFor(const std::shared_ptr<ActionListener>& listener,
+                         const std::shared_ptr<ActionListener>& dependency) -> void {
+    if (listenerGraph.vertex_set().empty())
+        return;
+    auto resultMap = findVertices(std::vector<std::shared_ptr<ActionListener>>{listener, dependency});
+    if (resultMap.size() != 2)
+        return;
+    boost::add_edge(resultMap[1], resultMap[0], listenerGraph);
+}
+
+auto Dispatcher::findVertices(const std::vector<std::shared_ptr<ActionListener>>& listeners)
+        -> std::vector<VertexDescriptor> {
+    std::vector<VertexDescriptor> result{};
+    boost::breadth_first_search(listenerGraph, initialNode, boost::visitor(Visitor{[&](auto v, auto g) {
+                                    for (int i = 0; i < listeners.size(); i++)
+                                        if (g[v].listener == listeners[i])
+                                            result[i] = v;
+                                }}));
+    return result;
+}
+
+auto Visitor::examine_vertex(VertexDescriptor v, const Graph& g) const -> void { visit(v, g); }
+Visitor::Visitor(std::function<void(VertexDescriptor, Graph)> visit): visit(std::move(visit)) {}
--- src/control/Dispatcher.h
+++ src/control/Dispatcher.h
@@ -5,8 +5,29 @@
 #pragma once
 
 #include <functional>
+#include <memory>
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/breadth_first_search.hpp>
+#include <boost/graph/graph_traits.hpp>
 
 #include "ActionListener.h"
+
+struct VertexData {
+    std::shared_ptr<ActionListener> listener;
+    std::function<bool(Action)> filter;
+};
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, VertexData> Graph;
+typedef boost::graph_traits<Graph>::vertex_descriptor VertexDescriptor;
+
+class Visitor: public boost::default_bfs_visitor {
+public:
+    Visitor(std::function<void(VertexDescriptor, Graph)> visit);
+    auto examine_vertex(VertexDescriptor v, const Graph& g) const -> void;
+    std::function<void(VertexDescriptor, Graph)> visit;
+};
+
 
 class Dispatcher {
 public:
@@ -25,8 +46,17 @@ public:
 
 public:
     auto dispatch(const Action& action) -> void;
-    auto registerListener(const ActionListener& storage, std::function<bool(Action)> filter) -> void;
+    auto waitFor(const std::shared_ptr<ActionListener>& listener, const std::shared_ptr<ActionListener>& dependency)
+            -> void;
+    auto registerListener(std::shared_ptr<ActionListener> listener, std::function<bool(Action)> filter) -> void;
+    // auto unregisterListener(const std::shared_ptr<ActionListener>& listener) -> void;
 
 private:
-    Dispatcher(){};
+    Dispatcher() = default;
+
+    Graph listenerGraph{};
+    VertexDescriptor initialNode;
+
+private:
+    auto findVertices(const std::vector<std::shared_ptr<ActionListener>>& listeners) -> std::vector<VertexDescriptor>;
 };
