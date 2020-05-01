d683752b6d2f90119be413eac55bbb77e4feb08c
Julius Lehmann <julius@devpi.de>
2020-05-01 21:35:16 +0200

    add method stub

--- src/xournalview/Viewport.cpp
+++ src/xournalview/Viewport.cpp
@@ -4,9 +4,4 @@
 
 #include "Viewport.h"
 
-#include "control/Dispatcher.h"
-
-Viewport::Viewport() {
-    Dispatcher::getMainStage().registerListener(*this,
-                                                [](auto action) { return typeid(action) == typeid(ViewportAction); });
-}
+auto viewportUpdate(Viewport model, ViewportAction action) -> ViewportResult {}
