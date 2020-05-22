6294dad243ad0bbc55481bede19d222f9a7a6209
Julius Lehmann <julius@devpi.de>
2020-05-22 12:32:09 +0200

    Running gtk 4 - nested scrolled windows

--- base/src/xournalpp/gui/MainWindow.cpp
+++ base/src/xournalpp/gui/MainWindow.cpp
@@ -3,9 +3,16 @@
 #include <gtk-4.0/gtk/gtk.h>
 
 MainWindow::MainWindow(GtkApplication* app, XournalppStore store): store(std::move(store)) {
-    auto gladeBuilder = gtk_builder_new_from_resource("/ui/main.glade");
-    this->window = GTK_WINDOW(gtk_builder_get_object(gladeBuilder, "mainWindow"));
-    gtk_window_set_application(this->window, app);
+    this->window = reinterpret_cast<GtkWindow*>(gtk_application_window_new(app));
+    auto scrolledTop = gtk_scrolled_window_new(NULL, NULL);
+    auto scrolledNested = gtk_scrolled_window_new(NULL, NULL);
+    auto child = gtk_image_new_from_file("/home/julius/Bilder/mr-robot-fsociety-logo-minimalism-qs-2560x1440.jpg");
+    gtk_widget_set_size_request(child, 1000, 1000);
+    gtk_scrolled_window_set_child(reinterpret_cast<GtkScrolledWindow*>(scrolledNested), child);
+    gtk_scrolled_window_set_child(reinterpret_cast<GtkScrolledWindow*>(scrolledTop), scrolledNested);
+    gtk_widget_set_hexpand(scrolledTop, true);
+    gtk_widget_set_vexpand(scrolledTop, true);
+    gtk_window_set_child(this->window, scrolledTop);
 }
 
 auto MainWindow::show() -> void { gtk_window_present(this->window); }
