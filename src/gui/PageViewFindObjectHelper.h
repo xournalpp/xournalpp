aaa2cd3280bdb9aacb831d0e6ee2df0f6d89de31
Roland Lötscher <40485433+rolandlo@users.noreply.github.com>
2020-07-28 07:05:20 +0200

    fix crash after selecting object from other  layer (#2136)
    Backport: 649fb8b

--- src/gui/PageViewFindObjectHelper.h
+++ src/gui/PageViewFindObjectHelper.h
@@ -44,13 +44,8 @@ public:
 		view->xournal->getControl()->clearSelection();
 		matchRect = { gint(x - 10), gint(y - 10), 20, 20 };
 
-		for (Layer* l : *view->page->getLayers())
-		{
-			if (view->page->isLayerVisible(l))
-			{
-				return checkLayer(l);
-			}
-		}
+        Layer* layer = this->view->getPage()->getSelectedLayer();
+        return checkLayer(layer);
 		return false;
 	}
 
