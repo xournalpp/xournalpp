dbac0ca876b5bb43e0859babe3e030789bbe0bf7
Ari Breitkreuz <ari.breitkreuz@pm.me>
2020-08-14 16:53:59 +0200

    Remove tests covered by std::filesystem

--- test/util/PathTest.cpp
+++ test/util/PathTest.cpp
@@ -25,9 +25,6 @@ class PathTest: public CppUnit::TestFixture {
 
     CPPUNIT_TEST(testUnsupportedUri);
     CPPUNIT_TEST(testPathFromUri);
-    CPPUNIT_TEST(testParentPath);
-    CPPUNIT_TEST(testFilename);
-    CPPUNIT_TEST(testHasExtension);
     CPPUNIT_TEST(testClearExtensions);
 
     CPPUNIT_TEST_SUITE_END();
@@ -48,39 +45,6 @@ public:
         fs::path b = PathUtil::fromUri("file:///tmp/test.txt");
         CPPUNIT_ASSERT_EQUAL(false, b.empty());
         CPPUNIT_ASSERT_EQUAL(G_DIR_SEPARATOR_S + string("tmp") + G_DIR_SEPARATOR_S + string("test.txt"), b.string());
-    }
-
-    void testParentPath() {
-        fs::path a("C:\\test\\abc\\xyz.txt");
-        CPPUNIT_ASSERT_EQUAL(string("C:\\test\\abc"), a.parent_path().string());
-        CPPUNIT_ASSERT_EQUAL(string("C:\\test"), a.parent_path().parent_path().string());
-
-        fs::path b("/temp/test/asdf.txt");
-        CPPUNIT_ASSERT_EQUAL(string("/temp/test"), b.parent_path().string());
-        CPPUNIT_ASSERT_EQUAL(string("/temp"), b.parent_path().parent_path().string());
-    }
-
-    void testFilename() {
-        fs::path a("C:\\test\\abc\\xyz.txt");
-        CPPUNIT_ASSERT_EQUAL(string("xyz.txt"), a.filename().string());
-        CPPUNIT_ASSERT_EQUAL(string("abc"), a.parent_path().filename().string());
-
-        fs::path b("/temp/test/asdf.txt");
-        CPPUNIT_ASSERT_EQUAL(string("asdf.txt"), b.filename().string());
-        CPPUNIT_ASSERT_EQUAL(string("test"), b.parent_path().filename().string());
-    }
-
-    void testHasExtension() {
-        // fs::path a("C:\\test\\abc\\xyz.txt");
-        fs::path a("/test/abc/xyz.txt");
-        CPPUNIT_ASSERT_EQUAL(string(".txt"), a.extension().string());
-
-        // fs::path b("C:\\test\\abc\\xyz.TXT");
-        fs::path b("test/abc/xyz.TXT");
-        CPPUNIT_ASSERT_EQUAL(string(".txt"), b.extension().string());
-
-        CPPUNIT_ASSERT_EQUAL(false, a.extension() == ".");
-        CPPUNIT_ASSERT_EQUAL(false, a.extension() == "xyz");
     }
 
     void testClearExtensions() {
@@ -139,18 +103,6 @@ public:
         b = fs::path("/test/asdf.pdf");
         PathUtil::clearExtensions(b);
         CPPUNIT_ASSERT_EQUAL(string("/test/asdf.pdf"), b.string());
-    }
-
-    void testOperators() {
-        fs::path a("/test/a");
-        fs::path b = a / "foo.pdf";
-        CPPUNIT_ASSERT_EQUAL(string("/test/a"), a.string());
-        CPPUNIT_ASSERT_EQUAL(string("/test/foo.pdf"), b.string());
-
-        a /= "bar.pdf";
-        CPPUNIT_ASSERT_EQUAL(string("/test/a/bar.pdf"), a.string());
-        // b should not be affected by a
-        CPPUNIT_ASSERT_EQUAL(string("/test/foo.pdf"), b.string());
     }
 };
 
