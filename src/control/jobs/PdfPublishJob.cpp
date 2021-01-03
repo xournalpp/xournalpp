c838fe2450f866989bfe71a7ea40e017dc9a301f
Daniel Haase <daniel.haase@kit.edu>
2021-01-03 21:20:52 +0100

    Windows C string conversion fixed

--- src/control/jobs/PdfPublishJob.cpp
+++ src/control/jobs/PdfPublishJob.cpp
@@ -21,7 +21,7 @@ void PdfPublishJob::afterExport() {
             if (pos == string::npos)
                 break;
             modscript.erase(pos, parameter.length());
-            modscript.insert(pos, string(target));
+            modscript.insert(pos, string(target.c_str()));
             pos += 1;
         }
         system(modscript.c_str());
