adea7ffdb1bc22251d63c9d0f1a403f984684c83
rolandlo <roland_loetscher@hotmail.com>
2021-02-08 17:15:56 +0100

    merge back ScrollHandlingGtk into base class

--- src/gui/Layout.h
+++ src/gui/Layout.h
@@ -153,7 +153,7 @@ private:
     static void checkScroll(GtkAdjustment* adjustment, double& lastScroll);
 
     /**
-     * Calls the ScrollHandlingGtk to set the layout size by updating the horizontal and vertical GtkAdjustments
+     * Calls the scroll handler to set the layout size by updating the horizontal and vertical GtkAdjustments
      */
     // Todo(Fabian): remove this function the true size always depends on the expose event
     void setLayoutSize(int width, int height);
--- src/gui/RepaintHandler.cpp
+++ src/gui/RepaintHandler.cpp
@@ -11,26 +11,18 @@ RepaintHandler::RepaintHandler(XournalView* xournal): xournal(xournal) {}
 RepaintHandler::~RepaintHandler() { this->xournal = nullptr; }
 
 void RepaintHandler::repaintPage(XojPageView* view) {
-    if (xournal->getScrollHandling()->fullRepaint()) {
-        gtk_widget_queue_draw(this->xournal->getWidget());
-    } else {
-        int x1 = view->getX();
-        int y1 = view->getY();
-        int x2 = x1 + view->getDisplayWidth();
-        int y2 = y1 + view->getDisplayHeight();
+    int x1 = view->getX();
+    int y1 = view->getY();
+    int x2 = x1 + view->getDisplayWidth();
+    int y2 = y1 + view->getDisplayHeight();
 
-        gtk_xournal_repaint_area(this->xournal->getWidget(), x1, y1, x2, y2);
-    }
+    gtk_xournal_repaint_area(this->xournal->getWidget(), x1, y1, x2, y2);
 }
 
 void RepaintHandler::repaintPageArea(XojPageView* view, int x1, int y1, int x2, int y2) {
-    if (xournal->getScrollHandling()->fullRepaint()) {
-        gtk_widget_queue_draw(this->xournal->getWidget());
-    } else {
-        int x = view->getX();
-        int y = view->getY();
-        gtk_xournal_repaint_area(this->xournal->getWidget(), x + x1, y + y1, x + x2, y + y2);
-    }
+    int x = view->getX();
+    int y = view->getY();
+    gtk_xournal_repaint_area(this->xournal->getWidget(), x + x1, y + y1, x + x2, y + y2);
 }
 
 void RepaintHandler::repaintPageBorder(XojPageView* view) { gtk_widget_queue_draw(this->xournal->getWidget()); }
