bf166698014aef75b1bd32353193fd9d99285319
Henry Heino <personalizedrefrigerator@gmail.com>
2021-02-21 16:33:17 -0800

    LaTeX Dialog: Add editor font setting

--- src/core/model/Font.h
+++ src/core/model/Font.h
@@ -22,6 +22,8 @@
 class XojFont: public Serializable {
 public:
     XojFont();
+    XojFont(std::string name, double size);
+
     ~XojFont() override;
 
 public:
@@ -31,7 +33,23 @@ public:
     double getSize() const;
     void setSize(double size);
 
+    /**
+     * @return The Pango-style string that represents this
+     * font.
+     */
+    std::string asString() const;
+
     void operator=(const XojFont& font);
+
+    /**
+     * Set this from a Pango-style font description.
+     * E.g.
+     *   Serif 12
+     * sets this' size to 12 and this font's name to Serif.
+     *
+     * @param description Pango-style font description.
+     */
+    void operator=(const std::string& description);
 
 public:
     // Serialize interface
