26368ef9a51be8b027271e0a1874f1a996868570
Febbe <fabian_kessler@gmx.de>
2021-03-01 21:45:38 +0100

    Update scrpts

--- mac-setup/build-gtk3.sh
+++ mac-setup/build-gtk3.sh
@@ -1,13 +1,11 @@
-export PATH="$HOME/.new_local/bin:$PATH"
-
+#!/bin/bash -x
 # missing six python lib:
 # after first failure download, unpack and install with
 # /Users/yourname/gtk/inst/bin/python setup.py install
+export PATH="$HOME/.new_local/bin:$PATH"
 
-curl https://gitlab.gnome.org/GNOME/gtk-osx/raw/master/gtk-osx-setup.sh -o gtk-osx-setup.sh
+curl -L -O https://gitlab.gnome.org/GNOME/gtk-osx/raw/master/gtk-osx-setup.sh
 chmod +x gtk-osx-setup.sh
 ./gtk-osx-setup.sh
-
-jhbuild bootstrap
 jhbuild bootstrap-gtk-osx
-jhbuild build python meta-gtk-osx-bootstrap meta-gtk-osx-gtk3
+jhbuild build meta-gtk-osx-bootstrap meta-gtk-osx-gtk3
--- mac-setup/build-portaudio.sh
+++ mac-setup/build-portaudio.sh
@@ -1,13 +1,13 @@
-# go to script directory
-cd "${0%/*}"
-
+#!/bin/bash -x
 export PATH="$HOME/.new_local/bin:$PATH"
 export LIBRARY_PATH="$HOME/gtk/inst/lib:$LIBRARY_PATH"
+# go to script directory
+cd "${0%/*}" || exit
 
-curl -L  http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz -o pa_stable_v190600_20161030.tgz
+curl -L http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz -o pa_stable_v190600_20161030.tgz
 tar xzf pa_stable_*.tgz
 
-cd portaudio
+cd portaudio || exit
 
 ./configure --enable-cxx --disable-mac-universal --prefix=$HOME/gtk/inst
 make -j2
