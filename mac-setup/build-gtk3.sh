aa754ff7d2c2711d9dc9c984b256255cd10880bc
Febbe <fabian_kessler@gmx.de>
2021-03-01 21:45:38 +0100

    WIP: Move mac pipeline to azurepipelines

--- mac-setup/build-gtk3.sh
+++ mac-setup/build-gtk3.sh
@@ -1,13 +1,11 @@
-export PATH="$HOME/.new_local/bin:$PATH"
-
 # missing six python lib:
 # after first failure download, unpack and install with
 # /Users/yourname/gtk/inst/bin/python setup.py install
 
-curl https://gitlab.gnome.org/GNOME/gtk-osx/raw/master/gtk-osx-setup.sh -o gtk-osx-setup.sh
+curl -L -O https://gitlab.gnome.org/GNOME/gtk-osx/raw/master/gtk-osx-setup.sh
 chmod +x gtk-osx-setup.sh
-./gtk-osx-setup.sh
 
-jhbuild bootstrap
-jhbuild bootstrap-gtk-osx
-jhbuild build python meta-gtk-osx-bootstrap meta-gtk-osx-gtk3
+./gtk-osx-setup.sh
+export PATH="$HOME/.new_local/bin:$PATH"
+jhbuild run bootstrap-gtk-osx
+jhbuild run build meta-gtk-osx-bootstrap meta-gtk-osx-gtk3
