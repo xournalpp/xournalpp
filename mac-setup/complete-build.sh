12c29c80764517e88c86b7202912a842e8be7676
Febbe <fabian_kessler@gmx.de>
2021-03-01 21:45:38 +0100

    Update scrpts

--- mac-setup/complete-build.sh
+++ mac-setup/complete-build.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash -x
+export PATH="$HOME/.new_local/bin:$PATH"
 
 # Build for macOS, without GTK and without Poppler
 
-if [ $# -eq 0 ]
-then
+if [ $# -eq 0 ]; then
   echo 'Please provide the path of your gtk installation'
   exit 1
 fi
@@ -11,10 +11,10 @@ fi
 export PATH="$HOME/.local/bin:$1/inst/bin:$PATH"
 
 mkdir -p ../build
-cd ../build/
+cd ../build/ || exit
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$1/inst ..
 make -j 4
 make install
 
-cd ../mac-setup/
-./build-app.sh $1
+cd ../mac-setup/ || exit
+./build-app.sh "$1"
