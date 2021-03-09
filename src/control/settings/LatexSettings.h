a7b755881770a47ff309b6734cf024c6f24949c1
DUOLabs <dvdugo333@gmail.com>
2021-03-09 00:22:13 -0500

    Fixed hang error for LaTeX preview

--- src/control/settings/LatexSettings.h
+++ src/control/settings/LatexSettings.h
@@ -19,5 +19,5 @@ class LatexSettings {
 public:
     bool autoCheckDependencies{true};
     fs::path globalTemplatePath{};
-    std::string genCmd{"pdflatex -interaction=nonstopmode '{}'"};
+    std::string genCmd{"pdflatex -halt-on-error -interaction=nonstopmode '{}'"};
 };
