2933f97ae2e2bedcf73f1f81fd5800fa5cd2616a
rolandlo <roland_loetscher@hotmail.com>
2021-04-05 15:24:35 +0200

    update build-gtk3 script

--- mac-setup/build-gtk3.sh
+++ mac-setup/build-gtk3.sh
@@ -3,9 +3,11 @@
 # after first failure download, unpack and install with
 # /Users/yourname/gtk/inst/bin/python setup.py install
 export PATH="$HOME/.new_local/bin:$PATH"
+export CFLAGS=-Wno-implicit-function-declaration $CFLAGS
 
 curl -L -O https://gitlab.gnome.org/GNOME/gtk-osx/raw/master/gtk-osx-setup.sh
 chmod +x gtk-osx-setup.sh
 ./gtk-osx-setup.sh
 jhbuild bootstrap-gtk-osx
+jhbuild build python3
 jhbuild build meta-gtk-osx-bootstrap meta-gtk-osx-gtk3
