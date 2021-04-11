0a89ed2c4a25af9174b3e524d19a50c71dea5a58
Michael J Gruber <github@grubix.eu>
2021-04-11 21:52:33 +0200

    allow position highlight without cursor
    When the "none" cursor type was introduced in 540ef0df ("Support "no
    icon" stylus cursor type #2093", 2020-07-11) it turned off the position
    highlighter (as well as the settings) for this mode because of the
    intended use case: writing on a "tablet screen" where the physical
    stylus indicates the cursor position (without distraction)
    
    There is no technical reason, though, to couple the position highlight
    to the cursor type. In addition, the position highlight can serve as a
    hinting tool ("laser pointer") when sharing the xournalpp screen, at
    least until we have a dedicated tool for that.
    
    So, decouple the settings, which still allows the full distraction free
    mode but also pure highlight mode.

--- src/core/gui/XournalppCursor.cpp
+++ src/core/gui/XournalppCursor.cpp
@@ -399,7 +399,8 @@ auto XournalppCursor::getHighlighterCursor() -> GdkCursor* {
 
 
 auto XournalppCursor::getPenCursor() -> GdkCursor* {
-    if (control->getSettings()->getStylusCursorType() == STYLUS_CURSOR_NONE) {
+    if ((control->getSettings()->getStylusCursorType() == STYLUS_CURSOR_NONE) &&
+        !control->getSettings()->isHighlightPosition()) {
         setCursor(CRSR_BLANK_CURSOR);
         return nullptr;
     }
@@ -407,7 +408,7 @@ auto XournalppCursor::getPenCursor() -> GdkCursor* {
         setCursor(CRSR_ARROW);
         return nullptr;
     }
-    if (this->drawDirActive) {
+    if ((control->getSettings()->getStylusCursorType() != STYLUS_CURSOR_NONE) && this->drawDirActive) {
         return createCustomDrawDirCursor(48, this->drawDirShift, this->drawDirCtrl);
     }
 
@@ -417,22 +418,24 @@ auto XournalppCursor::getPenCursor() -> GdkCursor* {
 auto XournalppCursor::createHighlighterOrPenCursor(int size, double alpha) -> GdkCursor* {
     auto irgb = control->getToolHandler()->getColor();
     auto drgb = Util::rgb_to_GdkRGBA(irgb);
-    bool big = control->getSettings()->getStylusCursorType() == STYLUS_CURSOR_BIG;
+    auto cursorType = control->getSettings()->getStylusCursorType();
+    auto cursor = (cursorType == STYLUS_CURSOR_NONE) ? CRSR_BLANK_CURSOR : CRSR_PENORHIGHLIGHTER;
     bool bright = control->getSettings()->isHighlightPosition();
     int height = size;
     int width = size;
 
     // create a hash of variables so we notice if one changes despite being the same cursor type:
-    gulong flavour = (big ? 1U : 0U) | (bright ? 2U : 0U) | static_cast<gulong>(64 * alpha) << 2U |
-                     static_cast<gulong>(size) << 9U | static_cast<gulong>(uint32_t(irgb)) << 14U;
+    gulong flavour = (cursorType == STYLUS_CURSOR_DOT ? 1U : 0U) | (cursorType == STYLUS_CURSOR_BIG ? 2U : 0U) |
+                     (bright ? 4U : 0U) | static_cast<gulong>(64 * alpha) << 3U | static_cast<gulong>(size) << 10U |
+                     static_cast<gulong>(uint32_t(irgb)) << 15U;
 
-    if (CRSR_PENORHIGHLIGHTER == this->currentCursor && flavour == this->currentCursorFlavour) {
+    if ((cursor == this->currentCursor) && (flavour == this->currentCursorFlavour)) {
         return nullptr;
     }
-    this->currentCursor = CRSR_PENORHIGHLIGHTER;
+    this->currentCursor = cursor;
     this->currentCursorFlavour = flavour;
 
-    if (big || bright) {
+    if ((cursorType == STYLUS_CURSOR_BIG) || bright) {
         height = width = 90;
     }
 
@@ -444,7 +447,7 @@ auto XournalppCursor::createHighlighterOrPenCursor(int size, double alpha) -> Gd
     cairo_surface_t* crCursor = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
     cairo_t* cr = cairo_create(crCursor);
 
-    if (big) {
+    if (cursorType == STYLUS_CURSOR_BIG) {
         // When using highlighter, paint the icon with the current color
         if (size == 5) {
             gdk_cairo_set_source_rgba(cr, &drgb);
@@ -481,19 +484,22 @@ auto XournalppCursor::createHighlighterOrPenCursor(int size, double alpha) -> Gd
         cairo_stroke(cr);
     }
 
-    auto drgbCopy = drgb;
-    drgbCopy.alpha = alpha;
-    gdk_cairo_set_source_rgba(cr, &drgbCopy);
-    double cursorSize = control->getToolHandler()->getThickness() * control->getZoomControl()->getZoom();
-    cairo_arc(cr, centerX, centerY, cursorSize / 2., 0, 2. * M_PI);
-    cairo_fill(cr);
+    if (cursorType != STYLUS_CURSOR_NONE) {
+        auto drgbCopy = drgb;
+        drgbCopy.alpha = alpha;
+        gdk_cairo_set_source_rgba(cr, &drgbCopy);
+        double cursorSize = control->getToolHandler()->getThickness() * control->getZoomControl()->getZoom();
+        cairo_arc(cr, centerX, centerY, cursorSize / 2., 0, 2. * M_PI);
+        cairo_fill(cr);
+    }
+
     cairo_destroy(cr);
     GdkPixbuf* pixbuf = xoj_pixbuf_get_from_surface(crCursor, 0, 0, width, height);
     cairo_surface_destroy(crCursor);
-    GdkCursor* cursor = gdk_cursor_new_from_pixbuf(
+    GdkCursor* gdkCursor = gdk_cursor_new_from_pixbuf(
             gtk_widget_get_display(control->getWindow()->getXournal()->getWidget()), pixbuf, centerX, centerY);
     g_object_unref(pixbuf);
-    return cursor;
+    return gdkCursor;
 }
 
 
