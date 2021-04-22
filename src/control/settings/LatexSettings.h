854837846d9139daa889f8abd466be6e9ba00aa7
rolandlo <roland_loetscher@hotmail.com>
2021-04-22 11:17:12 +0200

    specify pdflatex path on MacOS

--- src/control/settings/LatexSettings.h
+++ src/control/settings/LatexSettings.h
@@ -19,5 +19,9 @@ class LatexSettings {
 public:
     bool autoCheckDependencies{true};
     fs::path globalTemplatePath{};
+#ifdef __APPLE__
+    std::string genCmd{"/Library/TeX/texbin/pdflatex -halt-on-error -interaction=nonstopmode '{}'"};
+#else
     std::string genCmd{"pdflatex -halt-on-error -interaction=nonstopmode '{}'"};
+#endif
 };
