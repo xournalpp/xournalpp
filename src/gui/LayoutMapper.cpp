6ec492a512da303cf702f1b43d6d5330724df9c7
Fabian Keßler <fabian_kessler@gmx.de>
2021-07-11 15:49:52 +0200

    Remove std::unordered_map to reduce caching issues / increase performance

--- src/gui/LayoutMapper.cpp
+++ src/gui/LayoutMapper.cpp
@@ -2,6 +2,81 @@
 
 #include <algorithm>
 
+#include "control/settings/Settings.h"
+
+constexpr auto calc_default_grid_pos = [](size_t page_nr, GridPosition grid_size,
+                                          LayoutSettings::Orientation orientation) {
+    return orientation == LayoutSettings::Orientation::Vertical ?
+                   GridPosition{page_nr / grid_size.row, page_nr % grid_size.row} :
+                   GridPosition{page_nr % grid_size.col, page_nr / grid_size.col};
+};
+
+constexpr auto map1 = [](LayoutSettings const& data, size_t page_nr) {
+    GridPosition grid_pos{};
+    auto new_nr = page_nr + static_cast<size_t>(data.offset);
+    if (data.showPairedPages) {
+        auto grid_size = GridPosition{data.cols / 2, data.rows};
+        grid_pos = calc_default_grid_pos(new_nr / 2, grid_size, data.orientation);
+        (grid_pos.col *= 2) += (new_nr % 2);
+    } else {
+        grid_pos = calc_default_grid_pos(new_nr, {data.cols, data.rows}, data.orientation);
+    }
+    decltype(grid_pos) grid_pos2 = {
+            (data.horizontalDir == LayoutSettings::HorizontalDirection::RightToLeft ? data.cols - 1 - grid_pos.col :
+                                                                                      grid_pos.col),
+            (data.verticalDir == LayoutSettings::VerticalDirection::BottomToTop ? data.rows - 1 - grid_pos.row :
+                                                                                  grid_pos.row)};
+    return grid_pos2;
+};
+
+constexpr auto map2 = [](LayoutSettings const& data, GridPosition pos) -> std::optional<size_t> {
+    auto isRightToLeft = [&]() { return data.horizontalDir == LayoutSettings::RightToLeft; };
+    auto isBottomToTop = [&]() { return data.verticalDir == LayoutSettings::BottomToTop; };
+    auto isVertical = [&]() { return data.orientation == LayoutSettings::Vertical; };
+
+    if (isRightToLeft()) {  // reverse x
+        pos.col = data.cols - 1 - pos.col;
+    }
+    if (isBottomToTop()) {  // reverse y
+        pos.row = data.rows - 1 - pos.row;
+    }
+    size_t res{};
+    if (isVertical()) {
+        if (data.showPairedPages) {
+            res = ((pos.row + data.rows * (pos.col / 2)) * 2) + pos.col % 2;
+        } else {
+            res = pos.row + data.rows * pos.col;
+        }
+    } else {  // Horizontal
+        res = pos.col + data.cols * pos.row;
+    }
+    res -= data.offset;
+    if (res >= data.actualPages) {
+        return {};
+    }
+    return res;
+};
+
+// Testers
+
+constexpr LayoutSettings my_data0{1, 91, 92, 1};
+constexpr LayoutSettings my_data1{91, 1, 90, 1};
+constexpr LayoutSettings my_data2{
+        6, 6, 35, 1, true, LayoutSettings::Vertical, LayoutSettings::RightToLeft, LayoutSettings::BottomToTop};
+constexpr GridPosition test_pos0{0, 3};
+constexpr GridPosition test_pos1{3, 0};
+constexpr GridPosition test_pos2{3, 3};
+
+static_assert(map1(my_data0, *map2(my_data0, test_pos0)) == test_pos0);
+static_assert(map1(my_data1, *map2(my_data1, test_pos1)) == test_pos1);
+static_assert(map1(my_data2, *map2(my_data2, test_pos2)) == test_pos2);
+
+template <typename T>
+constexpr auto hash(T&& data_, GridPosition const& pos) {
+    return pos.col + pos.row * data_.cols;
+}
+
+
 /**
  * calculate
  *
@@ -13,7 +88,7 @@
  * @param firstPageOffset  Pages to offset - usually one or zero in order to pair up properly
  */
 
-void calculate(LayoutMapper::internal_data& data, size_t numRows, size_t numCols, bool useRows, int firstPageOffset) {
+void calculate(LayoutSettings& data, size_t numRows, size_t numCols, bool useRows, uint32_t firstPageOffset) {
     if (useRows) {
         data.rows = std::max<size_t>(1UL, numRows);
 
@@ -31,7 +106,7 @@ void calculate(LayoutMapper::internal_data& data, size_t numRows, size_t numCols
     }
 
 
-    if (data.orientation == LayoutMapper::Vertical) {
+    if (data.orientation == LayoutSettings::Vertical) {
         // Vertical Layout
         if (data.showPairedPages) {
             data.offset = firstPageOffset % (2 * data.rows);
@@ -45,20 +120,22 @@ void calculate(LayoutMapper::internal_data& data, size_t numRows, size_t numCols
 }
 
 void LayoutMapper::configureFromSettings(size_t numPages, Settings* settings) {
-    internal_data data;
+    LayoutSettings data;
     // get from user settings:
     data.actualPages = numPages;
     data.showPairedPages = settings->isShowPairedPages();
-    const int pairsOffset = data.showPairedPages ? settings->getPairsOffset() : 0;
+    auto const& pairsOffset = data.showPairedPages ? static_cast<uint32_t>(settings->getPairsOffset()) : 0U;
 
-    const bool fixRows = settings->isPresentationMode() ? false : settings->isViewFixedRows();
-    const size_t numCols = settings->isPresentationMode() ? 1 : settings->getViewColumns();
-    const size_t numRows = settings->isPresentationMode() ? 1 : settings->getViewRows();
+    auto const& isPresentationMode = settings->isPresentationMode();
+    auto const& fixRows = !isPresentationMode && settings->isViewFixedRows();
+    auto const& numCols = size_t(isPresentationMode ? 1 : settings->getViewColumns());
+    auto const& numRows = size_t(isPresentationMode ? 1 : settings->getViewRows());
 
     // assemble bitflags for LayoutType
-    data.orientation = (settings->isPresentationMode() || settings->getViewLayoutVert()) ? Vertical : Horizontal;
-    data.horizontalDir = settings->getViewLayoutR2L() ? RightToLeft : LeftToRight;
-    data.verticalDir = settings->getViewLayoutB2T() ? BottomToTop : TopToBottom;
+    data.orientation = (isPresentationMode || settings->getViewLayoutVert()) ? LayoutSettings::Vertical :
+                                                                               LayoutSettings::Horizontal;
+    data.horizontalDir = settings->getViewLayoutR2L() ? LayoutSettings::RightToLeft : LayoutSettings::LeftToRight;
+    data.verticalDir = settings->getViewLayoutB2T() ? LayoutSettings::BottomToTop : LayoutSettings::TopToBottom;
 
     calculate(data, numRows, numCols, fixRows, pairsOffset);
     if (data == data_) {
@@ -72,16 +149,12 @@ void LayoutMapper::precalculateMappers() {
     this->pageToRaster.clear();
     this->rasterToPage.clear();
     this->pageToRaster.resize(data_.actualPages);
-    this->rasterToPage.reserve(data_.rows * data_.cols);
+    this->rasterToPage.resize(data_.rows * data_.cols, size_t(-1));
 
-    for (size_t col{}; col < data_.cols; ++col) {
-        for (size_t row{}; row < data_.rows; ++row) {
-            auto maybe_index = map(col, row);
-            if (maybe_index) {
-                this->pageToRaster[*maybe_index] = std::pair<size_t, size_t>{col, row};
-                this->rasterToPage.emplace(std::pair<size_t, size_t>{col, row}, *maybe_index);
-            }
-        }
+    for (size_t i = 0; i != data_.actualPages; ++i) {
+        auto&& val = map1(data_, i);
+        this->pageToRaster[i] = val;
+        this->rasterToPage[hash(data_, val)] = i;
     }
 }
 
@@ -89,56 +162,15 @@ auto LayoutMapper::getColumns() const -> size_t { return data_.cols; }
 
 auto LayoutMapper::getRows() const -> size_t { return data_.rows; }
 
-auto LayoutMapper::getFirstPageOffset() const -> int { return data_.offset; }
-
 auto LayoutMapper::isPairedPages() const -> bool { return data_.showPairedPages; }
 
+auto LayoutMapper::at(size_t page) const -> GridPosition { return pageToRaster.at(page); }
 
-// Todo: replace with map<pair(x,y)> -> index and vector<index> -> pair(x,y)
-//       precalculate it in configure
-auto LayoutMapper::map(size_t col, size_t row) const -> std::optional<size_t> {
-    if (isRightToLeft()) {
-        // reverse x
-        col = data_.cols - 1 - col;
+auto LayoutMapper::at(GridPosition const& rasterXY) const -> std::optional<size_t> {
+    auto index = hash(data_, rasterXY);
+    if (index >= rasterToPage.size()) {
+        return std::nullopt;
     }
-
-    if (isBottomToTop()) {
-        // reverse y
-        row = data_.rows - 1 - row;
-    }
-
-    size_t res = 0;
-    if (isVertical()) {
-        if (data_.showPairedPages) {
-            res = ((row + data_.rows * (col / 2)) * 2) + col % 2;
-        } else {
-            res = row + data_.rows * col;
-        }
-    } else  // Horizontal
-    {
-        res = col + data_.cols * row;
-    }
-
-    res -= data_.offset;
-
-    if (res >= data_.actualPages) {
-        return {};
-    }
-
-    return res;
-}
-
-auto LayoutMapper::isVertical() const -> bool { return data_.orientation == Vertical; }
-
-auto LayoutMapper::isBottomToTop() const -> bool { return data_.verticalDir == BottomToTop; }
-
-auto LayoutMapper::isRightToLeft() const -> bool { return data_.horizontalDir == RightToLeft; }
-
-auto LayoutMapper::at(size_t page) const -> std::pair<size_t, size_t> { return pageToRaster.at(page); }
-
-auto LayoutMapper::at(std::pair<size_t, size_t> rasterXY) const -> std::optional<size_t> {
-    if (auto iter = rasterToPage.find(rasterXY); iter != end(rasterToPage)) {
-        return iter->second;
-    }
-    return std::nullopt;
+    auto val = rasterToPage[index];
+    return val >= data_.actualPages ? std::nullopt : std::optional<size_t>(val);  // Maybe optimize this
 }
--- src/gui/LayoutMapper.h
+++ src/gui/LayoutMapper.h
@@ -11,59 +11,67 @@
 
 #pragma once
 
+#include <cstdint>
 #include <optional>
-#include <string>
-#include <unordered_map>
+#include <tuple>
 #include <vector>
 
-#include "control/settings/Settings.h"
-#include "util/hashcombine.h"
+class Settings;
 
+struct GridPosition {
+    size_t col{};
+    size_t row{};
 
-namespace std {
-template <>
-class hash<std::pair<size_t, size_t>> {
-public:
-    size_t operator()(std::pair<size_t, size_t> const& data) const {
-        size_t seed = 0;
-        boost_c::hash_combine(seed, data.first);
-        boost_c::hash_combine(seed, data.second);
-        return seed;
+    constexpr friend bool operator==(GridPosition const& lhs, GridPosition const& rhs) noexcept {
+        return lhs.col == rhs.col && lhs.row == rhs.row;
     }
 };
-}  // namespace std
 
-/**
- * @brief Layout asks this mapper what page ( if any ) should be at a given column,row.
- */
-class LayoutMapper {
-    /**
-     * @brief The Layout of the pages
-     */
+struct LayoutSettings {
+    /// @brief The Layout of the pages
     enum Orientation : bool {
         Horizontal = false,
         Vertical = true,
     };
 
-    /**
-     * Horizontal read direction
-     */
+    /// Horizontal read direction
     enum HorizontalDirection : bool {
         LeftToRight = false,
         RightToLeft = true,
     };
 
-    /**
-     * Vertical read direction
-     */
+    /// Vertical read direction
     enum VerticalDirection : bool {
         TopToBottom = false,
         BottomToTop = true,
     };
 
+    size_t cols = 0;
+    size_t rows = 0;
+    size_t actualPages = 0;
+    uint32_t offset = 0;
+
+    bool showPairedPages = false;
+    Orientation orientation = Vertical;
+    HorizontalDirection horizontalDir = LeftToRight;
+    VerticalDirection verticalDir = TopToBottom;
+
+    constexpr friend auto operator==(LayoutSettings const& lhs, LayoutSettings const& rhs) {
+        auto tier = [](auto&& other) {
+            return std::tie(other.cols, other.rows, other.actualPages, other.offset, other.showPairedPages,
+                            other.orientation, other.horizontalDir, other.verticalDir);
+        };
+        return tier(lhs) == tier(rhs);
+    }
+};
+auto layoutSettings(Settings const*) -> LayoutSettings;
+
+/**
+ * @brief Layout asks this mapper what page ( if any ) should be at a given column,row.
+ */
+class LayoutMapper {
 public:
     LayoutMapper() = default;
-    ~LayoutMapper() = default;
 
     /**
      * configureFromSettings
@@ -75,17 +83,13 @@ public:
 
     void configureFromSettings(size_t numPages, Settings* settings);
 
-    std::pair<size_t, size_t> at(size_t) const;
-    std::optional<size_t> at(std::pair<size_t, size_t>) const;
+    auto at(size_t) const -> GridPosition;
+    auto at(GridPosition const&) const -> std::optional<size_t>;
 
     size_t getColumns() const;
     size_t getRows() const;
-    int getFirstPageOffset() const;
 
     bool isPairedPages() const;
-    bool isRightToLeft() const;
-    bool isBottomToTop() const;
-    bool isVertical() const;
 
 private:
     void precalculateMappers();
@@ -98,33 +102,12 @@ private:
      *
      * @return Page index to put at coordinates
      */
-    std::optional<size_t> map(size_t col, size_t row) const;
 
-private:
-    struct internal_data {
-        size_t cols = 0;
-        size_t rows = 0;
-        size_t actualPages = 0;
+public:
+    LayoutSettings data_;
 
-        int offset = 0;
+    friend void calculate(LayoutSettings& data, size_t numRows, size_t numCols, bool useRows, uint32_t firstPageOffset);
 
-        bool showPairedPages = false;
-        Orientation orientation = Vertical;
-        HorizontalDirection horizontalDir = LeftToRight;
-        VerticalDirection verticalDir = TopToBottom;
-
-        bool operator==(internal_data const& other) {
-            return std::tie(this->cols, this->rows, this->actualPages, this->offset, this->showPairedPages,
-                            this->orientation, this->horizontalDir, this->verticalDir) ==
-                   std::tie(other.cols, other.rows, other.actualPages, other.offset, other.showPairedPages,
-                            other.orientation, other.horizontalDir, other.verticalDir);
-        }
-    } data_;
-
-    std::vector<std::pair<size_t, size_t>> pageToRaster;
-    std::unordered_map<std::pair<size_t, size_t>, size_t> rasterToPage;
-
-    friend void calculate(LayoutMapper::internal_data& data, size_t numRows, size_t numCols, bool useRows,
-                          int firstPageOffset);
-    friend class std::hash<LayoutMapper::internal_data>;
+    std::vector<GridPosition> pageToRaster;
+    std::vector<size_t> rasterToPage;  // access with x*y_max+y+
 };
