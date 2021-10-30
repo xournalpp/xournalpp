f355a88be04edec09bf8616df4d2341d70d2d6ca
Benjamin Hennion <benjamin.hennion@wanadoo.fr>
2021-10-30 22:24:21 +0200

    Remove unused constants in ShapeRecognizerConfig

--- src/core/control/shaperecognizer/ShapeRecognizerConfig.h
+++ src/core/control/shaperecognizer/ShapeRecognizerConfig.h
@@ -22,15 +22,6 @@
 #define RECTANGLE_ANGLE_TOLERANCE (15 * M_PI / 180)  // angle tolerance in rectangles
 #define RECTANGLE_LINEAR_TOLERANCE 0.20              // vertex gap tolerance in rectangles
 #define POLYGON_LINEAR_TOLERANCE 0.20                // vertex gap tolerance in closed polygons
-#define ARROW_MAXSIZE 0.8                            // max size of arrow tip relative to main segment
-#define ARROW_ANGLE_MIN (5 * M_PI / 180)             // arrow tip angles relative to main segment
-#define ARROW_ANGLE_MAX (50 * M_PI / 180)
-#define ARROW_ASYMMETRY_MAX_ANGLE (30 * M_PI / 180)
-#define ARROW_ASYMMETRY_MAX_LINEAR 1.0     // size imbalance of two legs of tip
-#define ARROW_TIP_LINEAR_TOLERANCE 0.30    // gap tolerance on tip segments
-#define ARROW_SIDEWAYS_GAP_TOLERANCE 0.25  // gap tolerance in lateral direction
-#define ARROW_MAIN_LINEAR_GAP_MIN -0.3     // gap tolerance on main segment
-#define ARROW_MAIN_LINEAR_GAP_MAX +0.7     // gap tolerance on main segment
 
 
 #ifdef DEBUG_RECOGNIZER
