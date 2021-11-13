f4cedf423f18cca4c440507b67f4a48b2ff150e5
rolandlo <roland_loetscher@hotmail.com>
2021-11-13 18:17:35 +0100

    remove API testing from example plugin

--- plugins/Example/main.lua
+++ plugins/Example/main.lua
@@ -17,40 +17,4 @@ end
 function exampleCallback()
   result = app.msgbox("Test123", {[1] = "Yes", [2] = "No"});
   print("result = " .. result)
-
-  toolInfo = app.getToolInfo("active")
-  print("active tool: " .. toolInfo["type"])
-  print("active size (name): " .. toolInfo["size"]["name"])
-  print("active size (value): " .. toolInfo["size"]["value"])
-  print("active color: " .. string.format("0x%06x", toolInfo["color"]))
-  print("active fill opacity: " .. tostring(toolInfo["fillOpacity"]))
-  print("active drawing type: " .. toolInfo["drawingType"])
-  print("active line style: " .. toolInfo["lineStyle"])
-
-  penInfo = app.getToolInfo("pen")
-  print("pen size (name): " .. penInfo["size"]["name"])
-  print("pen size (value): " .. penInfo["size"]["value"])
-  print("pen filled: " .. tostring(penInfo["filled"]))
-  print("pen fill opacity: " .. tostring(penInfo["fillOpacity"]))
-  print("pen color: " .. string.format("0x%06x", penInfo["color"]))
-  print("pen drawing type: " .. penInfo["drawingType"])
-  print("pen line style: " .. penInfo["lineStyle"])
-
-  highlighterInfo = app.getToolInfo("highlighter")
-  print("highlighter size (name): " .. highlighterInfo["size"]["name"])
-  print("highlighter size (value): " .. highlighterInfo["size"]["value"])
-  print("highlighter filled: " .. tostring(highlighterInfo["filled"]))
-  print("highlighter fill opacity: " .. tostring(highlighterInfo["fillOpacity"]))
-  print("highlighter color: " .. string.format("0x%06x", highlighterInfo["color"]))
-  print("highlighter drawing type: " .. highlighterInfo["drawingType"])
-
-  eraserInfo = app.getToolInfo("eraser")
-  print("eraser type: " .. eraserInfo["type"])
-  print("eraser size (name): " .. tostring(eraserInfo["size"]["name"]))
-  print("eraser size (value): " .. tostring(eraserInfo["size"]["value"]))
-
-  textInfo = app.getToolInfo("text")
-  print("text font (name): " .. textInfo["font"]["name"])
-  print("text font (size): " .. tostring(textInfo["font"]["size"]))
-  print("text color: " .. string.format("0x%06x", textInfo["color"]))
 end
