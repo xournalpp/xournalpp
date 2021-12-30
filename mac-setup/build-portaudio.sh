f5e69770a99f866d32b142a0e65e94422c5212eb
Roland Lötscher <roland_loetscher@hotmail.com>
2021-12-30 20:16:16 +0100

    update macos build scripts

--- mac-setup/build-portaudio.sh
+++ mac-setup/build-portaudio.sh
@@ -4,7 +4,7 @@ export LIBRARY_PATH="$HOME/gtk/inst/lib:$LIBRARY_PATH"
 # go to script directory
 cd "${0%/*}" || exit
 
-curl -L http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz -o pa_stable_v190600_20161030.tgz
+curl -L http://files.portaudio.com/archives/pa_stable_v190700_20210406.tgz -o pa_stable_v190700_20210406.tgz
 tar xzf pa_stable_*.tgz
 
 cd portaudio || exit
