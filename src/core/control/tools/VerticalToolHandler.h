baefd26e1c2c2e0d2bf29c0329e9c10fbfb964ce
Bryan Tan <techniux@gmail.com>
2022-02-07 22:13:53 -0800

    Vertical spacing: move elements above line when holding Ctrl

--- src/core/control/tools/VerticalToolHandler.h
+++ src/core/control/tools/VerticalToolHandler.h
@@ -26,7 +26,12 @@
 
 class VerticalToolHandler: public ElementContainer {
 public:
-    VerticalToolHandler(Redrawable* view, const PageRef& page, Settings* settings, double y, double zoom);
+    /**
+     * @param initiallyReverse Set this to true if the user has the reverse mode
+     * button (e.g., Ctrl) held down when a vertical selection is started.
+     */
+    VerticalToolHandler(Redrawable* view, const PageRef& page, Settings* settings, double y, bool initiallyReverse,
+                        double zoom);
     ~VerticalToolHandler() override;
     VerticalToolHandler(VerticalToolHandler&) = delete;
     VerticalToolHandler& operator=(VerticalToolHandler&) = delete;
@@ -34,22 +39,67 @@ public:
     VerticalToolHandler&& operator=(VerticalToolHandler&&) = delete;
 
     void paint(cairo_t* cr, GdkRectangle* rect, double zoom);
+
+    /** Update the tool state with the new spacing position */
     void currentPos(double x, double y);
+
+    bool onKeyPressEvent(GdkEventKey* event);
+    bool onKeyReleaseEvent(GdkEventKey* event);
 
     std::unique_ptr<MoveUndoAction> finalize();
 
     std::vector<Element*>* getElements() override;
 
 private:
+    enum class Side {
+        /** elements above the reference line */
+        Above = -1,
+        /** elements below the reference line */
+        Below = 1,
+    };
+
+    /**
+     * Clear the currently moved elements, and then select all elements
+     * above/below startY (depending on the side) to use for the spacing.
+     * Lastly, redraw the elements to the buffer.
+     */
+    void adoptElements(Side side);
+
+    /**
+     * Recreate the buffer if the new zoom value is higher.
+     */
+    void updateZoom(double newZoom);
+
+    /**
+     * Clear the buffer and redraw the elements being spaced.
+     */
+    void redrawBuffer();
+
+    GdkWindow* window;
     Redrawable* view = nullptr;
     PageRef page;
     Layer* layer = nullptr;
     std::vector<Element*> elements;
 
+    /**
+     * Image buffer containing a rendering of the elements being spaced. This
+     * buffer is rendered below the endY if direction is below, or above the
+     * endY if the direction is above.
+     */
     cairo_surface_t* crBuffer = nullptr;
 
     double startY = 0;
     double endY = 0;
+
+    /**
+     * Indicates whether to move elements above (negative) or below (positive) the anchor line.
+     */
+    Side spacingSide;
+
+    /**
+     * Current zoom level.
+     */
+    double zoom;  // TODO: listen to zoom change in case zoom is changed during spacing
 
     /**
      * The handler for snapping points
