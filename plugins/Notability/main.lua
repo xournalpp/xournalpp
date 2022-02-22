62b8868a4b367659262df57d872dab24e4512ac9
rolandlo <roland_loetscher@hotmail.com>
2022-02-22 21:54:30 +0100

    take care of missing keys

--- plugins/Notability/main.lua
+++ plugins/Notability/main.lua
@@ -15,17 +15,17 @@ function getValue(keySeq, startIndex)
   for i=1, #keySeq do
     local key = keySeq[i]
     if type(key) == "string" then
-      id = parsedContents["$objects"][id+1][key]["CF$UID"]
+      tmp = parsedContents["$objects"][id+1][key]
     elseif type(key) == "number" then
       tmp = parsedContents["$objects"][id+1]["NS.objects"][key]
-      if tmp ~= nil then
-        id = tmp["CF$UID"]
-      else 
-        return nil
-      end
     else 
       print("type of key " .. key .. " is not number and not string")
       return nil -- not supported
+    end
+    if tmp ~= nil then
+      id = tmp["CF$UID"]
+    else 
+      return nil
     end
   end
   return parsedContents["$objects"][id+1]
@@ -34,6 +34,10 @@ end
 function getData(str, fmt)
   print("Accessing " .. str)
   local data = getValue({"richText", "Handwriting Overlay", "SpatialHash"})[str]
+  if data == nil then
+    print("the key " .. str .. " was not found") 
+    return {}
+  end
   data = string.gsub(data, "%s+", "") -- remove white spaces
   return dec(data, fmt)
 end
@@ -46,7 +50,9 @@ function extract(notePath, filename)
   local plistPath = "'" .. dirname .. "/" .. filename .. "'"
   print(plistPath)
 
-  local runCommand = assert(io.popen("unzip -p " .. notePath .. " " .. plistPath .. " | plistutil"))
+  local cmd = "unzip -p " .. notePath .. " " .. plistPath .. " | plistutil"
+  print(cmd)
+  local runCommand = assert(io.popen(cmd))
   local result = runCommand:read('*all')
   runCommand:close()
   return result
@@ -67,7 +73,7 @@ function import()
   local coords = getData("curvespoints", "f")
   local widths = getData("curveswidth", "f")
   local pressures = getData("curvesfractionalwidths", "f")
-  local styles = getData("curvesstyles", "b")
+  local styles = getData("curvesstyles", "b") -- can be {}
   local cols = getData("curvescolors", "u")
 
   -- post-process styles and cols
@@ -137,6 +143,7 @@ function import()
   end
 
   local paperSize = getValue({"NBNoteTakingSessionDocumentPaperLayoutModelKey", "documentPaperAttributes", "paperSize"})
+  paperSize = paperSize ~= nil and paperSize or "a4"
   print(paperSize)
   paperWidth = paperSizeDict[paperSize]["width"]
   paperHeight = paperSizeDict[paperSize]["height"]
@@ -148,25 +155,27 @@ function import()
     pageOffset[i] = math.floor(strokes[i]["y"][1] // paperHeight)
     print("Page offset of stroke " .. i .. " is " .. pageOffset[i])
   end
-  maxOffset = math.max(table.unpack(pageOffset))
-  print("Max offset = " .. maxOffset)
-  for j = 0, maxOffset do
-    strokesOfCurrentPage = {}
-    for k = 1, #strokes do
-      if pageOffset[k] == j then
-        newStroke = strokes[k]
-        for l = 1, #newStroke["y"] do 
-          newStroke["y"][l] = newStroke["y"][l] - pageOffset[k]*paperHeight
+  if #pageOffset>0 then
+    maxOffset = math.max(table.unpack(pageOffset))
+    print("Max offset = " .. maxOffset)
+    for j = 0, maxOffset do
+      strokesOfCurrentPage = {}
+      for k = 1, #strokes do
+        if pageOffset[k] == j then
+          newStroke = strokes[k]
+          for l = 1, #newStroke["y"] do 
+            newStroke["y"][l] = newStroke["y"][l] - pageOffset[k]*paperHeight
+          end
+          table.insert(strokesOfCurrentPage, newStroke)
+          print("Adding new stroke to page with offset " .. j)
         end
-        table.insert(strokesOfCurrentPage, newStroke)
-        print("Adding new stroke to page with offset " .. j)
       end
+      if #strokesOfCurrentPage > 0 then
+        app.addStrokes({strokes = strokesOfCurrentPage})
+        app.refreshPage()
+      end
+      if j < maxOffset then app.uiAction({action = "ACTION_NEW_PAGE_AFTER"}) end
     end
-    if #strokesOfCurrentPage > 0 then
-      app.addStrokes({strokes = strokesOfCurrentPage})
-      app.refreshPage()
-    end
-    if j < maxOffset then app.uiAction({action = "ACTION_NEW_PAGE_AFTER"}) end
   end
 
   print("Text in document: ")
@@ -174,4 +183,7 @@ function import()
   print(rawText)
   local pdfFileName = getValue({"richText", "pdfFiles", 1, "pdfFileName"})
   print(pdfFileName)
+  if pdfFileName then 
+    pdfFilePath = app.saveAs(pdfFileName)
+  end
 end
