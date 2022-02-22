8dd4b2c78abb753eb7b3430d3f50eb4266c7c6b9
rolandlo <roland_loetscher@hotmail.com>
2022-02-22 09:26:42 +0100

    support Page Size and multiple pages

--- plugins/Notability/main.lua
+++ plugins/Notability/main.lua
@@ -2,6 +2,14 @@ function initUi()
   app.registerUi({["menu"] = "Import .note file", ["callback"] = "import"});
 end
 
+local paperSizeDict = {
+  letter = {width = 611.86, height = 791.82},
+  legal = {width = 611.86, height = 1007.77},
+  a3 = {width = 841.70, height = 1190.28},
+  a4 = {width = 595.14, height = 841.70},
+  a5 = {width = 420.85, height = 595.14}
+}
+
 function getValue(keySeq, startIndex)
   local id = startIndex == nil and 1 or startIndex  -- Lua list index is always one bigger, since list indices start from 1
   for i=1, #keySeq do
@@ -30,16 +38,12 @@ function getData(str, fmt)
   return dec(data, fmt)
 end
 
-function extract()
-  local notePath = app.getFilePath({'*.note'})
-  notePath = "'" .. notePath .. "'" -- take care of spaces in file name
-  print(notePath)
-
+function extract(notePath, filename)
   local f = assert(io.popen("unzip -Z -1 " .. notePath .. " | head -1 | cut -d'/' -f1"))
   local dirname = assert(f:read('*a'))
   f:close()
   dirname = dirname:gsub('%\n', '')  --remove newline
-  local plistPath = "'" .. dirname .. "/Session.plist'"
+  local plistPath = "'" .. dirname .. "/" .. filename .. "'"
   print(plistPath)
 
   local runCommand = assert(io.popen("unzip -p " .. notePath .. " " .. plistPath .. " | plistutil"))
@@ -52,7 +56,11 @@ function import()
   require("plist")     -- for reading the plist in xml format to a Lua table
   require("base64")    -- for base64 decription
 
-  local contents = extract()
+  local notePath = app.getFilePath({'*.note'})
+  notePath = "'" .. notePath .. "'" -- take care of spaces in file name
+  print(notePath)
+
+  local contents = extract(notePath, "Session.plist")
   parsedContents = plistParse(contents)
 
   local numpoints = getData("curvesnumpoints", "u")
@@ -116,8 +124,50 @@ function import()
     table.insert(strokes, stroke)
   end
 
-  app.addStrokes({strokes = strokes})
-  app.refreshPage()
+  local docStructure = app.getDocumentStructure()
+  local currentPage = docStructure["currentPage"]  
+  local isAnnotated = docStructure["pages"][currentPage]["isAnnotated"]
+  local bgCurrentPage = docStructure["pages"][currentPage]["pageTypeFormat"]
+  local hasPdfBackground = (bgNextPage == ":pdf") 
+
+  if isAnnotated or hasPdfBackground then
+    app.uiAction({action = "ACTION_NEW_PAGE_AFTER"})
+    currentPage = currentPage + 1
+    app.changeCurrentPageBackground("plain")
+  end
+
+  local paperSize = getValue({"NBNoteTakingSessionDocumentPaperLayoutModelKey", "documentPaperAttributes", "paperSize"})
+  print(paperSize)
+  paperWidth = paperSizeDict[paperSize]["width"]
+  paperHeight = paperSizeDict[paperSize]["height"]
+  app.setPageSize(paperWidth, paperHeight)
+
+  numPages = 0
+  pageOffset = {}
+  for i=1, #strokes do
+    pageOffset[i] = math.floor(strokes[i]["y"][1] // paperHeight)
+    print("Page offset of stroke " .. i .. " is " .. pageOffset[i])
+  end
+  maxOffset = math.max(table.unpack(pageOffset))
+  print("Max offset = " .. maxOffset)
+  for j = 0, maxOffset do
+    strokesOfCurrentPage = {}
+    for k = 1, #strokes do
+      if pageOffset[k] == j then
+        newStroke = strokes[k]
+        for l = 1, #newStroke["y"] do 
+          newStroke["y"][l] = newStroke["y"][l] - pageOffset[k]*paperHeight
+        end
+        table.insert(strokesOfCurrentPage, newStroke)
+        print("Adding new stroke to page with offset " .. j)
+      end
+    end
+    if #strokesOfCurrentPage > 0 then
+      app.addStrokes({strokes = strokesOfCurrentPage})
+      app.refreshPage()
+    end
+    if j < maxOffset then app.uiAction({action = "ACTION_NEW_PAGE_AFTER"}) end
+  end
 
   print("Text in document: ")
   local rawText = getValue({"richText", "attributedString", 1})
