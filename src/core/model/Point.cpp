a0db651d3dbaa1f3cebfd54632929027b1190fde
Benjamin Hennion <benjamin.hennion@wanadoo.fr>
2022-04-03 11:24:20 +0200

    Add Point::isInside and Point::relativeLineTo

--- src/core/model/Point.cpp
+++ src/core/model/Point.cpp
@@ -8,18 +8,14 @@ Point::Point(double x, double y, double z): x(x), y(y), z(z) {}
 
 auto Point::lineLengthTo(const Point& p) const -> double { return std::hypot(this->x - p.x, this->y - p.y); }
 
-auto Point::lineTo(const Point& p, double length) const -> Point {
-    double factor = lineLengthTo(p);
-    factor = length / factor;
+auto Point::lineTo(const Point& p, double length) const -> Point { return relativeLineTo(p, length / lineLengthTo(p)); }
 
-    double x = p.x - this->x;
-    double y = p.y - this->y;
-    x *= factor;
-    y *= factor;
-    x += this->x;
-    y += this->y;
-
-    return Point(x, y);
+auto Point::relativeLineTo(const Point& p, const double ratio) const -> Point {
+    return Point(x + ratio * (p.x - x), y + ratio * (p.y - y), z + ratio * (p.z - z));
 }
 
 auto Point::equalsPos(const Point& p) const -> bool { return this->x == p.x && this->y == p.y; }
+
+auto Point::isInside(const xoj::util::Rectangle<double>& rect) const -> bool {
+    return x >= rect.x && x <= rect.x + rect.width && y >= rect.y && y <= rect.y + rect.height;
+}
