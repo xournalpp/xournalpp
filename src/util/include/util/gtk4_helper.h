f5907903442e89883f1f84dc95b69c4feba7cb6f
Febbe <fabian_kessler@gmx.de>
2022-04-25 12:03:15 +0200

    Added transition header It defines replacement functions for gtk3-gtk4

---
+++ src/util/include/util/gtk4_helper.h
@@ -0,0 +1,23 @@
+/*
+ * Xournal++
+ *
+ * header for missing gtk4 functions (part of the gtk4 port)
+ * will be removed later
+ *
+ * @author Xournal++ Team
+ * https://github.com/xournalpp/xournalpp
+ *
+ * @license GNU GPLv2 or later
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+
+void gtk_box_append(GtkBox* box, GtkWidget* child) {
+    constexpr auto default_expand = false;
+    gtk_box_pack_start(GTK_BOX(box), child, default_expand, true, 0);
+}
+
+void gtk_box_remove(GtkBox* box, GtkWidget* child) { gtk_container_remove(GTK_CONTAINER(box), child); }
