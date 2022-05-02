xournalpp/xournalpp

commit 38258a498f9edfc8cea0e842da745dcaa0fa7505

Fix TextEditor repaint bounds
The text tool triggers a whole page repaint on every key press, which is
extremely inefficient. This commit changes the repaint to use the bounds of
the underlying Text element (and fixes a memory management-related segfault
there).

src/gui/TextEditor.cpp
@@ -906,7 +906,10 @@ auto TextEditor::blinkCallback(TextEditor* te) -> gint {
     return false;
 }
 
-void TextEditor::repaintEditor() { this->gui->repaintPage(); }
+void TextEditor::repaintEditor() {
+    auto rect = text->boundingRect();
+    this->gui->repaintRect(rect.x, rect.y, rect.width, rect.height);
+}
 
 /**
  * Calculate the UTF-8 Char offset into a byte offset.

src/view/TextView.cpp
@@ -114,6 +114,6 @@ void TextView::calcSize(const Text* t, double& width, double& height) {
     height = (static_cast<double>(h)) / PANGO_SCALE;
     g_object_unref(layout);
 
-    cairo_surface_destroy(surface);
     cairo_destroy(cr);
+    cairo_surface_destroy(surface);
 }

commit 845a2714e3e735d68ded8542727e7d1653fc928c

Fix TextEditor render delay regression
The optimization introduced in `38258a4` incorrectly used repaintRect (draw
buffer to screen) instead of rerenderRect (update buffer), which introduced a
visible delay in the typed characters appearing on the screen.

src/gui/TextEditor.cpp
@@ -908,7 +908,7 @@ auto TextEditor::blinkCallback(TextEditor* te) -> gint {
 
 void TextEditor::repaintEditor() {
     auto rect = text->boundingRect();
-    this->gui->repaintRect(rect.x, rect.y, rect.width, rect.height);
+    this->gui->rerenderRect(rect.x, rect.y, rect.width, rect.height);
 }
 
 /**