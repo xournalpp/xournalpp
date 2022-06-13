90f872dfbc420dfe66fe06bb2ee9bb158f33b09f
Fabian Keßler <fabian_kessler@gmx.de>
2022-06-13 10:58:33 +0200

    Bugfixes in LatexGenerator:  - Fixed memory leak  - Fixed Crash with utf8 paths on windows

--- src/core/control/latex/LatexGenerator.cpp
+++ src/core/control/latex/LatexGenerator.cpp
@@ -11,7 +11,12 @@
 #include "util/PathUtil.h"                   // for getLongPath
 #include "util/PlaceholderString.h"          // for PlaceholderString
 #include "util/i18n.h"                       // for FS, _F
+#include "util/raii/GLibGuards.h"            // for GErrorGuard, GStrvGuard
+#include "util/safe_casts.h"                 // for as_signed
 #include "util/serdesstream.h"               // for serdes_stream
+
+
+using namespace xoj::util;
 
 LatexGenerator::LatexGenerator(const LatexSettings& settings): settings(settings) {}
 
@@ -20,7 +25,7 @@ auto LatexGenerator::templateSub(const std::string& input, const std::string& te
     const static std::regex substRe("%%XPP_((TOOL_INPUT)|(TEXT_COLOR))%%");
     std::string output;
     output.reserve(templ.length());
-    int templatePos = 0;
+    size_t templatePos = 0;
     for (std::sregex_iterator it(templ.begin(), templ.end(), substRe); it != std::sregex_iterator{}; it++) {
         std::smatch match = *it;
         std::string matchStr = match[1];
@@ -35,9 +40,9 @@ auto LatexGenerator::templateSub(const std::string& input, const std::string& te
             s << std::hex << std::setfill('0') << std::setw(6) << std::right << tmp_color;
             repl = s.str();
         }
-        output.append(templ, templatePos, match.position() - templatePos);
+        output.append(templ, templatePos, as_unsigned(match.position()) - templatePos);
         output.append(repl);
-        templatePos = match.position() + match.length();
+        templatePos = as_unsigned(match.position() + match.length());
     }
     output.append(templ, templatePos);
     return output;
@@ -46,32 +51,34 @@ auto LatexGenerator::templateSub(const std::string& input, const std::string& te
 auto LatexGenerator::asyncRun(const fs::path& texDir, const std::string& texFileContents) -> Result {
     std::string cmd = this->settings.genCmd;
     GError* err = nullptr;
-    const auto&& fail = [&](GenError&& res) -> Result {
-        g_error_free(err);
-        return res;
-    };
-
-    auto texFilePath = (Util::getLongPath(texDir) / "tex.tex").string();
-    for (auto i = cmd.find(u8"{}"); i != std::string::npos; i = cmd.find(u8"{}", i + texFilePath.length())) {
-        cmd.replace(i, 2, texFilePath);
+    GErrorGuard guard{&err};
+    std::string texFilePathOSEncoding;
+    try {
+        texFilePathOSEncoding = (Util::getLongPath(texDir) / "tex.tex").string();
+    } catch (fs::filesystem_error const& e) {
+        GenError res{FS(_F("Failed to parse LaTeX generator path: {1}") % e.what())};
     }
+
+    for (auto i = cmd.find(u8"{}"); i != std::string::npos; i = cmd.find(u8"{}", i + texFilePathOSEncoding.length())) {
+        cmd.replace(i, 2, texFilePathOSEncoding);
+    }
+    // Todo (rolandlo): is this a todo?
     // Windows note: g_shell_parse_argv assumes POSIX paths, so Windows paths need to be escaped.
     gchar** argv = nullptr;
+    GStrvGuard guard2{&argv};
     if (!g_shell_parse_argv(cmd.c_str(), nullptr, &argv, &err)) {
-        return fail({FS(_F("Failed to parse LaTeX generator command: {1}") % err->message)});
+        return GenError{FS(_F("Failed to parse LaTeX generator command: {1}") % err->message)};
     }
     gchar* prog = argv[0];
     if (!prog || !(prog = g_find_program_in_path(prog))) {
-        GenError res{FS(_F("Failed to find LaTeX generator program in PATH: {1}") % argv[0])};
-        g_strfreev(argv);
-        g_error_free(err);
-        return res;
+        return GenError{FS(_F("Failed to find LaTeX generator program in PATH: {1}") % argv[0])};
     }
     g_free(argv[0]);
     argv[0] = prog;
 
-    if (!g_file_set_contents(texFilePath.c_str(), texFileContents.c_str(), texFileContents.length(), &err)) {
-        return fail({FS(_F("Could not save .tex file: {1}") % err->message)});
+    if (!g_file_set_contents(texFilePathOSEncoding.c_str(), texFileContents.c_str(), as_signed(texFileContents.size()),
+                             &err)) {
+        return GenError({FS(_F("Could not save .tex file: {1}") % err->message)});
     }
 
     auto flags = static_cast<GSubprocessFlags>(G_SUBPROCESS_FLAGS_STDOUT_PIPE | G_SUBPROCESS_FLAGS_STDERR_MERGE);
@@ -80,13 +87,11 @@ auto LatexGenerator::asyncRun(const fs::path& texDir, const std::string& texFile
     auto* proc = g_subprocess_launcher_spawnv(launcher, argv, &err);
 
     std::string progName(prog);
-    g_strfreev(argv);
     g_object_unref(launcher);
 
     if (proc) {
         return {proc};
-    } else {
-        return fail(
-                {FS(_F("Could not start {1}: {2} (exit code: {3})") % progName.c_str() % err->message % err->code)});
     }
+    return GenError(
+            {FS(_F("Could not start {1}: {2} (exit code: {3})") % progName.c_str() % err->message % err->code)});
 }
