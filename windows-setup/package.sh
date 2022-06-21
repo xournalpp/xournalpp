f3ded0aa9ab51ece06b4eb1f1c4ada294014fff7
rolandlo <roland_loetscher@hotmail.com>
2022-06-21 17:13:01 +0200

    copy gtk-sourceview shared data

--- windows-setup/package.sh
+++ windows-setup/package.sh
@@ -57,6 +57,9 @@ cp -r /mingw64/share/glib-2.0 "$setup_dir"/share/
 echo "copy poppler shared"
 cp -r /mingw64/share/poppler "$setup_dir"/share/
 
+echo "copy gtksourceview shared"
+cp -r /mingw64/share/gtksourceview-4 "$setup_dir"/share
+
 echo "copy gspawn-win64-helper"
 cp /mingw64/bin/gspawn-win64-helper.exe "$setup_dir"/bin
 cp /mingw64/bin/gspawn-win64-helper-console.exe "$setup_dir"/bin
