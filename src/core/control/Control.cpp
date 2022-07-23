d4a9f69128e281b7326cf776179808bbcfae7e64
forgedOctopus <65341804+forgedOctopus@users.noreply.github.com>
2022-07-23 16:49:52 +0200

    Don't change pen line style when setting line style of selection

--- src/core/control/Control.cpp
+++ src/core/control/Control.cpp
@@ -2966,9 +2966,9 @@ void Control::setLineStyle(const string& style) {
 
     if (sel) {
         undoRedo->addUndoAction(sel->setLineStyle(stl));
+    } else {
+        this->toolHandler->setLineStyle(stl);
     }
-
-    this->toolHandler->setLineStyle(stl);
 }
 
 void Control::setToolSize(ToolSize size) {
