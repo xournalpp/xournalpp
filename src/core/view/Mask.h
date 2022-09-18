cd657f2b212ff4275e36dc10caaf98037259f4b3
Benjamin Hennion <benjamin.hennion@wanadoo.fr>
2022-09-18 14:09:49 +0200

    Implement a Mask helper class for cairo surfaces

---
+++ src/core/view/Mask.h
@@ -0,0 +1,48 @@
+/*
+ * Xournal++
+ *
+ * Virtual class for showing overlays (e.g. active tools, selections and so on)
+ *
+ * @author Xournal++ Team
+ * https://github.com/xournalpp/xournalpp
+ *
+ * @license GNU GPLv2 or later
+ */
+
+#pragma once
+
+#include <cairo.h>
+
+#include "util/raii/CairoWrappers.h"
+
+class Range;
+
+namespace xoj::view {
+
+/**
+ * @brief Mask class: a cairo surface and its context to draw on, with the later purpose of blitting or using as a
+ * buffer.
+ */
+class Mask {
+public:
+    Mask() = default;
+    /**
+     * @brief Create a mask tailored for the specified target
+     * @param target A cairo surface similar to that on which the mask will be used
+     * @param extent The extent of the mask, in local coordinates (i.e. in the coordinates of the cairo context(s) on
+     * which the mask will be used).
+     * @param zoom The local zoom ratio (zoom ratio of the cairo context(s) on which the mask will be used).
+     * @param DPIScaling The targeted screen DPI scaling
+     * @param contentType The intended content of the mask
+     */
+    Mask(cairo_surface_t* target, const Range& extent, double zoom, int DPIScaling,
+         cairo_content_t contentType = CAIRO_CONTENT_ALPHA);
+    cairo_t* get();
+    bool isInitialized() const;
+    void blitTo(cairo_t* targetCr) const;
+    void wipe();
+
+private:
+    xoj::util::CairoSPtr cr;
+};
+};  // namespace xoj::view
