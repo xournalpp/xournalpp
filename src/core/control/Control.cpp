xournalpp/xournalpp

commit a08c58270d6a47d45f5a3284fda6690c2c51066f

Auto-select edited text object when switching from text- to select-tool (#4315)
* First functioning version, still TODOs left (remove dulicate + cleanup)

* Implements Issue #1169, autoselecting currently editing text when switching to a selection tool

* code cleanup

* cover edge-case Selection exists n selectTextAndCloseTextTool

* changes selectTextAndCloseTextTool() to more fitting name selectAndEndText()

* code cleanup in selectAndEndText()

* further code cleanup

* refactores selectTool() into handleSelectToolAction()

* resolves some nitpicks

* removes unnecessary include

* Adds mutex lock to document in selectTool()

Co-authored-by: Benjamin Hennion <benjamin.hennion@wanadoo.fr>

* minor fix

Co-authored-by: Timon Ensel <timon.ensel@tum.de>
Co-authored-by: Benjamin Hennion <benjamin.hennion@wanadoo.fr>

src/core/control/Control.cpp
@@ -1781,6 +1781,27 @@ void Control::undoRedoPageChanged(PageRef page) {
 }
 
 void Control::selectTool(ToolType type) {
+    // keep text-selection when switching from text to seletion tool
+    auto oldTool = getToolHandler()->getActiveTool();
+    if (oldTool && win
+                && isSelectToolType(type)
+                && oldTool->getToolType() == ToolType::TOOL_TEXT
+                && !(this->win->getXournal()->getTextEditor()->getText()->getText().empty())) {
+        auto xournal = this->win->getXournal();
+        Text* textobj = xournal->getTextEditor()->getText();
+        clearSelectionEndText();
+
+        auto pageNr = getCurrentPageNo();
+        XojPageView* view = xournal->getViewFor(pageNr);
+        g_assert(view != nullptr);
+        this->doc->lock();
+        PageRef page = this->doc->getPage(pageNr);
+        auto selection = new EditSelection(this->undoRedo, textobj, view, page);
+        this->doc->unlock();
+    
+        xournal->setSelection(selection);
+    }
+
     toolHandler->selectTool(type);
     toolHandler->fireToolChanged();
 

src/core/control/Tool.cpp
@@ -17,6 +17,8 @@ Tool::~Tool() {}
 
 auto Tool::getName() const -> std::string { return this->name; }
 
+auto Tool::getToolType() const -> ToolType { return this->type; }
+
 void Tool::setCapability(unsigned int capability, bool enabled) {
     if (enabled) {
         this->capabilities |= capability;

src/core/control/Tool.h
@@ -41,6 +41,8 @@ public:
 public:
     std::string getName() const;
 
+    ToolType getToolType() const;
+
     bool hasCapability(ToolCapabilities cap) const;
 
     double getThickness(ToolSize size) const;

src/core/control/ToolEnums.cpp
@@ -96,6 +96,12 @@ auto drawingTypeFromString(const std::string& type) -> DrawingType {
     return DRAWING_TYPE_DEFAULT;
 }
 
+auto isSelectToolType(ToolType type) -> bool {
+    return type == TOOL_SELECT_RECT
+            || type == TOOL_SELECT_REGION
+            || type == TOOL_SELECT_OBJECT;
+}
+
 auto toolTypeToString(ToolType type) -> std::string {
     switch (type) {
         case TOOL_NONE:

src/core/control/ToolEnums.h
@@ -78,6 +78,7 @@ enum ToolType {
 
     TOOL_END_ENTRY
 };
+auto isSelectToolType(ToolType type) -> bool;
 
 // The count of tools
 #define TOOL_COUNT (TOOL_END_ENTRY - 1)

src/core/control/ToolHandler.cpp
@@ -199,6 +199,8 @@ void ToolHandler::addToolChangedListener(ToolChangedCallback listener) {
 
 auto ToolHandler::getTool(ToolType type) const -> Tool& { return *(this->tools[type - TOOL_PEN]); }
 
+auto ToolHandler::getActiveTool() const -> Tool* {return this->activeTool; }
+
 auto ToolHandler::getToolType() const -> ToolType {
     Tool* tool = this->activeTool;
     return tool->type;

src/core/control/ToolHandler.h
@@ -239,6 +239,13 @@ public:
     Tool& getTool(ToolType type) const;
 
     /**
+     * @brief Get the active Tool, returns nullptr if no tool is active
+     * 
+     * @return Tool*
+    */
+    Tool* getActiveTool() const;
+
+    /**
      * @brief Set the Eraser Type of the Eraser in the toolbar
      * @note Here the Eraser Tool in the toolbar is changed regardless of the the tool currently selected.
      * This is necessary to allow users to change the Eraser type for their Button Tools while having another tool

commit 97dbd2902506734d90e8f1355952eb05121276fe

Fix segfault when switching from text to selection tool
This fixes a regression introduced by commit a08c582

src/core/control/Control.cpp
@@ -1786,6 +1786,7 @@ void Control::selectTool(ToolType type) {
     if (oldTool && win
                 && isSelectToolType(type)
                 && oldTool->getToolType() == ToolType::TOOL_TEXT
+                && this->win->getXournal()->getTextEditor()
                 && !(this->win->getXournal()->getTextEditor()->getText()->getText().empty())) {
         auto xournal = this->win->getXournal();
         Text* textobj = xournal->getTextEditor()->getText();