0fe46e288f9ec45145abe714a6bfdc4613d77053
Bryan Tan <bryantan@technius.net>
2022-10-29 13:06:35 -0700

    mac: update jhbuild instructions and steps
    - Rewrite the mac build readme with more detailed instructions
    - Use a custom jhbuild moduleset for the build
    - Remove manual dependency build scripts for poppler, portaudio

--- mac-setup/complete-build.sh
+++ mac-setup/complete-build.sh
@@ -1,20 +1,37 @@
-#!/bin/bash -x
-export PATH="$HOME/.new_local/bin:$PATH"
+#!/usr/bin/env bash
 
-# Build for macOS, without GTK and without Poppler
+set -e
+set -o pipefail
 
+# Build and package the application
 if [ $# -eq 0 ]; then
-  echo 'Please provide the path of your gtk installation'
+  echo 'Please provide the path of your gtk prefix parent (example: ~/gtk)'
   exit 1
 fi
 
-export PATH="$HOME/.local/bin:$1/inst/bin:$PATH"
+ENSURE_JHBUILD=
+if [ -z "$UNDER_JHBUILD" ]; then
+    if ! which jhbuild > /dev/null; then
+        echo "Not running jhbuild environment..."
+        echo "Warning: jhbuild not found on PATH! This script may not work as expected."
+    else
+        echo "Not running jhbuild environment, prepending jhbuild to every command"
+        ENSURE_JHBUILD="jhbuild run"
+    fi
+fi
 
-mkdir -p ../build
-cd ../build/ || exit
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$1/inst ..
-make -j 4
-make install
+root_dir=$(python3 -c 'import pathlib, sys; print(pathlib.Path(sys.argv[1]).resolve().parent.parent)' "$0")
+prefix_parent="$1"
+[ ! -d "$prefix_parent" ] && echo "$prefix_parent doesn't exist." && exit 1
+[ ! -d "$prefix_parent"/inst ] && echo "$prefix_parent/inst doesn't exist." && exit 1
 
-cd ../mac-setup/ || exit
-./build-app.sh "$1"
+build_dir="$root_dir"/build
+mkdir -p "$build_dir"
+
+pushd "$build_dir"
+$ENSURE_JHBUILD cmake .. -GNinja
+$ENSURE_JHBUILD cmake --build .
+$ENSURE_JHBUILD cmake --install . --prefix "$prefix_parent"/inst
+popd
+
+$ENSURE_JHBUILD bash ./build-app.sh "$prefix_parent"
