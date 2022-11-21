672fc139f2b15703fec378536a6d0d808d620751
Timon Ensel <timon.ensel@tum.de>
2022-11-21 11:39:33 +0100

    fixes zoom update

--- src/core/control/zoom/ZoomControl.cpp
+++ src/core/control/zoom/ZoomControl.cpp
@@ -229,7 +229,8 @@ void ZoomControl::initZoomHandler(GtkWidget* window, GtkWidget* widget, XournalV
     gtk_widget_add_events(widget, GDK_TOUCHPAD_GESTURE_MASK);
     g_signal_connect(widget, "scroll-event", G_CALLBACK(onScrolledwindowMainScrollEvent), this);
     g_signal_connect(widget, "event", G_CALLBACK(onTouchpadPinchEvent), this);
-    g_signal_connect(window, "configure-event", G_CALLBACK(onWindowSizeChangedEvent), this);
+    g_signal_connect(window, "signal_size_allocate", G_CALLBACK(onWindowSizeChangedEvent), this);
+    g_signal_connect(window, "configure_event", G_CALLBACK(onWindowSizeChangedEvent), this);
     registerListener(this->control);
 }
 
@@ -304,8 +305,8 @@ auto ZoomControl::updateZoomPresentationValue(size_t pageNo) -> bool {
     }
 
     Rectangle widget_rect = getVisibleRect();
-    double zoom_fit_width = widget_rect.width / (page->getWidth() + 14.0);
-    double zoom_fit_height = widget_rect.height / (page->getHeight() + 14.0);
+    double zoom_fit_width = widget_rect.width / page->getWidth();
+    double zoom_fit_height = widget_rect.height / page->getHeight();
     double zoom_presentation = zoom_fit_width < zoom_fit_height ? zoom_fit_width : zoom_fit_height;
     if (zoom_presentation < this->zoomMin) {
         return false;
