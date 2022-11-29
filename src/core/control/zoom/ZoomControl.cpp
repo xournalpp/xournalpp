20aa6e3119aad91027ede0742b7b67ef76cf857e
forgottosave <forgottosave.ensel@tum.de>
2022-11-29 17:29:52 +0100

    fix fitpage similar to presentation page

--- src/core/control/zoom/ZoomControl.cpp
+++ src/core/control/zoom/ZoomControl.cpp
@@ -282,8 +282,8 @@ auto ZoomControl::updateZoomFitValue(size_t pageNo) -> bool {
     }
 
     Rectangle widget_rect = getVisibleRect();
-    double zoom_fit_width = widget_rect.width / (page->getWidth() + 14.0);
-    double zoom_fit_height = widget_rect.height / (page->getHeight() + 14.0);
+    double zoom_fit_width = (widget_rect.width - 14.0) / page->getWidth();
+    double zoom_fit_height = (widget_rect.height - 14.0) / page->getHeight();
     double zoom_fit = zoom_fit_width < zoom_fit_height ? zoom_fit_width : zoom_fit_height;
     
     if (zoom_fit < this->zoomMin || zoom_fit > this->zoomMax) {
