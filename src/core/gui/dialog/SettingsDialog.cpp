1fdb53915a424628395f14598f26756409befdf0
idotobi <post@tobidot.eu>
2023-06-16 13:21:53 +0200

    debug: try out possible cause for core dump

--- src/core/gui/dialog/SettingsDialog.cpp
+++ src/core/gui/dialog/SettingsDialog.cpp
@@ -179,8 +179,10 @@ SettingsDialog::SettingsDialog(GladeSearchpath* gladeSearchPath, Settings* setti
     }
 
     g_message("Adding LatexContainer");
+    g_message("LatexTabBox %s", this->get("latexTabBox"));
+    g_message("Latexpanel %s", &this->latexPanel);
 
-    gtk_container_add(GTK_CONTAINER(this->get("latexTabBox")), this->latexPanel.get("latexSettingsPanel"));
+    // gtk_container_add(GTK_CONTAINER(this->get("latexTabBox")), this->latexPanel.get("latexSettingsPanel"));
 }
 
 SettingsDialog::~SettingsDialog() = default;
