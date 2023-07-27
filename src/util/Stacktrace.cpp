f93aa671d842857482764f5c1435ebb3da3e606a
rolandlo <roland_loetscher@hotmail.com>
2023-07-27 21:46:04 +0200

    fix clang-format

--- src/util/Stacktrace.cpp
+++ src/util/Stacktrace.cpp
@@ -71,10 +71,10 @@ fs::path Stacktrace::getExePath() {
 #else
 auto Stacktrace::getExePath() -> fs::path {
 #ifndef PATH_MAX
-  // This is because PATH_MAX is (per posix) not defined if there is
-  // no limit, e.g., on GNU Hurd. The "right" workaround is to not use
-  // PATH_MAX, instead stat the link and use stat.st_size for
-  // allocating the buffer.
+    // This is because PATH_MAX is (per posix) not defined if there is
+    // no limit, e.g., on GNU Hurd. The "right" workaround is to not use
+    // PATH_MAX, instead stat the link and use stat.st_size for
+    // allocating the buffer.
 #define PATH_MAX 4096
 #endif
     std::array<char, PATH_MAX> result{};
