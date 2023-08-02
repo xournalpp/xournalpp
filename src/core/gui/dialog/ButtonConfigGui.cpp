a2236995350db8644578109c1f0c706a3c590cd0
Lennard Hofmann <lennard.hofmann@web.de>
2023-08-02 10:01:11 +0200

    Add missing double arrow preference
    This also fixes an inconsistency in the Edit → Preferences → Defaults
    dialog where the selected drawing type does not match the effective
    drawing type if something below "Draw Arrow" is selected.

--- src/core/gui/dialog/ButtonConfigGui.cpp
+++ src/core/gui/dialog/ButtonConfigGui.cpp
@@ -134,6 +134,8 @@ ButtonConfigGui::ButtonConfigGui(GladeSearchpath* gladeSearchPath, GtkWidget* w,
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(this->cbDrawingType), _("Draw Ellipse"));
     // DRAWING_TYPE_ARROW
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(this->cbDrawingType), _("Draw Arrow"));
+    // DRAWING_TYPE_DOUBLE_ARROW
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(this->cbDrawingType), _("Draw Double Arrow"));
     // DRAWING_TYPE_COORDINATE_SYSTEM
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(this->cbDrawingType), _("Draw coordinate system"));
     // DRAWING_TYPE_STROKE_RECOGNIZER
