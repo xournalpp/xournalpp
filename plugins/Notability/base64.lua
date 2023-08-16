e357ef187f7c0b3a681db03c410a924cb1fef6d1
rolandlo <roland_loetscher@hotmail.com>
2023-08-16 23:33:01 +0200

    Use "mime" from LuaSocket for base64 decryption

--- plugins/Notability/base64.lua
+++ plugins/Notability/base64.lua
@@ -1,28 +1,31 @@
 #!/usr/bin/env lua
 
--- Inspired from http://lua-users.org/wiki/BaseSixtyFour
-
--- working lua base64 codec (c) 2006-2008 by Alex Kloss
--- compatible with lua 5.1
--- http://www.it-rfc.de
--- licensed under the terms of the LGPL2
-
--- decryption table
-local base64bytes = {['A']=0,['B']=1,['C']=2,['D']=3,['E']=4,['F']=5,['G']=6,['H']=7,['I']=8,['J']=9,['K']=10,['L']=11,['M']=12,['N']=13,['O']=14,['P']=15,['Q']=16,['R']=17,['S']=18,['T']=19,['U']=20,['V']=21,['W']=22,['X']=23,['Y']=24,['Z']=25,['a']=26,['b']=27,['c']=28,['d']=29,['e']=30,['f']=31,['g']=32,['h']=33,['i']=34,['j']=35,['k']=36,['l']=37,['m']=38,['n']=39,['o']=40,['p']=41,['q']=42,['r']=43,['s']=44,['t']=45,['u']=46,['v']=47,['w']=48,['x']=49,['y']=50,['z']=51,['0']=52,['1']=53,['2']=54,['3']=55,['4']=56,['5']=57,['6']=58,['7']=59,['8']=60,['9']=61,['+']=62,['/']=63,['=']=nil}
+local mime = require "mime" -- https://lunarmodules.github.io/luasocket/
 
 function dec(data,fmt)
-	local x = os.clock()
 	local char, byte = string.char, string.byte
-	local pad = 3 - ((#data-1) % 4)
-	local result = (data..string.rep('=', pad)):gsub("....", function(cs)
-		local a = base64bytes[cs:sub(1,1)] or 0
-		local b = base64bytes[cs:sub(2,2)] or 0
-		local c = base64bytes[cs:sub(3,3)] or 0
-		local d = base64bytes[cs:sub(4,4)] or 0
-		return char((a << 2) + (b >> 4)) .. char(((b << 4) & 255) + (c >> 2)) .. char(((c << 6) & 255) + d)
-	 end)
+
+    -- Inspired from http://lua-users.org/wiki/BaseSixtyFour
+
+    -- working lua base64 codec (c) 2006-2008 by Alex Kloss
+    -- compatible with lua 5.1
+    -- http://www.it-rfc.de
+    -- licensed under the terms of the LGPL2
+
+    -- decryption table
+    -- local base64bytes = {['A']=0,['B']=1,['C']=2,['D']=3,['E']=4,['F']=5,['G']=6,['H']=7,['I']=8,['J']=9,['K']=10,['L']=11,['M']=12,['N']=13,['O']=14,['P']=15,['Q']=16,['R']=17,['S']=18,['T']=19,['U']=20,['V']=21,['W']=22,['X']=23,['Y']=24,['Z']=25,['a']=26,['b']=27,['c']=28,['d']=29,['e']=30,['f']=31,['g']=32,['h']=33,['i']=34,['j']=35,['k']=36,['l']=37,['m']=38,['n']=39,['o']=40,['p']=41,['q']=42,['r']=43,['s']=44,['t']=45,['u']=46,['v']=47,['w']=48,['x']=49,['y']=50,['z']=51,['0']=52,['1']=53,['2']=54,['3']=55,['4']=56,['5']=57,['6']=58,['7']=59,['8']=60,['9']=61,['+']=62,['/']=63,['=']=nil}
+	-- local pad = 3 - ((#data-1) % 4)
+	-- local result = (data..string.rep('=', pad)):gsub("....", function(cs)
+	--   local a = base64bytes[cs:sub(1,1)] or 0
+	--   local b = base64bytes[cs:sub(2,2)] or 0
+	--   local c = base64bytes[cs:sub(3,3)] or 0
+	--   local d = base64bytes[cs:sub(4,4)] or 0
+	--   return char((a << 2) + (b >> 4)) .. char(((b << 4) & 255) + (c >> 2)) .. char(((c << 6) & 255) + d)
+	-- end)
+	local x = os.clock()
+	local result = mime.unb64(data)
 	local y = os.clock()
-	print(string.format("elapsed time for string substitution: %.2f\n", y - x))
+	print(string.format("elapsed time for mime.unb64: %.2f\n", y - x))
  
 	converted = {}
 	if fmt == "f" then
