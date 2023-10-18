094ae377b469fce8684081e3eada6ea441cc9d95
Simon Preibisch <simonpreibisch@gmail.com>
2023-10-18 07:59:33 +0200

    added default value for button config to new code

--- src/core/control/settings/SettingsDescription.cpp
+++ src/core/control/settings/SettingsDescription.cpp
@@ -27,7 +27,15 @@ Setting<SettingsElement::SETTING_FONT>::value_type Setting<SettingsElement::SETT
 Setting<SettingsElement::SETTING_DEFAULT_SAVE_NAME>::value_type Setting<SettingsElement::SETTING_DEFAULT_SAVE_NAME>::DEFAULT = _("%F-Note-%H-%M");
 Setting<SettingsElement::SETTING_DEFAULT_PDF_EXPORT_NAME>::value_type Setting<SettingsElement::SETTING_DEFAULT_PDF_EXPORT_NAME>::DEFAULT = _("%{name}_annotated");
 Setting<SettingsElement::SETTING_LATEX_SETTINGS>::value_type Setting<SettingsElement::SETTING_LATEX_SETTINGS>::DEFAULT{};
-Setting<SettingsElement::SETTING_NESTED_BUTTON_CONFIG>::value_type Setting<SettingsElement::SETTING_NESTED_BUTTON_CONFIG>::DEFAULT{};
+Setting<SettingsElement::SETTING_NESTED_BUTTON_CONFIG>::value_type Setting<SettingsElement::SETTING_NESTED_BUTTON_CONFIG>::DEFAULT{
+        std::make_shared<ButtonConfig>(TOOL_ERASER, Colors::black, TOOL_SIZE_NONE, DRAWING_TYPE_DEFAULT, ERASER_TYPE_NONE), // Eraser
+        std::make_shared<ButtonConfig>(TOOL_HAND, Colors::black, TOOL_SIZE_NONE, DRAWING_TYPE_DEFAULT, ERASER_TYPE_NONE), // Middle button
+        std::make_shared<ButtonConfig>(TOOL_NONE, Colors::black, TOOL_SIZE_NONE, DRAWING_TYPE_DEFAULT, ERASER_TYPE_NONE), // Right button
+        std::make_shared<ButtonConfig>(TOOL_NONE, Colors::black, TOOL_SIZE_NONE, DRAWING_TYPE_DEFAULT, ERASER_TYPE_NONE), // Touch
+        std::make_shared<ButtonConfig>(TOOL_PEN, Colors::black, TOOL_SIZE_FINE, DRAWING_TYPE_DEFAULT, ERASER_TYPE_NONE), // Default config
+        std::make_shared<ButtonConfig>(TOOL_NONE, Colors::black, TOOL_SIZE_NONE, DRAWING_TYPE_DEFAULT, ERASER_TYPE_NONE), // Pen button 1
+        std::make_shared<ButtonConfig>(TOOL_NONE, Colors::black, TOOL_SIZE_NONE, DRAWING_TYPE_DEFAULT, ERASER_TYPE_NONE) // Pen button 2
+};
 Setting<SettingsElement::SETTING_NESTED_DEVICE_CLASSES>::value_type Setting<SettingsElement::SETTING_NESTED_DEVICE_CLASSES>::DEFAULT{};
 Setting<SettingsElement::SETTING_NESTED_TOOLS>::value_type Setting<SettingsElement::SETTING_NESTED_TOOLS>::DEFAULT{};
 Setting<SettingsElement::SETTING_NESTED_TOUCH>::value_type Setting<SettingsElement::SETTING_NESTED_TOUCH>::DEFAULT{};
