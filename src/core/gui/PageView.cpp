e1ed102518fe5597caa10fead923ee8fd7ada9d7
jerdoe <92479519+jerdoe@users.noreply.github.com>
2023-10-23 01:11:51 +0400

    Cleanup obsolete include in PageView
    In older versions, the opacity toolbox was hidden when clicking in the
    PageView.
    
    However, the UI/UX design has evolved to hide it when leaving the toolbox.
    
    This commit addresses the outdated code in the PageView,
    removing the now-obsolete include statements.
    
    By doing so, it improves the codebase's clarity and ensures that the commit
    history remains focused on relevant changes.

--- src/core/gui/PageView.cpp
+++ src/core/gui/PageView.cpp
@@ -46,7 +46,6 @@
 #include "control/tools/VerticalToolHandler.h"      // for VerticalToolHandler
 #include "gui/FloatingToolbox.h"                    // for FloatingToolbox
 #include "gui/MainWindow.h"                         // for MainWindow
-#include "gui/OpacityPreviewToolbox.h"
 #include "gui/PdfFloatingToolbox.h"                 // for PdfFloatingToolbox
 #include "gui/SearchBar.h"                          // for SearchBar
 #include "gui/inputdevices/PositionInputData.h"     // for PositionInputData
@@ -419,6 +418,7 @@ auto XojPageView::onButtonClickEvent(const PositionInputData& pos) -> bool {
     if (h->getToolType() == TOOL_FLOATING_TOOLBOX) {
         this->showFloatingToolbox(pos);
     }
+
     return true;
 }
 
