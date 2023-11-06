8e59801db5d1774643f11e9366298f55b99cdd4d
Benjamin Hennion <17818041+bhennion@users.noreply.github.com>
2023-11-06 09:11:47 +0100

    Const-correctness in snapping handler

--- src/core/control/tools/SnapToGridInputHandler.cpp
+++ src/core/control/tools/SnapToGridInputHandler.cpp
@@ -3,44 +3,44 @@
 #include "control/settings/Settings.h"
 #include "model/Snapping.h"
 
-SnapToGridInputHandler::SnapToGridInputHandler(Settings* settings): settings(settings) {}
+SnapToGridInputHandler::SnapToGridInputHandler(const Settings* settings): settings(settings) {}
 
-double SnapToGridInputHandler::snapVertically(double y, bool alt) {
+double SnapToGridInputHandler::snapVertically(double y, bool alt) const {
     if (alt != settings->isSnapGrid()) {
         return Snapping::snapVertically(y, settings->getSnapGridSize(), settings->getSnapGridTolerance());
     }
     return y;
 }
 
-double SnapToGridInputHandler::snapHorizontally(double x, bool alt) {
+double SnapToGridInputHandler::snapHorizontally(double x, bool alt) const {
     if (alt != settings->isSnapGrid()) {
         return Snapping::snapHorizontally(x, settings->getSnapGridSize(), settings->getSnapGridTolerance());
     }
     return x;
 }
 
-Point SnapToGridInputHandler::snapToGrid(Point const& pos, bool alt) {
+Point SnapToGridInputHandler::snapToGrid(Point const& pos, bool alt) const {
     if (alt != settings->isSnapGrid()) {
         return Snapping::snapToGrid(pos, settings->getSnapGridSize(), settings->getSnapGridTolerance());
     }
     return pos;
 }
 
-double SnapToGridInputHandler::snapAngle(double radian, bool alt) {
+double SnapToGridInputHandler::snapAngle(double radian, bool alt) const {
     if (alt != settings->isSnapRotation()) {
         return Snapping::snapAngle(radian, settings->getSnapRotationTolerance());
     }
     return radian;
 }
 
-Point SnapToGridInputHandler::snapRotation(Point const& pos, Point const& center, bool alt) {
+Point SnapToGridInputHandler::snapRotation(Point const& pos, Point const& center, bool alt) const {
     if (alt != settings->isSnapRotation()) {
         return Snapping::snapRotation(pos, center, settings->getSnapRotationTolerance());
     }
     return pos;
 }
 
-Point SnapToGridInputHandler::snap(Point const& pos, Point const& center, bool alt) {
+Point SnapToGridInputHandler::snap(Point const& pos, Point const& center, bool alt) const {
     Point rotationSnappedPoint{snapRotation(pos, center, alt)};
     return snapToGrid(rotationSnappedPoint, alt);
 }
