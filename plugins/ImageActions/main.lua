7abac55096403fec240ad3852fbdefff4b103d96
Roland Lötscher <roland_loetscher@hotmail.com>
2024-03-07 22:17:00 +0100

    Format document using the Lua Language Server

--- plugins/ImageActions/main.lua
+++ plugins/ImageActions/main.lua
@@ -1,29 +1,31 @@
 function initUi()
-  app.registerUi({["menu"] = "Invert selected images", ["callback"] = "cb", ["mode"] = 1})
-  app.registerUi({["menu"] = "Downscale resolution of selected images by factor 0.5", ["callback"] = "cb", ["mode"] = 2})
-  app.registerUi({["menu"] = "Flip selected images horizontally", ["callback"] = "cb", ["mode"] = 3})
-  app.registerUi({["menu"] = "Flip selected images vertically", ["callback"] = "cb", ["mode"] = 4})
-  app.registerUi({["menu"] = "Rotate selected images 90 degree clockwise", ["callback"] = "cb", ["mode"] = 5})
-  app.registerUi({["menu"] = "Rotate selected images 90 degree counterclockwise", ["callback"] = "cb", ["mode"] = 6})
-  app.registerUi({["menu"] = "Make white pixels transparent", ["callback"] = "cb", ["mode"] = 7})
-  app.registerUi({["menu"] = "Trim selected images", ["callback"] = "cb", ["mode"] = 8})
-  app.registerUi({["menu"] = "Split images vertically by largest white block", ["callback"] = "cb", ["mode"] = 9})
-  app.registerUi({["menu"] = "Split images horizontally by largest white block", ["callback"] = "cb", ["mode"] = 10})
+  app.registerUi({ ["menu"] = "Invert selected images", ["callback"] = "cb", ["mode"] = 1 })
+  app.registerUi({ ["menu"] = "Downscale resolution of selected images by factor 0.5", ["callback"] = "cb", ["mode"] = 2 })
+  app.registerUi({ ["menu"] = "Flip selected images horizontally", ["callback"] = "cb", ["mode"] = 3 })
+  app.registerUi({ ["menu"] = "Flip selected images vertically", ["callback"] = "cb", ["mode"] = 4 })
+  app.registerUi({ ["menu"] = "Rotate selected images 90 degree clockwise", ["callback"] = "cb", ["mode"] = 5 })
+  app.registerUi({ ["menu"] = "Rotate selected images 90 degree counterclockwise", ["callback"] = "cb", ["mode"] = 6 })
+  app.registerUi({ ["menu"] = "Make white pixels transparent", ["callback"] = "cb", ["mode"] = 7 })
+  app.registerUi({ ["menu"] = "Trim selected images", ["callback"] = "cb", ["mode"] = 8 })
+  app.registerUi({ ["menu"] = "Split images vertically by largest white block", ["callback"] = "cb", ["mode"] = 9 })
+  app.registerUi({ ["menu"] = "Split images horizontally by largest white block", ["callback"] = "cb", ["mode"] = 10 })
 end
 
 function cb(mode)
   local images = app.getImages("selection")
   local hasVips, vips = pcall(require, "vips")
   if not hasVips then
-    app.openDialog("You need to have lua-vips and luaffifb installed and included in your Lua package path in order to use this plugin. \n" .. 
-               "See https://github.com/rolandlo/lua-vips/tree/fix-lua-5.3#installation for installation instructions.\n\n", {"OK"}, "", true)
+    app.openDialog(
+      "You need to have lua-vips and luaffifb installed and included in your Lua package path in order to use this plugin. \n" ..
+      "See https://github.com/rolandlo/lua-vips/tree/fix-lua-5.3#installation for installation instructions.\n\n", { "OK" },
+      "", true)
     return
   end
   local imdata = {}
-  for i=1,#images do
+  for i = 1, #images do
     local im = images[i]
     local image = vips.Image.new_from_buffer(im["data"])
-    local x, y, maxWidth, maxHeight=im["x"], im["y"], math.ceil(im["width"]), math.ceil(im["height"])
+    local x, y, maxWidth, maxHeight = im["x"], im["y"], math.ceil(im["width"]), math.ceil(im["height"])
     if mode == 1 then
       image = image:invert()
     elseif mode == 2 then
@@ -39,11 +41,11 @@ function cb(mode)
       image = image:rot("d270")
       maxWidth, maxHeight = maxHeight, maxWidth
     elseif mode == 7 then
-      if image:bands()<4 then
-        image = image:bandjoin(255) -- add alpha channel
+      if image:bands() < 4 then
+        image = image:bandjoin(255)    -- add alpha channel
       end
-      local white = {240, 240, 240, 0} -- pixels are considered white, if its r,g,b values are >=240 and alpha is arbitrary
-      local transparent = {0, 0, 0, 0}
+      local white = { 240, 240, 240, 0 } -- pixels are considered white, if its r,g,b values are >=240 and alpha is arbitrary
+      local transparent = { 0, 0, 0, 0 }
       image = image:more(white):bandand():ifthenelse(transparent, image)
     elseif mode == 8 then
       local width = image:width()
@@ -54,8 +56,9 @@ function cb(mode)
       maxWidth, maxHeight = maxWidth * newWidth // width, maxHeight * newHeight // height
     end
 
-    if mode ~= 9 and mode ~=10 then
-      table.insert(imdata, {data = image:write_to_buffer(".png"), x=x, y=y, maxWidth=maxWidth, maxHeight=maxHeight})
+    if mode ~= 9 and mode ~= 10 then
+      table.insert(imdata, { data = image:write_to_buffer(".png"), x = x, y = y, maxWidth = maxWidth, maxHeight =
+      maxHeight })
     else
       local vertical = mode == 9
       -- find largest sequence of white or transparent pixel rows/columns
@@ -67,17 +70,17 @@ function cb(mode)
 
       for i = 0, last - 1 do
         if vertical then
-          isWhite[i+1] = filter:extract_area(0, i, width, 1):avg() > 254
+          isWhite[i + 1] = filter:extract_area(0, i, width, 1):avg() > 254
         else
-          isWhite[i+1] = filter:extract_area(i, 0, 1, height):avg() > 254
+          isWhite[i + 1] = filter:extract_area(i, 0, 1, height):avg() > 254
         end
       end
 
       if #isWhite == 0 then
-        word = vertical and "rows" or "columns"
-        print("There are no completely white " .. word .. " in this image. Skipping.") 
-        table.insert(imdata, {data = im["data"], x=x, y=y, maxHeight=maxHeight, maxWidth=maxWidth})
-        goto continue 
+        local word = vertical and "rows" or "columns"
+        print("There are no completely white " .. word .. " in this image. Skipping.")
+        table.insert(imdata, { data = im["data"], x = x, y = y, maxHeight = maxHeight, maxWidth = maxWidth })
+        goto continue
       end
 
       local whiteEnds = {}
@@ -89,11 +92,11 @@ function cb(mode)
         if isWhite[i] then
           if currentLength > 0 then
             currentLength = currentLength + 1
-          else 
+          else
             currentLength = 1
             firstAfter = true
           end
-        else 
+        else
           if firstAfter then
             whiteEnds[n] = i - 1
             whiteLengths[n] = currentLength
@@ -108,7 +111,7 @@ function cb(mode)
 
       if #whiteEnds == 0 then
         print("Could not find a split")
-        table.insert(imdata, {data = im["data"], x=x, y=y, maxHeight=maxHeight, maxWidth=maxWidth})
+        table.insert(imdata, { data = im["data"], x = x, y = y, maxHeight = maxHeight, maxWidth = maxWidth })
         goto continue
       end
 
@@ -123,17 +126,19 @@ function cb(mode)
       if vertical then
         local image1 = image:extract_area(0, 0, width, index - maxLength // 2)
         local image2 = image:extract_area(0, index - maxLength // 2, width, height - index + maxLength // 2)
-        table.insert(imdata, {data = image1:write_to_buffer(".png"), x=x, y=y, maxWidth=maxWidth})
-        table.insert(imdata, {data = image2:write_to_buffer(".png"), x=x, y=y+index*maxHeight/height, maxWidth=maxWidth})
-      else 
+        table.insert(imdata, { data = image1:write_to_buffer(".png"), x = x, y = y, maxWidth = maxWidth })
+        table.insert(imdata, { data = image2:write_to_buffer(".png"), x = x, y = y + index * maxHeight / height, maxWidth =
+        maxWidth })
+      else
         local image1 = image:extract_area(0, 0, index - maxLength // 2, height)
         local image2 = image:extract_area(index - maxLength // 2, 0, width - index + maxLength // 2, height)
-        table.insert(imdata, {data = image1:write_to_buffer(".png"), x=x, y=y, maxHeight=maxHeight})
-        table.insert(imdata, {data = image2:write_to_buffer(".png"), x=x+index*maxWidth/width, y=y, maxHeight=maxHeight})
+        table.insert(imdata, { data = image1:write_to_buffer(".png"), x = x, y = y, maxHeight = maxHeight })
+        table.insert(imdata, { data = image2:write_to_buffer(".png"), x = x + index * maxWidth / width, y = y, maxHeight =
+        maxHeight })
       end
       ::continue::
     end
   end
-  app.uiAction({action = "ACTION_DELETE"})
-  app.addImages({images=imdata, allowUndoRedoAction = "grouped"})
+  app.uiAction({ action = "ACTION_DELETE" })
+  app.addImages({ images = imdata, allowUndoRedoAction = "grouped" })
 end
