9aaa044dfcf779a132620ee94df79268bb1e7fc9
Fabian Keßler <fabian_kessler@gmx.de>
2024-04-02 13:34:43 +0200

    Fixed Memory leaks for some GLibGuards, added out_ptr  - similar to c++23 out_ptr

--- src/util/include/util/raii/GLibGuards.h
+++ src/util/include/util/raii/GLibGuards.h
@@ -12,6 +12,7 @@
 #pragma once
 
 #include <memory>
+#include <type_traits>
 
 #include <gtk/gtk.h>
 namespace xoj::util {
@@ -21,9 +22,9 @@ inline namespace raii {
     namespace detail {                                           \
     struct class_name##GuardDelete {                             \
         auto operator()(type* ptr) const -> void {               \
-            if (*ptr) {                                          \
-                deletion_func(*ptr);                             \
-                *ptr = nullptr;                                  \
+            if (ptr) {                                           \
+                deletion_func(ptr);                              \
+                ptr = nullptr;                                   \
             }                                                    \
         }                                                        \
     };                                                           \
@@ -32,8 +33,30 @@ inline namespace raii {
 
 #define XOJ_GIO_GUARD_GENERATOR_TYPE(type, deletion_func) XOJ_GIO_GUARD_GENERATOR(type, type, deletion_func)
 
-XOJ_GIO_GUARD_GENERATOR(GError, GError*, g_error_free);
-XOJ_GIO_GUARD_GENERATOR_TYPE(GStrv, g_strfreev);
+XOJ_GIO_GUARD_GENERATOR(GStrv, gchar*, g_strfreev);
+XOJ_GIO_GUARD_GENERATOR_TYPE(GError, g_error_free);
+
+template <typename Smart, typename Pointer /* , typename ... Tuple */>
+struct out_ptr_t {
+    out_ptr_t(Smart& smart /* , Tuple tuple */): smart(smart) /* , tuple(tuple) */ {}
+    ~out_ptr_t() noexcept { smart.reset(ptr /*,  Todo: unpack tuble with indices trick if required*/); }
+    out_ptr_t(out_ptr_t const&) = delete;
+    auto operator=(out_ptr_t const&) -> out_ptr_t& = delete;
+
+    operator Pointer*() noexcept { return &ptr; }
+
+private:
+    Smart& smart;
+    Pointer ptr{};
+    // Tuple tuple;
+};
+
+
+template <typename Smart, typename... A>
+auto out_ptr(Smart& s /*, A&&... a */) noexcept -> out_ptr_t<Smart, typename Smart::pointer /* , std::tuple<A...> */> {
+    return out_ptr_t<Smart, typename Smart::pointer /* , std::tuple<A...> */>  //
+            (s /* , std::tuple<A...>(std::forward<A>(a)...) */);
+}
 
 }  // namespace raii
 }  // namespace xoj::util
