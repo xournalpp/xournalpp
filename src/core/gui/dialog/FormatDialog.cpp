d11be87b773a94587e1dcc85177f08e4e29221a2
Benjamin Hennion <17818041+bhennion@users.noreply.github.com>
2024-04-14 22:27:51 +0200

    Apply gtk4-builder-tool simplify --3to4 to ui/pageFormat.ui

--- src/core/gui/dialog/FormatDialog.cpp
+++ src/core/gui/dialog/FormatDialog.cpp
@@ -14,7 +14,7 @@
 
 class GladeSearchpath;
 
-constexpr auto UI_FILE = "pageFormat.glade";
+constexpr auto UI_FILE = "pageFormat.ui";
 constexpr auto UI_DIALOG_NAME = "pageFormatDialog";
 
 using namespace xoj::popup;
