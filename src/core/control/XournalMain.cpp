1aeaa5ccf346abe85db130d8aa829781ac998ddb
Benjamin Hennion <17818041+bhennion@users.noreply.github.com>
2024-04-16 16:44:49 +0200

    Apply gtk4-builder-tool simplify --3to4 to About dialog

--- src/core/control/XournalMain.cpp
+++ src/core/control/XournalMain.cpp
@@ -459,7 +459,7 @@ void on_startup(GApplication* application, XMPtr app_data) {
     const MigrateResult migrateResult = migrateSettings();
 
     app_data->gladePath = std::make_unique<GladeSearchpath>();
-    initResourcePath(app_data->gladePath.get(), "ui/about.glade");
+    initResourcePath(app_data->gladePath.get(), "ui/about.ui");
     initResourcePath(app_data->gladePath.get(), "ui/xournalpp.css", false);
 
     app_data->control = std::make_unique<Control>(application, app_data->gladePath.get(), app_data->disableAudio);
