7b3859cef1ba6eee6d29da4ea126877e19cb7d7c
Paul Pan <i@0x7f.app>
2024-04-26 16:29:57 +0800

    Use `wbindtextdomain` on Windows

--- src/core/control/XournalMain.cpp
+++ src/core/control/XournalMain.cpp
@@ -85,7 +85,13 @@ void initCAndCoutLocales() {
 void initLocalisation() {
 #ifdef ENABLE_NLS
     fs::path localeDir = Util::getGettextFilepath(Util::getLocalePath());
+
+#ifdef _WIN32
+    wbindtextdomain(GETTEXT_PACKAGE, localeDir.wstring().c_str());
+#else
     bindtextdomain(GETTEXT_PACKAGE, localeDir.u8string().c_str());
+#endif
+
     textdomain(GETTEXT_PACKAGE);
 
 #ifdef _WIN32
