f41c3264c549714e90765df42fbf028c837bc5bd
Benjamin Hennion <17818041+bhennion@users.noreply.github.com>
2024-05-01 12:13:34 +0200

    Fix InputContext:     * Fix debug flags     * Fix double/triple clicks     * Fetch event backlogs for stylus input

--- src/core/gui/inputdevices/InputContext.h
+++ src/core/gui/inputdevices/InputContext.h
@@ -15,6 +15,7 @@
 #include <memory>  // for unique_ptr
 #include <set>     // for set
 #include <string>  // for string
+#include <vector>
 
 #include <gdk/gdk.h>  // for GdkEvent, GdkModifierType
 #include <glib.h>     // for gulong
@@ -33,13 +34,14 @@ class TouchDrawingInputHandler;
 class TouchInputHandler;
 class XournalView;
 
+struct InputEvent;
+
 class InputContext final {
 
 private:
     XournalView* view;
     ScrollHandling* scrollHandling;
 
-    gulong signal_id{0};
     std::unique_ptr<StylusInputHandler> stylusHandler;
     std::unique_ptr<MouseInputHandler> mouseHandler;
     std::unique_ptr<TouchDrawingInputHandler> touchDrawingHandler;
@@ -49,9 +51,9 @@ private:
 
     GtkWidget* widget = nullptr;
     /// Event controllers added to the main widget. Used to remove them on ~InputContext() to stop signals
-    GtkEventController* keyCtrl = nullptr;
-    GtkEventController* legCtrl = nullptr;
+    std::vector<GtkEventController*> eventControllers;
 
+    int nbPress = 0;  ///< Number of consecutive clicks (1 for first)
 
     std::set<std::string> knownDevices;
 
@@ -76,17 +78,14 @@ private:
      */
     static bool eventCallback(GtkEventControllerLegacy* widget, GdkEvent* event, InputContext* self);
 
+    // static void stylusMotionCallback(GtkGestureStylus* ctrl, gdouble x, gdouble y, InputContext* self);
+
     /**
      * Handle the events
      * @param event The event to handle
      * @return Whether the event was handled
      */
-    bool handle(GdkEvent* event);
-
-    /**
-     * Print debug output
-     */
-    void printDebug(GdkEvent* event);
+    bool handle(const InputEvent& event);
 
 public:
     /**
