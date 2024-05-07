xournalpp/xournalpp

commit bc1f4815a67f38a2def76712c0ae71911630240d

Make BackgroundSelectDialog's GTK4-compatible

src/core/control/PageBackgroundChangeController.cpp
@@ -29,9 +29,10 @@
 #include "undo/UndoRedoHandler.h"                        // for UndoRedoHandler
 #include "util/Assert.h"                                 // for xoj_assert
 #include "util/PathUtil.h"                               // for fromGFile
-#include "util/Util.h"                                   // for npos
-#include "util/XojMsgBox.h"                              // for XojMsgBox
-#include "util/i18n.h"                                   // for FS, _, _F
+#include "util/PopupWindowWrapper.h"
+#include "util/Util.h"       // for npos
+#include "util/XojMsgBox.h"  // for XojMsgBox
+#include "util/i18n.h"       // for FS, _, _F
 
 #include "Control.h"     // for Control
 #include "filesystem.h"  // for path
@@ -44,20 +45,38 @@ PageBackgroundChangeController::PageBackgroundChangeController(Control* control)
 void PageBackgroundChangeController::applyBackgroundToAllPages(const PageType& pt) {
     control->clearSelectionEndText();
 
-    Document* doc = control->getDocument();
+    auto apply = [pt, this](CommitParameter param = std::nullopt) {
+        auto groupUndoAction = std::make_unique<GroupUndoAction>();
 
-    auto groupUndoAction = std::make_unique<GroupUndoAction>();
-
-    for (size_t p = 0; p < doc->getPageCount(); p++) {
-        auto undoAction = commitPageTypeChange(p, pt);
-        if (undoAction) {
-            groupUndoAction->addAction(std::move(undoAction));
+        Document* doc = control->getDocument();
+        doc->lock();
+        auto nbPages = doc->getPageCount();
+        for (size_t p = 0; p < nbPages; p++) {
+            auto undoAction = commitPageTypeChange(p, pt, param);
+            if (undoAction) {
+                groupUndoAction->addAction(std::move(undoAction));
+            }
         }
+        doc->unlock();
+
+        control->getUndoRedoHandler()->addUndoAction(std::move(groupUndoAction));
+
+        // Special background types may alter the page sizes as well
+        auto fire = pt.isSpecial() ? &Control::firePageSizeChanged : &Control::firePageChanged;
+        for (size_t n = 0; n < nbPages; n++) {
+            (control->*fire)(n);
+        }
+        control->updateBackgroundSizeButton();
+        control->getWindow()->getMenubar()->getPageTypeSubmenu().setSelected(std::move(pt));
+    };
+
+    if (pt.isImagePage()) {
+        askForImageBackground([apply = std::move(apply)](BackgroundImage img) { apply(std::move(img)); });
+    } else if (pt.isPdfPage()) {
+        askForPdfBackground([apply = std::move(apply)](size_t pdfPage) { apply(pdfPage); });
+    } else {
+        apply();
     }
-
-    control->getUndoRedoHandler()->addUndoAction(std::move(groupUndoAction));
-
-    control->getWindow()->getMenubar()->getPageTypeSubmenu().setSelected(pt);
 }
 
 void PageBackgroundChangeController::applyCurrentPageBackgroundToAll() {
@@ -80,7 +99,7 @@ void PageBackgroundChangeController::changePdfPagesBackground(const fs::path& fi
 
     for (size_t p = 0; p < doc->getPageCount(); p++) {
         if (doc->getPage(p)->getBackgroundType().format == PageTypeFormat::Pdf) {
-            this->control->firePageChanged(p);
+            this->control->firePageSizeChanged(p);
         }
     }
 
@@ -88,40 +107,82 @@ void PageBackgroundChangeController::changePdfPagesBackground(const fs::path& fi
     this->control->getUndoRedoHandler()->addUndoAction(std::move(undoAction));
 }
 
-void PageBackgroundChangeController::changeCurrentPageBackground(const PageType& pageType) {
+void PageBackgroundChangeController::changeCurrentPageBackground(const PageType& pt) {
     control->clearSelectionEndText();
 
-    PageRef page = control->getCurrentPage();
-    if (!page) {
-        return;
-    }
-
-    Document* doc = control->getDocument();
-    const size_t pageNr = doc->indexOf(page);
+    const size_t pageNr = control->getCurrentPageNo();
     xoj_assert(pageNr != npos);
 
-    auto undoAction = commitPageTypeChange(pageNr, pageType);
-    if (undoAction) {
-        control->getUndoRedoHandler()->addUndoAction(std::move(undoAction));
-    }
+    auto apply = [this, pt, pageNr](CommitParameter param = std::nullopt) {
+        Document* doc = control->getDocument();
+        doc->lock();
+        auto undoAction = commitPageTypeChange(pageNr, pt, std::move(param));
+        doc->unlock();
+        if (undoAction) {
+            control->getUndoRedoHandler()->addUndoAction(std::move(undoAction));
+        }
 
-    control->getWindow()->getMenubar()->getPageTypeSubmenu().setSelected(pageType);
+        // Special background types may alter the page sizes as well
+        if (pt.isSpecial()) {
+            control->firePageSizeChanged(pageNr);
+        } else {
+            control->firePageChanged(pageNr);
+        }
+        control->updateBackgroundSizeButton();
+        control->getWindow()->getMenubar()->getPageTypeSubmenu().setSelected(std::move(pt));
+    };
+
+    if (pt.isImagePage()) {
+        askForImageBackground([apply = std::move(apply)](BackgroundImage img) { apply(std::move(img)); });
+    } else if (pt.isPdfPage()) {
+        askForPdfBackground([apply = std::move(apply)](size_t pdfPage) { apply(pdfPage); });
+    } else {
+        apply();
+    }
 }
 
 void PageBackgroundChangeController::setPageTypeForNewPages(const std::optional<PageType>& pt) {
     this->pageTypeForNewPages = pt;
 }
 
-auto PageBackgroundChangeController::commitPageTypeChange(const size_t pageNum, const PageType& pageType)
-        -> std::unique_ptr<UndoAction> {
+static void setPageImageBackground(const PageRef& page, BackgroundImage img) {
+    page->setBackgroundImage(std::move(img));
+    page->setBackgroundType(PageType(PageTypeFormat::Image));
+
+    // Apply correct page size
+    GdkPixbuf* pixbuf = page->getBackgroundImage().getPixbuf();
+    if (pixbuf) {
+        page->setSize(gdk_pixbuf_get_width(pixbuf), gdk_pixbuf_get_height(pixbuf));
+    } else {
+        g_warning("PageBackgroundChangeController::setPageImageBackground(): Page with image background but nullptr "
+                  "pixbuf");
+    }
+}
+
+static void setPagePdfBackground(const PageRef& page, size_t pdfPageNr, Document* doc) {
+    if (pdfPageNr < doc->getPdfPageCount()) {
+        // no need to set a type, if we set the page number the type is also set
+        page->setBackgroundPdfPageNr(pdfPageNr);
+
+        XojPdfPageSPtr p = doc->getPdfPage(pdfPageNr);
+        page->setSize(p->getWidth(), p->getHeight());
+    } else {
+        g_warning("PageBackgroundChangeController::setPagePdfBackground() with past-the-end PDF page number");
+    }
+}
+
+auto PageBackgroundChangeController::commitPageTypeChange(size_t pageNum, const PageType& pageType,
+                                                          CommitParameter param) -> std::unique_ptr<UndoAction> {
+    xoj_assert(pageType.isImagePage() == std::holds_alternative<BackgroundImage>(param));
+    xoj_assert(pageType.isPdfPage() == std::holds_alternative<size_t>(param));
+
     PageRef page = control->getDocument()->getPage(pageNum);
     if (!page) {
+        g_warning("PageBackgroundChangeController::commitPageTypeChange() could not fetch page %zu", pageNum);
         return {};
     }
 
     Document* doc = control->getDocument();
-    const size_t pageNr = doc->indexOf(page);
-    xoj_assert(pageNr != npos);
 
     // Get values for Undo / Redo
     const double origW = page->getWidth();
@@ -131,125 +192,40 @@ auto PageBackgroundChangeController::commitPageTypeChange(const size_t pageNum, 
     PageType origType = page->getBackgroundType();
 
     // Apply the new background
-    applyPageBackground(page, pageType);
+    if (std::holds_alternative<BackgroundImage>(param)) {  // Image background
+        xoj_assert(!std::get<BackgroundImage>(param).isEmpty());
+        setPageImageBackground(page, std::move(std::get<BackgroundImage>(param)));
+    } else if (std::holds_alternative<size_t>(param)) {  // PDF background
+        setPagePdfBackground(page, std::get<size_t>(param), doc);
+    } else {
+        page->setBackgroundType(pageTypeForNewPages.value());
+    }
 
-    control->firePageChanged(pageNr);
-    control->updateBackgroundSizeButton();
     return std::make_unique<PageBackgroundChangedUndoAction>(page, origType, origPdfPage, origBackgroundImage, origW,
                                                              origH);
 }
 
-/**
- * Apply a new Image Background, asks the user which image should be inserted
- *
- * @return true on success, false if the user cancels
- */
-auto PageBackgroundChangeController::applyImageBackground(PageRef page) -> bool {
+void PageBackgroundChangeController::askForImageBackground(std::function<void(BackgroundImage)> callback) {
     Document* doc = control->getDocument();
 
-    doc->lock();
-    ImagesDialog dlg(control->getGladeSearchPath(), doc, control->getSettings());
-    doc->unlock();
-
-    dlg.show(GTK_WINDOW(control->getGtkWindow()));
-    BackgroundImage img = dlg.getSelectedImage();
-
-    if (!img.isEmpty()) {
-        page->setBackgroundImage(img);
-        page->setBackgroundType(PageType(PageTypeFormat::Image));
-    } else if (dlg.shouldShowFilechooser()) {
-        bool attach = false;
-        GFile* file = ImageOpenDlg::show(control->getGtkWindow(), control->getSettings(), true, &attach);
-        if (file == nullptr) {
-            // The user canceled
-            return false;
-        }
-
-
-        auto filepath = Util::fromGFile(file);
-
-        BackgroundImage newImg;
-        GError* err = nullptr;
-        newImg.loadFile(filepath, &err);
-        newImg.setAttach(attach);
-        if (err) {
-            XojMsgBox::showErrorToUser(control->getGtkWindow(),
-                                       FS(_F("This image could not be loaded. Error message: {1}") % err->message));
-            g_error_free(err);
-            return false;
-        }
-
-
-        page->setBackgroundImage(newImg);
-        page->setBackgroundType(PageType(PageTypeFormat::Image));
-    }
-
-    // Apply correct page size
-    GdkPixbuf* pixbuf = page->getBackgroundImage().getPixbuf();
-    if (pixbuf) {
-        page->setSize(gdk_pixbuf_get_width(pixbuf), gdk_pixbuf_get_height(pixbuf));
-
-        size_t pageNr = doc->indexOf(page);
-        if (pageNr != npos) {
-            // Only if the page is already inserted into the document
-            control->firePageSizeChanged(pageNr);
-        }
-    }
-
-    return true;
+    auto dlg = xoj::popup::PopupWindowWrapper<ImagesDialog>(control->getGladeSearchPath(), doc, control->getSettings(),
+                                                            std::move(callback));
+    dlg.show(control->getGtkWindow());
 }
 
-/**
- * Apply a new PDF Background, asks the user which page should be selected
- *
- * @return true on success, false if the user cancels
- */
-auto PageBackgroundChangeController::applyPdfBackground(PageRef page) -> bool {
+void PageBackgroundChangeController::askForPdfBackground(std::function<void(size_t)> callback) {
     Document* doc = control->getDocument();
 
     if (doc->getPdfPageCount() == 0) {
-
         std::string msg = _("You don't have any PDF pages to select from. Cancel operation.\n"
                             "Please select another background type: Menu \"Journal\" → \"Configure Page Template\".");
         XojMsgBox::showErrorToUser(control->getGtkWindow(), msg);
-        return false;
+        return;
     }
 
-    doc->lock();
-    auto dlg = PdfPagesDialog(control->getGladeSearchPath(), doc, control->getSettings());
-    doc->unlock();
-
-    dlg.show(GTK_WINDOW(control->getGtkWindow()));
-
-    size_t selected = dlg.getSelectedPage();
-
-    if (selected < doc->getPdfPageCount()) {
-        // no need to set a type, if we set the page number the type is also set
-        page->setBackgroundPdfPageNr(selected);
-
-        XojPdfPageSPtr p = doc->getPdfPage(selected);
-        page->setSize(p->getWidth(), p->getHeight());
-    }
-
-    return true;
-}
-
-/**
- * Apply the background to the page, asks for PDF Page or Image, if needed
- *
- * @return true on success, false if the user cancels
- */
-auto PageBackgroundChangeController::applyPageBackground(PageRef page, const PageType& pt) -> bool {
-    if (pt.isPdfPage()) {
-        return applyPdfBackground(page);
-    }
-    if (pt.isImagePage()) {
-        return applyImageBackground(page);
-    }
-
-
-    page->setBackgroundType(pt);
-    return true;
+    auto dlg = xoj::popup::PopupWindowWrapper<PdfPagesDialog>(control->getGladeSearchPath(), doc,
+                                                              control->getSettings(), std::move(callback));
+    dlg.show(control->getGtkWindow());
 }
 
 /**
@@ -287,28 +263,40 @@ void PageBackgroundChangeController::insertNewPage(size_t position, bool shouldS
     model.parse(control->getSettings()->getPageTemplate());
 
     auto page = std::make_shared<XojPage>(model.getPageWidth(), model.getPageHeight());
-    PageRef current = control->getCurrentPage();
+
+    auto afterConfigured = [position, shouldScrollToPage, ctrl = this->control](PageRef page) {
+        ctrl->insertPage(page, position, shouldScrollToPage);
+    };
 
     if (!pageTypeForNewPages) {
+        PageRef current = control->getCurrentPage();
         xoj_assert(current);
         copyBackgroundFromOtherPage(page, current);
+        afterConfigured(std::move(page));
+    } else if (pageTypeForNewPages->isImagePage()) {
+        askForImageBackground([after = std::move(afterConfigured), page = std::move(page)](BackgroundImage img) {
+            setPageImageBackground(page, std::move(img));
+            after(std::move(page));
+        });
+    } else if (pageTypeForNewPages->isPdfPage()) {
+        askForPdfBackground([after = std::move(afterConfigured), page = std::move(page), doc](size_t pdfPageNr) {
+            setPagePdfBackground(page, pdfPageNr, doc);
+            after(std::move(page));
+        });
     } else {
         // Create a new page from template
-        if (!applyPageBackground(page, pageTypeForNewPages.value())) {
-            // User canceled PDF or Image Selection
-            return;
-        }
+        page->setBackgroundType(pageTypeForNewPages.value());
 
         // Set background Color
         page->setBackgroundColor(model.getBackgroundColor());
 
         if (model.isCopyLastPageSize()) {
+            PageRef current = control->getCurrentPage();
             xoj_assert(current);
             page->setSize(current->getWidth(), current->getHeight());
         }
+        afterConfigured(std::move(page));
     }
-
-    control->insertPage(page, position, shouldScrollToPage);
 }
 
 void PageBackgroundChangeController::documentChanged(DocumentChangeType type) {}

src/core/control/PageBackgroundChangeController.h
@@ -12,13 +12,16 @@
 #pragma once
 
 #include <cstddef>  // for size_t
-#include <memory>   // for unique_ptr
+#include <functional>
+#include <memory>  // for unique_ptr
 #include <optional>
+#include <variant>
 
-#include "model/DocumentChangeType.h"       // for DocumentChangeType
-#include "model/DocumentListener.h"         // for DocumentListener
-#include "model/PageRef.h"                  // for PageRef
-#include "model/PageType.h"                 // for PageType
+#include "model/BackgroundImage.h"
+#include "model/DocumentChangeType.h"  // for DocumentChangeType
+#include "model/DocumentListener.h"    // for DocumentListener
+#include "model/PageRef.h"             // for PageRef
+#include "model/PageType.h"            // for PageType
 
 #include "filesystem.h"  // for path
 
@@ -59,31 +62,21 @@ private:
      */
     static void copyBackgroundFromOtherPage(PageRef target, PageRef source);
 
-    /**
-     * Apply the background to the page, asks for PDF Page or Image, if needed
-     *
-     * @return true on success, false if the user cancels
-     */
-    bool applyPageBackground(PageRef page, const PageType& pt);
+    /// Asks the user to choose a pdf page, and calls callback
+    void askForPdfBackground(std::function<void(size_t)> callback);
 
-    /**
-     * Apply a new PDF Background, asks the user which page should be selected
-     *
-     * @return true on success, false if the user cancels
-     */
-    bool applyPdfBackground(PageRef page);
+    /// Asks the user to choose an image background, and calls callback
+    void askForImageBackground(std::function<void(BackgroundImage)> callback);
 
+    /// Nothing, a BackgroundImage or the number of a pdf page
+    using CommitParameter = std::variant<std::nullopt_t, BackgroundImage, size_t>;
     /**
-     * Apply a new Image Background, asks the user which image should be inserted
-     *
-     * @return true on success, false if the user cancels
+     * Applies pageType to page number pageNum.
+     *      If pageType.isImage(), a BackgroundImage must be provided
+     *      If pageType.isPdf(), a pdf page number must be provided in the std::variant
      */
-    bool applyImageBackground(PageRef page);
-
-    /**
-     * Perform the page type change.
-     */
-    auto commitPageTypeChange(size_t pageNum, const PageType& pageType) -> std::unique_ptr<UndoAction>;
+    auto commitPageTypeChange(size_t pageNum, const PageType& pageType, CommitParameter param = std::nullopt)
+            -> std::unique_ptr<UndoAction>;
 
 private:
     Control* control = nullptr;

src/core/gui/dialog/backgroundSelect/BackgroundSelectDialogBase.cpp
@@ -6,7 +6,9 @@
 #include <gdk/gdk.h>      // for GDK_EXPOSURE_MASK
 #include <glib-object.h>  // for G_CALLBACK, g_signal_connect
 
-#include "util/Util.h"        // for paintBackgroundWhite
+#include "gui/Builder.h"
+#include "util/Util.h"  // for paintBackgroundWhite
+#include "util/gtk4_helper.h"
 #include "util/safe_casts.h"  // for round_cast
 
 #include "BaseElementView.h"  // for BaseElementView
@@ -14,86 +16,84 @@
 class GladeSearchpath;
 
 
+constexpr auto UI_FILE = "backgroundSelection.glade";
+constexpr auto UI_DIALOG_NAME = "Dialog";
+
 BackgroundSelectDialogBase::BackgroundSelectDialogBase(GladeSearchpath* gladeSearchPath, Document* doc,
-                                                       Settings* settings, const std::string& glade,
-                                                       const std::string& mainWnd):
-        GladeGui(gladeSearchPath, glade, std::move(mainWnd)), settings(settings), doc(doc) {
-    this->layoutContainer = gtk_layout_new(nullptr, nullptr);
-    gtk_widget_show(this->layoutContainer);
-    this->scrollPreview = get("scrollContents");
-    gtk_container_add(GTK_CONTAINER(scrollPreview), layoutContainer);
+                                                       Settings* settings, const char* title):
+        settings(settings), doc(doc) {
+    Builder builder(gladeSearchPath, UI_FILE);
+    this->window.reset(GTK_WINDOW(builder.get(UI_DIALOG_NAME)));
+    gtk_window_set_title(window.get(), title);
 
-    gtk_widget_set_events(this->layoutContainer, GDK_EXPOSURE_MASK);
-    g_signal_connect(this->layoutContainer, "draw", G_CALLBACK(Util::paintBackgroundWhite), nullptr);
+    this->container = GTK_FIXED(gtk_fixed_new());
+    gtk_widget_set_hexpand(GTK_WIDGET(this->container), true);
+    this->okButton = builder.get("btOk");
+    this->vbox = GTK_BOX(builder.get("vbox"));
 
-    g_signal_connect(this->window, "size-allocate", G_CALLBACK(sizeAllocate), this);
-    gtk_window_set_default_size(GTK_WINDOW(this->window), 800, 600);
+    this->scrolledWindow = GTK_SCROLLED_WINDOW(builder.get("scrollContents"));
+    gtk_scrolled_window_set_child(scrolledWindow, GTK_WIDGET(container));
+
+    g_signal_connect(gtk_scrolled_window_get_hadjustment(scrolledWindow), "notify::page-size",
+                     G_CALLBACK(+[](GObject*, GParamSpec*, gpointer d) {
+                         static_cast<BackgroundSelectDialogBase*>(d)->layout();
+                     }),
+                     this);
+
+    gtk_window_set_default_size(this->window.get(), 800, 600);
+    gtk_widget_set_sensitive(this->okButton, false);
+
+    g_signal_connect_swapped(builder.get("btCancel"), "clicked", G_CALLBACK(gtk_window_close), this->window.get());
+
+    g_signal_connect(this->container, "realize", G_CALLBACK(+[](GtkWidget*, gpointer self) {
+                         static_cast<BackgroundSelectDialogBase*>(self)->layout();
+                     }),
+                     this);
 }
 
-BackgroundSelectDialogBase::~BackgroundSelectDialogBase() {
-    for (BaseElementView* e: elements) { delete e; }
-    elements.clear();
-}
-
-void BackgroundSelectDialogBase::sizeAllocate(GtkWidget* widget, GtkRequisition* requisition,
-                                              BackgroundSelectDialogBase* dlg) {
-    GtkAllocation alloc = {0};
-    gtk_widget_get_allocation(dlg->scrollPreview, &alloc);
-    if (dlg->lastWidth == alloc.width) {
-        return;
-    }
-    dlg->lastWidth = alloc.width;
-    dlg->layout();
-}
+BackgroundSelectDialogBase::~BackgroundSelectDialogBase() = default;
 
 auto BackgroundSelectDialogBase::getSettings() -> Settings* { return this->settings; }
 
 void BackgroundSelectDialogBase::layout() {
-    double x = 0;
-    double y = 0;
-    double row_height = 0;
-    double max_row_width = 0;
+    int x = 0;
+    int y = 0;
+    int row_height = 0;
+    int max_row_width = 0;
 
-    GtkAllocation alloc = {0};
-    gtk_widget_get_allocation(this->scrollPreview, &alloc);
+    auto width = gtk_adjustment_get_page_size(gtk_scrolled_window_get_hadjustment(scrolledWindow));
 
-    for (BaseElementView* p: this->elements) {
+    for (const auto& p: this->entries) {
         if (!gtk_widget_get_visible(p->getWidget())) {
             continue;
         }
 
-        if (x + p->getWidth() > alloc.width) {
+        if (x + p->getWidth() > width) {
+            max_row_width = std::max(max_row_width, x);
             y += row_height;
             x = 0;
             row_height = 0;
         }
-        max_row_width = std::max(max_row_width, x + static_cast<double>(p->getWidth()));
+        gtk_fixed_move(this->container, p->getWidget(), x, y);
 
-        gtk_layout_move(GTK_LAYOUT(this->layoutContainer), p->getWidget(), round_cast<int>(x), round_cast<int>(y));
-
-        row_height =
-                std::max(row_height,
-                         static_cast<double>(p->getHeight()));  // TODO(fabian): page height should be double as well
-
+        row_height = std::max(row_height, p->getHeight());
         x += p->getWidth();
     }
+    max_row_width = std::max(max_row_width, x);
 
-    gtk_layout_set_size(GTK_LAYOUT(this->layoutContainer), as_unsigned(round_cast<int>(max_row_width)),
-                        as_unsigned(round_cast<int>(y + row_height)));
+    gtk_widget_set_size_request(GTK_WIDGET(this->container), max_row_width, y + row_height);
 }
 
-void BackgroundSelectDialogBase::show(GtkWindow* parent) {
-    for (BaseElementView* e: elements) { gtk_layout_put(GTK_LAYOUT(this->layoutContainer), e->getWidget(), 0, 0); }
+void BackgroundSelectDialogBase::populate() {
+    for (const auto& e: entries) {
+        gtk_fixed_put(this->container, e->getWidget(), 0, 0);
+    }
 
-    if (!elements.empty()) {
+    if (!entries.empty()) {
         setSelected(0);
     }
 
     layout();
-
-    gtk_window_set_transient_for(GTK_WINDOW(this->window), parent);
-    gtk_dialog_run(GTK_DIALOG(this->window));
-    gtk_widget_hide(this->window);
 }
 
 void BackgroundSelectDialogBase::setSelected(size_t selected) {
@@ -102,12 +102,13 @@ void BackgroundSelectDialogBase::setSelected(size_t selected) {
     }
 
     size_t lastSelected = this->selected;
-    if (lastSelected < elements.size()) {
-        elements[lastSelected]->setSelected(false);
+    if (lastSelected < entries.size()) {
+        entries[lastSelected]->setSelected(false);
     }
 
-    if (selected < elements.size()) {
-        elements[selected]->setSelected(true);
+    if (selected < entries.size()) {
+        entries[selected]->setSelected(true);
         this->selected = selected;
+        gtk_widget_set_sensitive(okButton, true);
     }
 }

src/core/gui/dialog/backgroundSelect/BackgroundSelectDialogBase.h
@@ -11,61 +11,52 @@
 
 #pragma once
 
-#include <string>  // for string
+#include <memory>
 #include <vector>  // for vector
 
-#include <gtk/gtk.h>  // for GtkWidget, GtkRequisition, GtkWindow
+#include <gtk/gtk.h>  // for GtkWidget, GtkWindow
 
-#include "gui/GladeGui.h"  // for GladeGui
 #include "util/Util.h"     // for npos
+#include "util/raii/GtkWindowUPtr.h"
 
 class Document;
 class Settings;
 class BaseElementView;
 class GladeSearchpath;
 
-class BackgroundSelectDialogBase: public GladeGui {
-public:
-    BackgroundSelectDialogBase(GladeSearchpath* gladeSearchPath, Document* doc, Settings* settings,
-                               const std::string& glade, const std::string& mainWnd);
-    ~BackgroundSelectDialogBase() override;
+class BackgroundSelectDialogBase {
+protected:
+    BackgroundSelectDialogBase(GladeSearchpath* gladeSearchPath, Document* doc, Settings* settings, const char* title);
+    ~BackgroundSelectDialogBase();
 
 public:
     Settings* getSettings();
-    void show(GtkWindow* parent) override;
-    virtual void setSelected(size_t selected);
+    void setSelected(size_t selected);
+
+    inline GtkWindow* getWindow() const { return window.get(); }
 
 protected:
+    /// Adds the entry widgets
+    void populate();
     void layout();
 
-private:
-    static void sizeAllocate(GtkWidget* widget, GtkRequisition* requisition, BackgroundSelectDialogBase* dlg);
-
-private:
 protected:
+    xoj::util::GtkWindowUPtr window;
+
     Settings* settings = nullptr;
-    GtkWidget* scrollPreview = nullptr;
-    GtkWidget* layoutContainer = nullptr;
+    GtkScrolledWindow* scrolledWindow = nullptr;
+    GtkFixed* container = nullptr;  ///< Area where miniatures are layed out
+    GtkWidget* okButton = nullptr;
+    GtkBox* vbox = nullptr;  ///< Vertical GtkBox containing all.
 
     Document* doc = nullptr;
 
-    /**
-     * Selection confirmed
-     */
+    /// Selection confirmed
     bool confirmed = false;
 
-    /**
-     * Selected image, none if npos
-     */
+    /// Selected entry, none if npos
     size_t selected = npos;
 
-    /**
-     * To check if the size has real changed
-     */
-    int lastWidth = 0;
-
-    /**
-     * Elements to display
-     */
-    std::vector<BaseElementView*> elements;
+    /// Entries to display
+    std::vector<std::unique_ptr<BaseElementView>> entries;
 };

commit 8882f8aae66a5cbcc0f269de25c33aabe8650809

Fix crash when changing page type
This is a regression from #5630
The crash happens when using menu Journal > Page Background
(e.g. changing from graph to lined)

src/core/control/PageBackgroundChangeController.cpp
@@ -198,7 +198,7 @@ auto PageBackgroundChangeController::commitPageTypeChange(size_t pageNum, const 
     } else if (std::holds_alternative<size_t>(param)) {  // PDF background
         setPagePdfBackground(page, std::get<size_t>(param), doc);
     } else {
-        page->setBackgroundType(pageTypeForNewPages.value());
+        page->setBackgroundType(pageType);
     }
 
     return std::make_unique<PageBackgroundChangedUndoAction>(page, origType, origPdfPage, origBackgroundImage, origW,