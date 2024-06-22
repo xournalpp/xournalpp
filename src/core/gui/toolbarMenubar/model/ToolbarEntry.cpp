9fc31874ec8a7be3201712ec372a9c324c5ed846
Benjamin Hennion <17818041+bhennion@users.noreply.github.com>
2024-06-22 12:39:45 +0200

    Working Toolbar customization

--- src/core/gui/toolbarMenubar/model/ToolbarEntry.cpp
+++ src/core/gui/toolbarMenubar/model/ToolbarEntry.cpp
@@ -31,25 +31,6 @@ auto ToolbarEntry::getName() const -> std::string { return this->name; }
 
 void ToolbarEntry::setName(std::string name) { this->name = std::move(name); }
 
-auto ToolbarEntry::addItem(std::string item) -> int { return entries.emplace_back(std::move(item)).getId(); }
-
-auto ToolbarEntry::removeItemById(int id) -> bool {
-    auto it = std::find_if(entries.begin(), entries.end(), [id](const auto& it) { return it.getId() == id; });
-    if (it != entries.end()) {
-        entries.erase(it);
-        return true;
-    }
-    g_warning("ToolbarEntry::removeItemById: Tried to remove inexistant ID");
-    return false;
-}
-
-auto ToolbarEntry::insertItem(std::string item, int position) -> int {
-    xoj_assert(position >= 0);
-    if (position >= static_cast<int>(entries.size())) {
-        return entries.emplace_back(std::move(item)).getId();
-    }
-
-    return entries.emplace(std::next(entries.begin(), position), std::move(item))->getId();
-}
+void ToolbarEntry::addItem(std::string item) { entries.emplace_back(std::move(item)); }
 
 auto ToolbarEntry::getItems() const -> const ToolbarItemVector& { return entries; }
