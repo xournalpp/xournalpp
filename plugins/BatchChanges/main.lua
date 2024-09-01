7fd41715d3623a9ac69b4d322edba8f5100e7ebb
atticus-sullivan <oss.heindl+git@protonmail.com>
2024-09-01 16:19:43 +0200

    bugfixes and cleanup

--- plugins/BatchChanges/main.lua
+++ plugins/BatchChanges/main.lua
@@ -1,22 +1,20 @@
 local m = {
+    NORMAL = 0,
     FORCE = 1,
 }
 
 -- Register all Toolbar actions and intialize all UI stuff
 function initUi()
-  app.registerUi{menu="Add one empty pages after all pdf page once", callback="AddEmpty", mode=m.FORCE}
+  app.registerUi{menu="Add one empty pages after all pdf page once", callback="AddEmpty", mode=m.NORMAL}
   app.registerUi{menu="Add empty pages after every page", callback="AddEmpty", mode=m.FORCE}
-
-  -- app.registerUi{menu="Background for editing", callback="SetBackgroundAll", mode=0}
-  -- app.registerUi{menu="Background for printing", callback="SetBackgroundAll", mode=1}
 end
 
 function AddEmpty(mode)
     local struct = app.getDocumentStructure()
     for p=#struct.pages,1,-1 do
         local page = struct.pages[p]
-        local pred
-        if not mode == m.FORCE then
+        local pred = true
+        if mode ~= m.FORCE then
             -- page p is a pdf page
             pred = page.pdfBackgroundPageNo ~= 0
             if pred then
@@ -36,17 +34,3 @@ function AddEmpty(mode)
         end
     end
 end
-
--- function SetBackgroundAll(mode)
---     local struct = app.getDocumentStructure()
---     for p in ipairs(struct.pages) do
---         app.setCurrentPage(p)
---         local color = 0
---         if mode == 0 then
---             color = 0x0
---         else
---             color = 0x1
---         end
---         app.changeCurrentPageBackground{color=color}
---     end
--- end
