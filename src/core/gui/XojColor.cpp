1798039b3d3c2b8b04e8af9eb2940e570a09539e
Benjamin Hennion <17818041+bhennion@users.noreply.github.com>
2024-09-25 22:09:21 +0200

    Removed unused class gui/XojColor

--- src/core/gui/XojColor.cpp
+++
@@ -1,9 +0,0 @@
-#include "XojColor.h"
-
-#include <utility>
-
-XojColor::XojColor(Color color, std::string name): color(color), name(std::move(name)) {}
-
-auto XojColor::getColor() const -> Color { return this->color; }
-
-auto XojColor::getName() const -> std::string { return this->name; }
