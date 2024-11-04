c534d84043e4d84dcf12b8b977ad4e2db4f6d0fd
Roland Lötscher <roland_loetscher@hotmail.com>
2024-11-04 16:19:44 +0100

    Initial commit
    Co-authored-by: MiltonBalaOfficial <118467836+MiltonBalaOfficial@users.noreply.github.com>

---
+++ plugins/ShapeLibrary/Shapes/scaleneTriangle.lua
@@ -0,0 +1,13 @@
+local strokesData = {
+  stroke1 = {
+    x = { 34.423111438978, 134.42311143898, 184.42311143898, 34.423111438978 },
+    y = { 114.44275633398, 114.44275633398, 27.840215955541, 114.44275633398 },
+    pressure = {},
+    tool = "pen",
+    color = 16711680,
+    width = 2.26,
+    fill = -1,
+    lineStyle = "plain",
+  },
+}
+return strokesData   -- Return the strokesData table
