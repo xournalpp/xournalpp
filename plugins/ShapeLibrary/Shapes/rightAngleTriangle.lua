f127e37ed12769c75e22c297f39b1a494e0b51cb
Roland Lötscher <roland_loetscher@hotmail.com>
2024-11-04 16:19:44 +0100

    Initial commit
    Co-authored-by: MiltonBalaOfficial <118467836+MiltonBalaOfficial@users.noreply.github.com>

---
+++ plugins/ShapeLibrary/Shapes/rightAngleTriangle.lua
@@ -0,0 +1,13 @@
+local strokesData = {
+  stroke1 = {
+    x = { 134.36899221113, 34.368992211127, 34.368992211127, 134.36899221113 },
+    y = { 123.630125013, 123.630125013, 23.630125012996, 123.630125013 },
+    pressure = {},
+    tool = "pen",
+    color = 255,
+    width = 2.26,
+    fill = -1,
+    lineStyle = "plain",
+  },
+}
+return strokesData   -- Return the strokesData table
