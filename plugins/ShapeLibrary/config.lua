9977453cbf3a1d985bb661a25db5fc0bc9c6045f
Roland Lötscher <roland_loetscher@hotmail.com>
2024-11-05 16:13:05 +0100

    Update shapes and add some new
    @Co-authored-by: MiltonBalaOfficial <118467836+MiltonBalaOfficial@users.noreply.github.com>

--- plugins/ShapeLibrary/config.lua
+++ plugins/ShapeLibrary/config.lua
@@ -56,6 +56,10 @@ local _M = {
             [2] = { name = "Immovable Support", shapeName = "immuvableSupport" },
             [3] = { name = "Right Hand Rule", shapeName = "rightHandRule" },
             [4] = { name = "Vertical Circle", shapeName = "verticalCircle" },
+            [5] = { name = "Venturi Meter", shapeName = "venturiMeter" },
+            [6] = { name = "Cyclotron", shapeName = "cyclotron" },
+            [7] = { name = "Car", shapeName = "car" },
+            [8] = { name = "Organ Pipes", shapeName = "organPipes" },
         },
     },
     --    [6] = {
