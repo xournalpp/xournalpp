426aaa1e67287e9f2061f7d6fb1b46d0c8b1b76e
Roland Lötscher <roland_loetscher@hotmail.com>
2024-11-13 03:15:31 +0100

    Minor fixups to FitToContent and HighlightPosition plugins

--- plugins/HighlightPosition/main.lua
+++ plugins/HighlightPosition/main.lua
@@ -2,7 +2,7 @@ function initUi()
   app.registerUi({["menu"] = "Toggle Highlight Position", ["callback"] = "laser", ["accelerator"] = "<Alt>x"});
 end
 
-local togglestate = false;
+local toggleState = false;
 
 function laser()
   toggleState = not toggleState
