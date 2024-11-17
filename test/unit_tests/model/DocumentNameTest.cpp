ac5493ba5c81d4b9f8b20db49dab8cdcb3d3dde4
Fabian Keßler <fabian_kessler@gmx.de>
2024-11-17 11:52:24 +0100

    improved test

--- test/unit_tests/model/DocumentNameTest.cpp
+++ test/unit_tests/model/DocumentNameTest.cpp
@@ -17,16 +17,18 @@
 #include "model/Document.h"
 #include "model/DocumentHandler.h"
 
+static void test_u8_fn(Document& doc, Document::DocumentType dt, std::string_view left, std::string_view right, std::string_view expected) {
+    auto p = doc.createSaveFilename(dt, left, right);
+    std::cout << p << std::endl;
+    ASSERT_EQ(p.u8string(), expected);
+}
+
 TEST(DocumentName, testUTF8) {
     DocumentHandler dh;
     Document doc(&dh);
-    auto p = doc.createSaveFilename(Document::PDF, u8"%%ç测试ôê€ß", u8"%{name}测试");
-    std::cout << p << std::endl;
-    p = doc.createSaveFilename(Document::XOPP, u8"%%ç测试ôê€ß", u8"%{name}测试");
-    std::cout << p << std::endl;
+    test_u8_fn(doc,Document::PDF, u8"%%ç测试ôê€ß", u8"%{name}测试",u8"%ç测试ôê€ß");
+    test_u8_fn(doc,Document::XOPP, u8"%%ç测试ôê€ß", u8"%{name}测试",u8"%ç测试ôê€ß");
     doc.setFilepath(fs::u8path(u8"ùèçüûin/ë€ds测试q.xopp"));
-    p = doc.createSaveFilename(Document::PDF, u8"%%ç测试ôê€ß", u8"%{name}测试");
-    std::cout << p << std::endl;
-    p = doc.createSaveFilename(Document::XOPP, u8"%%ç测试ôê€ß", u8"%{name}测试");
-    std::cout << p << std::endl;
+    test_u8_fn(doc,Document::PDF, u8"%%ç测试ôê€ß", u8"%{name}测试",u8"ë€ds测试q测试");
+    test_u8_fn(doc,Document::XOPP, u8"%%ç测试ôê€ß", u8"%{name}测试",u8"ë€ds测试q");
 }
