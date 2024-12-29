a732910615d9e2dda2065ab3cd93af0d4ffbb472
Roland Lötscher <roland_loetscher@hotmail.com>
2024-12-29 11:41:17 +0100

    reorganize code and add initial lgi dialog

---
+++ plugins/ShapeLibrary/shape_library.lua
@@ -0,0 +1,38 @@
+-- Register Toolbar actions and initialize UI stuff
+function initUi()
+    -- Getting the source folder Path (The plugin folder-path)
+
+    app.registerUi({
+        ["menu"] = "Insert Shapes (simple)",
+        ["callback"] = "Show_simple_dialog",
+        ["toolbarId"] = "shapedialog",
+        ["iconName"] = "shapes_symbolic"
+    })
+    app.registerUi({
+        ["menu"] = "Insert Shapes (lgi)",
+        ["callback"] = "Show_lgi_dialog",
+    })
+    app.registerUi({
+        ["menu"] = "Extract_Stroke_Info",
+        ["callback"] = "Store_stroke_info",
+        ["toolbarId"] =
+        "StrInfo",
+        ["iconName"] = "extract-info-symbolic"
+    })
+end
+
+function Store_stroke_info()
+        local stroke_io = require "stroke_info"
+        local strokes = app.getStrokes("selection")
+        stroke_io.store_strokes_info_in_file(strokes)
+end
+
+function Show_simple_dialog()
+    local simple_dialog = require "simple_dialog"
+    simple_dialog.showMainShapeDialog()
+end
+
+function Show_lgi_dialog()
+    local lgi_dialog = require "lgi_dialog"
+    lgi_dialog.showMainShapeDialog()
+end
