915985688e3e1a5a3e7dd6f022657579773ee131
Roland Lötscher <roland_loetscher@hotmail.com>
2025-02-16 14:30:04 +0100

    Minor refactoring

--- plugins/ShapeLibrary/lgi_dialog.lua
+++ plugins/ShapeLibrary/lgi_dialog.lua
@@ -1,8 +1,10 @@
 local sep = package.config:sub(1, 1) -- path separator depends on OS
 local sourcePath = debug.getinfo(1).source:match("@?(.*" .. sep .. ")")
-local shapes_dict = require "config"
 local stroke_io = require "stroke_io"
 local insertion_helper = require "insertion_helper"
+local config_helper = require "config_helper"
+
+local shapes_dict = config_helper.getShapesData() or {}
 
 local _M = {}
 local hasLgi, lgi = pcall(require, "lgi")
@@ -23,6 +25,62 @@ local Gtk = lgi.require("Gtk", "3.0")
 local assert = lgi.assert
 local drawing = require "drawing"
 local index = 1 -- the index is set when one of the shapes is selected
+
+local function loadShapesFromDict(dict, window, reset)
+    if reset then
+        for _, page in ipairs(window.child.stack:get_children()) do
+            page:destroy()
+        end
+    end
+
+    for i, category in ipairs(dict) do
+        local scrolled_window = Gtk.ScrolledWindow {
+            margin_bottom = 24,
+            margin_top = 24,
+            propagate_natural_height = true,
+            max_content_height = 400,
+            Gtk.FlowBox {
+                id = 'flow_box',
+                orientation = "HORIZONTAL",
+                max_children_per_line = 6,
+                min_children_per_line = 3,
+                column_spacing = 12,
+                row_spacing = 12,
+                homogeneous = true,
+                selection_mode = "SINGLE",
+                on_child_activated = function(_, child) index = child:get_index() + 1 end
+            }
+        }
+        for j, shape in ipairs(category.shapes) do
+            local shape_box = Gtk.Box {
+                orientation = "VERTICAL",
+                spacing = 12,
+                Gtk.Label {
+                    label = shape.name,
+                },
+                Gtk.Frame {
+                    shadow_type = 'IN',
+                    expand = true,
+                    Gtk.DrawingArea {
+                        id = 'drawing_area',
+                        width = 100,
+                        height = 100,
+                    }
+                }
+            }
+            local strokes = get_strokes(i, j)
+            function shape_box.child.drawing_area:on_draw(cr)
+                drawing.draw_strokes(strokes)(self, cr)
+                return true
+            end
+
+            scrolled_window.child.flow_box:add(shape_box)
+        end
+
+        window.child.stack:add_titled(scrolled_window, i, category.name)
+    end
+    window:show_all()
+end
 
 function _M.showMainShapeDialog()
     local provider = Gtk.CssProvider()
@@ -53,21 +111,31 @@ function _M.showMainShapeDialog()
                         id = 'switcher',
                     },
                     Gtk.Box {
-                        orientation = "HORIZONTAL",
-                        hexpand = true,
-                        halign = "CENTER",
+                        orientation = "VERTICAL",
                         spacing = 12,
-                        Gtk.Button {
-                            id = 'add_category_button',
-                            hexpand = false,
-                            label = '+',
-                            tooltip_text = 'Add new category',
+                        Gtk.Box {
+                            orientation = "HORIZONTAL",
+                            hexpand = true,
+                            halign = "CENTER",
+                            spacing = 12,
+                            Gtk.Button {
+                                id = 'add_category_button',
+                                hexpand = false,
+                                label = '+',
+                                tooltip_text = 'Add new category',
+                            },
+                            Gtk.Button {
+                                id = 'remove_category_button',
+                                hexpand = false,
+                                label = '-',
+                                tooltip_text = 'Remove selected category',
+                            },
                         },
                         Gtk.Button {
-                            id = 'remove_category_button',
+                            id = 'rename_category_button',
                             hexpand = false,
-                            label = '-',
-                            tooltip_text = 'Remove selected category',
+                            label = 'Rename',
+                            tooltip_text = 'Rename selected category',
                         },
                     },
                 },
@@ -121,6 +189,12 @@ function _M.showMainShapeDialog()
                             label = "-",
                             tooltip_text = "Remove selected shape",
                         },
+                        Gtk.Button {
+                            id = 'rename_shape_button',
+                            hexpand = false,
+                            label = "Rename",
+                            tooltip_text = "Rename selected shape"
+                        }
                     },
                 }
             }
@@ -130,12 +204,18 @@ function _M.showMainShapeDialog()
     window.child.lbl_shape:get_style_context():add_provider(provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
     window.child.insert_button.on_button_press_event = function(event)
         local category = tonumber(window.child.stack:get_visible_child_name())
+        if not shapes_dict[category].shapes[index] then
+            print("Select shape first")
+            return
+        end
         local shape_name = shapes_dict[category].shapes[index].shapeName
         insertion_helper.insert_stroke(shape_name)
     end
 
-    -- Dialogs --
+    window.child.switcher:set_stack(window.child.stack)
+    loadShapesFromDict(shapes_dict, window, false)
 
+    -- Dialogs --
     window.child.add_category_button.on_button_press_event = function(event)
         local dialog = Gtk.Dialog {
             title = "Add Category",
@@ -165,7 +245,9 @@ function _M.showMainShapeDialog()
 
         function dialog:on_response(response)
             if response == Gtk.ResponseType.OK then
-                print("Add code for adding category: " .. hbox.child.name_entry.text)
+                config_helper.addCategory(hbox.child.name_entry.text)
+                shapes_dict = config_helper.getShapesData()
+                loadShapesFromDict(shapes_dict, window, true)
             end
             dialog:destroy()
         end
@@ -200,7 +282,7 @@ function _M.showMainShapeDialog()
 
         function dialog:on_response(response)
             if response == Gtk.ResponseType.OK then
-                print("Add code for removing category: " .. category_name)
+                config_helper.removeCategory(category_name)
             end
             dialog:destroy()
         end
@@ -210,7 +292,9 @@ function _M.showMainShapeDialog()
 
     window.child.replace_shape_button.on_button_press_event = function(event)
         local category = tonumber(window.child.stack:get_visible_child_name())
+        local category_name = shapes_dict[category].name
         local name = shapes_dict[category].shapes[index].name
+        local shapeName = shapes_dict[category].shapes[index].shapeName
 
         local dialog = Gtk.Dialog {
             width_request = 300,
@@ -236,7 +320,7 @@ function _M.showMainShapeDialog()
 
         function dialog:on_response(response)
             if response == Gtk.ResponseType.OK then
-                print("Add code for updating shape: " .. name)
+                config_helper.replaceShape(category_name, name, name, shapeName, shapeName)
             end
             dialog:destroy()
         end
@@ -322,58 +406,6 @@ function _M.showMainShapeDialog()
 
         dialog:show()
     end
-
-
-    window.child.switcher:set_stack(window.child.stack)
-
-    for i, category in ipairs(shapes_dict) do
-        local scrolled_window = Gtk.ScrolledWindow {
-            margin_bottom = 24,
-            margin_top = 24,
-            propagate_natural_height = true,
-            max_content_height = 400,
-            Gtk.FlowBox {
-                id = 'flow_box',
-                orientation = "HORIZONTAL",
-                max_children_per_line = 6,
-                min_children_per_line = 3,
-                column_spacing = 12,
-                row_spacing = 12,
-                homogeneous = true,
-                selection_mode = "SINGLE",
-                on_child_activated = function(_, child) index = child:get_index() + 1 end
-            }
-        }
-        for j, shape in ipairs(category.shapes) do
-            local shape_box = Gtk.Box {
-                orientation = "VERTICAL",
-                spacing = 12,
-                Gtk.Label {
-                    label = shape.name,
-                },
-                Gtk.Frame {
-                    shadow_type = 'IN',
-                    expand = true,
-                    Gtk.DrawingArea {
-                        id = 'drawing_area',
-                        width = 100,
-                        height = 100,
-                    }
-                }
-            }
-            local strokes = get_strokes(i, j)
-            function shape_box.child.drawing_area:on_draw(cr)
-                drawing.draw_strokes(strokes)(self, cr)
-                return true
-            end
-
-            scrolled_window.child.flow_box:add(shape_box)
-        end
-
-        window.child.stack:add_titled(scrolled_window, i, category.name)
-    end
-
-    window:show_all()
 end
 
 return _M
