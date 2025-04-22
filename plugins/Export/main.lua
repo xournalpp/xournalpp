1f04ee4d011fd690a5cb55af683714d01c54feb3
Yaroslav Halchenko <debian@onerussian.com>
2025-04-22 13:54:25 -0400

    [DATALAD RUNCMD] Fix intialize typo
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "git-sedi intialize initialize",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^

--- plugins/Export/main.lua
+++ plugins/Export/main.lua
@@ -1,4 +1,4 @@
--- Register all Toolbar actions and intialize all UI stuff
+-- Register all Toolbar actions and initialize all UI stuff
 function initUi()
   app.registerUi({["menu"] = "Export to pdf", ["callback"] = "exportPdf", ["accelerator"] = "<Shift><Alt>p"});
   app.registerUi({["menu"] = "Export to svg", ["callback"] = "exportSvg", ["accelerator"] = "<Shift><Alt>s"});
