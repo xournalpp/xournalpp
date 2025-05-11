8f7fe41341a9266631bfa4c1a3b5a404a19490ba
Benjamin Hennion <17818041+bhennion@users.noreply.github.com>
2025-05-11 12:26:28 +0200

    Fix two warnings (#6422)
    * Fix -Wreturn-type warning
    
    * Fix unused-return-value warning

--- src/core/audio/AudioQueue.h
+++ src/core/audio/AudioQueue.h
@@ -106,7 +106,7 @@ public:
     [[nodiscard]] std::unique_lock<std::mutex> acquire_lock() {
         std::unique_lock retLock{this->queueLock, std::defer_lock};
         std::lock(retLock, this->internalLock);
-        std::lock_guard{this->internalLock, std::adopt_lock};
+        std::lock_guard guard{this->internalLock, std::adopt_lock};
         return retLock;
     }
 
