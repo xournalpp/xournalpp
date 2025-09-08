81cc307be4b0ec6f5d9d222874e1c20c085c4a3b
Thomas Moerschell <144177788+tmoerschell@users.noreply.github.com>
2025-09-08 15:42:21 +0200

    Use constexpr tag and attribute names for file save
    - Centralize all tag and attribute name strings in dedicated header files
    - Reference tag names using an enum
    - Use string views for tag names

--- src/util/include/util/OutputStream.h
+++ src/util/include/util/OutputStream.h
@@ -11,7 +11,8 @@
 
 #pragma once
 
-#include <string>  // for string
+#include <string>       // for string
+#include <string_view>  // for string_view
 
 #include <zlib.h>  // for gzFile
 
@@ -24,7 +25,7 @@ public:
 
 public:
     void write(const char* str);
-    void write(const std::string& str);
+    void write(const std::string_view sv);
 
     virtual void write(const char* data, size_t len) = 0;
     virtual void close() = 0;
