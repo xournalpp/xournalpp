f9f1c25c099fc946b815f1d01d1039ecf24a038f
Thomas Moerschell <144177788+tmoerschell@users.noreply.github.com>
2025-10-26 18:20:44 +0100

    Declare all strings used for file loading and saving as UTF-8
    - Adapt file saving to accept UTF-8 strings
    - Adapt file loading to use UTF-8 tag names and use xoj::util::utf8 in
      some places.

--- src/core/control/xojfile/XmlTags.h
+++ src/core/control/xojfile/XmlTags.h
@@ -39,8 +39,8 @@ enum class Type : size_t {
 };
 
 // Names corresponding to the xoj::xml_tags::Type enum. They must imperatively correspond to the order of the enum!
-constexpr EnumIndexedArray<std::string_view, Type> NAMES = {
-        "[unknown]", "xournal",   "MrWriter", "title", "preview", "page",     "audio",     "background",
-        "layer",     "timestamp", "stroke",   "text",  "image",   "teximage", "attachment"};
+constexpr EnumIndexedArray<std::u8string_view, Type> NAMES = {
+        u8"[unknown]", u8"xournal",   u8"MrWriter", u8"title", u8"preview", u8"page",     u8"audio",     u8"background",
+        u8"layer",     u8"timestamp", u8"stroke",   u8"text",  u8"image",   u8"teximage", u8"attachment"};
 
 }  // namespace xoj::xml_tags
