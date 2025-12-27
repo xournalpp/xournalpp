2236dff104fc43a4a13ccc7dabecd3292f45849a
Roland Lötscher <roland_loetscher@hotmail.com>
2025-12-27 14:57:48 +0100

    Split off LinkPopover class from LinkEditor class

--- src/core/control/tools/LinkEditor.cpp
+++ src/core/control/tools/LinkEditor.cpp
@@ -18,18 +18,68 @@
 #include "util/XojMsgBox.h"            // for XojMsgBox
 #include "util/i18n.h"                 // for FS, _, _F
 
-LinkEditor::LinkEditor(XournalView* view): view(view), control(view->getControl()), documentWidget(view->getWidget()) {
-    this->createPopover();
+LinkPopover::LinkPopover(XournalView* view): view(view) {
+    this->popover = GTK_POPOVER(gtk_popover_new(view->getWidget()));
+    gtk_popover_set_modal(popover, false);
+    gtk_popover_set_constrain_to(this->popover, GTK_POPOVER_CONSTRAINT_WINDOW);
+    GtkWidget* vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    this->label = GTK_LABEL(gtk_label_new(""));
+    gtk_box_pack_start(GTK_BOX(vbox), GTK_WIDGET(this->label), true, true, POPOVER_PADDING);
+    gtk_container_add(GTK_CONTAINER(this->popover), vbox);
+}
+
+LinkPopover::~LinkPopover() {
+    gtk_widget_destroy(GTK_WIDGET(this->label));
+    gtk_widget_destroy(GTK_WIDGET(this->popover));
+}
+
+void LinkPopover::show() { gtk_widget_show(GTK_WIDGET(this->popover)); }
+
+void LinkPopover::show_all() { gtk_widget_show_all(GTK_WIDGET(this->popover)); }
+
+void LinkPopover::hide() { gtk_widget_hide(GTK_WIDGET(this->popover)); }
+
+void LinkPopover::popup() { gtk_popover_popup(this->popover); }
+
+void LinkPopover::popdown() { gtk_popover_popdown(this->popover); }
+
+bool LinkPopover::hasLink() { return (this->link != nullptr); }
+
+void LinkPopover::updateLabel(bool markup) {
+    if (markup) {
+        std::string url = this->link->getUrl();
+        std::string str = "<a href=\"" + url + "\"> " + url + "</a> \n" +
+                          "<span size=\"smaller\"><i> Double click to edit. CTRL + click to open. </i></span>";
+        gtk_label_set_markup(this->label, str.c_str());
+    } else {
+        gtk_label_set_text(this->label, this->link->getUrl().c_str());
+    }
+}
+
+void LinkPopover::positionPopover() {
+    if (this->link) {
+        XojPageView* pageView = view->getViewFor(view->getCurrentPage());
+        auto pos = pageView->getPixelPosition();
+        GdkRectangle rect{pos.x + int(this->link->getX() * pageView->getZoom()),
+                          pos.y + int(this->link->getY() * pageView->getZoom()),
+                          int(this->link->getElementWidth() * pageView->getZoom()),
+                          int(this->link->getElementHeight() * pageView->getZoom())};
+        gtk_popover_set_pointing_to(this->popover, &rect);
+    }
+}
+
+void LinkPopover::linkTo(Link* link) {
+    this->link = link;
+    positionPopover();
+}
+
+LinkEditor::LinkEditor(XournalView* view): view(view), control(view->getControl()) {
+    this->highlightPopover = std::make_unique<LinkPopover>(view);
+    this->selectPopover = std::make_unique<LinkPopover>(view);
     this->control->getZoomControl()->addZoomListener(this);
 }
 
-LinkEditor::~LinkEditor() {
-    gtk_widget_destroy(GTK_WIDGET(this->linkPopoverLabelHighlight));
-    gtk_widget_destroy(GTK_WIDGET(this->linkPopoverHighlight));
-    gtk_widget_destroy(GTK_WIDGET(this->linkPopoverLabelSelect));
-    gtk_widget_destroy(GTK_WIDGET(this->linkPopoverSelect));
-    this->control->getZoomControl()->removeZoomListener(this);
-}
+LinkEditor::~LinkEditor() { this->control->getZoomControl()->removeZoomListener(this); }
 
 void LinkEditor::startEditing(const PageRef& page, const int x, const int y) {
     this->linkElement = nullptr;
@@ -92,56 +142,56 @@ void LinkEditor::select(const PageRef& page, const int x, const int y, const boo
     bool noSelection = true;
     for (auto&& e: page->getSelectedLayer()->getElements()) {
         if (e->getType() == ELEMENT_LINK && e->containsPoint(x, y)) {
-            Link* localLinkElement = dynamic_cast<Link*>(e.get());
+            Link* link = dynamic_cast<Link*>(e.get());  // link on which user clicked
 
             if (controlDown) {
                 GError* error = NULL;
-                gtk_show_uri_on_window(NULL, localLinkElement->getUrl().c_str(), GDK_CURRENT_TIME, &error);
+                gtk_show_uri_on_window(NULL, link->getUrl().c_str(), GDK_CURRENT_TIME, &error);
                 if (error != NULL) {
                     auto* win = control->getGtkWindow();
-                    std::string msg =
-                            FS(_F("Error opening “{1}”: {2}") % localLinkElement->getUrl().c_str() % error->message);
+                    std::string msg = FS(_F("Error opening “{1}”: {2}") % link->getUrl() % error->message);
                     XojMsgBox::showMessageToUser(win, msg, GTK_MESSAGE_ERROR);
                     g_error_free(error);
                 }
                 return;
             }
 
-            if (!localLinkElement->isSelected()) {
-                localLinkElement->setSelected(true);
-                this->positionPopover(pageView, localLinkElement, linkPopoverSelect);
-                gtk_label_set_markup(GTK_LABEL(this->linkPopoverLabelSelect),
-                                     this->toLinkMarkup(localLinkElement->getUrl()).c_str());
-                gtk_widget_show_all(GTK_WIDGET(this->linkPopoverSelect));
-                if (localLinkElement->isHighlighted()) {
-                    gtk_widget_hide(GTK_WIDGET(this->linkPopoverHighlight));
+            if (!link->isSelected()) {
+                if (link->isHighlighted()) {
+                    this->highlightPopover->hide();
                 }
-                gtk_popover_popup(this->linkPopoverSelect);
-                this->selectedLink = localLinkElement;
+                link->setSelected(true);
+                // FixMe !!
+                this->selectPopover->linkTo(link);
+                this->selectPopover->updateLabel(true);
+                this->selectPopover->show_all();
+                this->selectPopover->popup();
             } else {
-                localLinkElement->setSelected(false);
-                if (localLinkElement->isHighlighted()) {
-                    gtk_widget_hide(GTK_WIDGET(this->linkPopoverSelect));
-                    gtk_widget_show(GTK_WIDGET(this->linkPopoverHighlight));
+                link->setSelected(false);
+                if (link->isHighlighted()) {
+                    this->selectPopover->hide();
+                    this->highlightPopover->show();
                 } else {
-                    gtk_popover_popdown(this->linkPopoverSelect);
+                    this->selectPopover->popdown();
                 }
-                this->selectedLink = nullptr;
+                this->selectPopover->linkTo(nullptr);
             }
-            page->fireElementChanged(localLinkElement);
+            page->fireElementChanged(link);
             noSelection = false;
         } else if (e->getType() == ELEMENT_LINK) {
-            Link* localLinkElement = dynamic_cast<Link*>(e.get());
-            localLinkElement->setSelected(false);
-            page->firePageChanged();
+            Link* link = dynamic_cast<Link*>(e.get());
+            if (link->isSelected()) {
+                link->setSelected(false);
+                page->fireElementChanged(link);
+            }
         }
     }
 
     if (noSelection) {
-        gtk_popover_popdown(this->linkPopoverSelect);
-        bool noPreselection = (this->selectedLink == nullptr);
-        this->selectedLink = nullptr;
-        if (noPreselection) {
+        this->selectPopover->popdown();
+        bool preselection = this->selectPopover->hasLink();
+        this->selectPopover->linkTo(nullptr);
+        if (!preselection) {
             startEditing(page, x, y);
         }
     }
@@ -150,75 +200,37 @@ void LinkEditor::select(const PageRef& page, const int x, const int y, const boo
 void LinkEditor::highlight(const PageRef& page, const int x, const int y, XojPageView* pageView) {
     for (auto&& e: page->getSelectedLayer()->getElements()) {
         if (e->getType() == ELEMENT_LINK && e->containsPoint(x, y)) {
-            this->highlightedLink = dynamic_cast<Link*>(e.get());
-            if (this->highlightedLink->isHighlighted()) {
+            Link* link = dynamic_cast<Link*>(e.get());
+            if (link->isHighlighted()) {
                 continue;
             }
-            this->highlightedLink->setHighlighted(true);
-            page->fireElementChanged(this->highlightedLink);
-            GdkWindow* window = gtk_widget_get_window(this->documentWidget);
+            link->setHighlighted(true);
+            this->highlightPopover->linkTo(link);
+            page->fireElementChanged(link);
+            GdkWindow* window = gtk_widget_get_window(view->getWidget());
             GdkCursor* cursor = gdk_cursor_new_from_name(gdk_window_get_display(window), "alias");
             gdk_window_set_cursor(window, cursor);
-            positionPopover(pageView, this->highlightedLink, this->linkPopoverHighlight);
-            gtk_label_set_text(GTK_LABEL(this->linkPopoverLabelHighlight), highlightedLink->getUrl().c_str());
-            if (!this->highlightedLink->isSelected()) {
-                gtk_widget_show_all(GTK_WIDGET(this->linkPopoverHighlight));
-                gtk_popover_popup(this->linkPopoverHighlight);
+            this->highlightPopover->updateLabel(false);
+            if (!link->isSelected()) {
+                this->highlightPopover->show_all();
+                this->highlightPopover->popup();
             }
-        } else if (e->getType() == ELEMENT_LINK && this->highlightedLink == e.get()) {
-            Link* localLinkElement = dynamic_cast<Link*>(e.get());
-            localLinkElement->setHighlighted(false);
-            page->fireElementChanged(localLinkElement);
+        } else if (e->getType() == ELEMENT_LINK && this->highlightPopover->getLink() == e.get()) {
+            Link* link = dynamic_cast<Link*>(e.get());
+            link->setHighlighted(false);
+            page->fireElementChanged(link);
             GdkWindow* window = gtk_widget_get_window(view->getWidget());
             GdkCursor* cursor = gdk_cursor_new_from_name(gdk_window_get_display(window), "hand2");
             gdk_window_set_cursor(window, cursor);
-            if (!this->highlightedLink->isSelected()) {
-                gtk_popover_popdown(this->linkPopoverHighlight);
+            if (!this->highlightPopover->getLink()->isSelected()) {
+                this->highlightPopover->popdown();
             }
-            this->highlightedLink = nullptr;
+            this->highlightPopover->linkTo(nullptr);
         }
     }
 }
 
-void LinkEditor::createPopover() {
-    this->linkPopoverHighlight = GTK_POPOVER(gtk_popover_new(view->getWidget()));
-    gtk_popover_set_modal(linkPopoverHighlight, false);
-    gtk_popover_set_constrain_to(this->linkPopoverHighlight, GTK_POPOVER_CONSTRAINT_WINDOW);
-    GtkWidget* vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    this->linkPopoverLabelHighlight = gtk_label_new("");
-    gtk_box_pack_start(GTK_BOX(vbox), this->linkPopoverLabelHighlight, true, true, POPOVER_PADDING);
-    gtk_container_add(GTK_CONTAINER(this->linkPopoverHighlight), vbox);
-
-    this->linkPopoverSelect = GTK_POPOVER(gtk_popover_new(view->getWidget()));
-    gtk_popover_set_modal(linkPopoverSelect, false);
-    gtk_popover_set_constrain_to(this->linkPopoverSelect, GTK_POPOVER_CONSTRAINT_WINDOW);
-    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    this->linkPopoverLabelSelect = gtk_label_new("");
-    gtk_box_pack_start(GTK_BOX(vbox), this->linkPopoverLabelSelect, true, true, POPOVER_PADDING);
-    gtk_container_add(GTK_CONTAINER(this->linkPopoverSelect), vbox);
-}
-
-std::string LinkEditor::toLinkMarkup(std::string url) {
-    return "<a href=\"" + url + "\"> " + url + "</a> \n" +
-           "<span size=\"smaller\"><i> Double click to edit. CTRL + click to open. </i></span>";
-}
-
-void LinkEditor::positionPopover(XojPageView* pageView, Link* element, GtkPopover* popover) {
-    auto pos = pageView->getPixelPosition();
-    GdkRectangle rect{pos.x + int(element->getX() * pageView->getZoom()),
-                      pos.y + int(element->getY() * pageView->getZoom()),
-                      int(element->getElementWidth() * pageView->getZoom()),
-                      int(element->getElementHeight() * pageView->getZoom())};
-    gtk_popover_set_pointing_to(popover, &rect);
-}
-
 void LinkEditor::zoomChanged() {
-    if (this->selectedLink != nullptr) {
-        this->positionPopover(view->getViewFor(view->getCurrentPage()), this->selectedLink, this->linkPopoverSelect);
-    }
-
-    if (this->highlightedLink != nullptr) {
-        this->positionPopover(view->getViewFor(view->getCurrentPage()), this->highlightedLink,
-                              this->linkPopoverHighlight);
-    }
+    this->selectPopover->positionPopover();
+    this->highlightPopover->positionPopover();
 }
