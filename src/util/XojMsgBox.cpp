3b3a5ee02669af92bcca575128ce8fe6109e578c
rolandlo <roland_loetscher@hotmail.com>
2026-03-03 19:58:54 +0100

    Fix palette directories not showing up in file explorer on Windows
    Two issues are fixed:
    
    1) On Windows a valid file uri is not file://<path>, but file:///<path>.
    It doesn't matter if backslashes or slashes are being used in the path.
    
    2) Link activation didn't work on Windows even with correct URI.
    
    Also replaced deprecated function gtk_show_uri by gtk_show_uri_on_window

--- src/util/XojMsgBox.cpp
+++ src/util/XojMsgBox.cpp
@@ -205,12 +205,12 @@ void XojMsgBox::showHelp(GtkWindow* win) { openURL(win, XOJ_HELP); }
 
 void XojMsgBox::openURL(GtkWindow* win, const char* url) {
 #ifdef _WIN32
-    // gvfs is not in MSYS repositories, so we can't use gtk_show_uri.
+    // gvfs is not in MSYS repositories, so we can't use gtk_show_uri_on_window.
     // Instead, we use the native API.
     ShellExecute(nullptr, "open", url, nullptr, nullptr, SW_SHOW);
 #else
     GError* error = nullptr;
-    gtk_show_uri(gtk_window_get_screen(win), url, gtk_get_current_event_time(), &error);
+    gtk_show_uri_on_window(win, url, gtk_get_current_event_time(), &error);
 
     if (error) {
         std::string msg = FS(_F("Unable to open the link: {1}\n{2}") % url % error->message);
