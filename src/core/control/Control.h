b5cfa192aab2f6e414b3381de4c434103f3a425c
the-an0nym <the-an0nym@users.noreply.github.com>
2026-03-10 11:54:50 +0100

    Add page rotation UI to sidebar rightclick

--- src/core/control/Control.h
+++ src/core/control/Control.h
@@ -238,6 +238,8 @@ public:
     void deletePage();
     void movePageTowardsBeginning();
     void movePageTowardsEnd();
+    void rotatePageClockwise();
+    void rotatePageCounterClockwise();
 
     /**
      * Ask the user whether a page with the given id
