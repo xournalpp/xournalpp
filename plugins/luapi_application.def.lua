608ed1b18273a8eb7397ed0ec44240576b200b74
atticus-sullivan <oss.heindl+git@protonmail.com>
2026-06-21 18:31:07 +0200

    run the lua-def script to update the lua-def file

--- plugins/luapi_application.def.lua
+++ plugins/luapi_application.def.lua
@@ -1206,6 +1206,7 @@ app.C = {
     Tool_selectPdfTextRect = 22,
     Tool_laserPointerPen = 23,
     Tool_laserPointerHighlighter = 24,
+    Tool_link = 25,
     EraserType_none = 0,
     EraserType_default = 1,
     EraserType_whiteout = 2,
