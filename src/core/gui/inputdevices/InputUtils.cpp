14194e2016022edc2a71955cfcc3860b25a63bdd
Benjamin Hennion <17818041+bhennion@users.noreply.github.com>
2026-07-05 13:44:50 +0200

    Fix data-race related segfault in RenderJob::renderToBuffer() (#7581)
    The bug is due to a very short time when the ToolHandler's activeTool
    pointer is null, when using a button/device with no tool configuration assigned to it

--- src/core/gui/inputdevices/InputUtils.cpp
+++ src/core/gui/inputdevices/InputUtils.cpp
@@ -7,6 +7,7 @@
 #include "control/settings/ButtonConfig.h"   // for ButtonConfig
 #include "control/settings/Settings.h"       // for Settings
 #include "control/settings/SettingsEnums.h"  // for Button, BUTTON_TOUCH
+#include "util/Assert.h"
 
 
 bool InputUtils::applyButton(ToolHandler* toolHandler, Settings* settings, Button button) {
@@ -16,11 +17,8 @@ bool InputUtils::applyButton(ToolHandler* toolHandler, Settings* settings, Butto
         toolChanged = true;
         ButtonConfig* cfg = settings->getButtonConfig(button);
 
-        // if ToolChange is not activated for this button stay with ToolBartool
-        if (cfg->getAction() == TOOL_NONE)
-            toolChanged = toolHandler->pointActiveToolToToolbarTool();
-        else
-            cfg->applyNoChangeSettings(toolHandler, button);
+        xoj_assert(cfg->getAction() != TOOL_NONE);
+        cfg->applyNoChangeSettings(toolHandler, button);
     }
     return toolChanged;
 }
